13471d121586e8c82e88fe4e0dc9e37b6c930524
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-01-13 23:57:30 +0000

    * Makeit.m build-script modified to add link directive for the   user32.lib which is also needed.
    * Rebuilt Screen.dll with a proper compiler so the startup
      dialog box disappears and we get the expected user experience ;)
    
    -> Starting from this revision we could do official releases for
       Windows.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@121 f5efbe48-b203-0410-9429-fc5e098e5a54

--- PsychSourceGL/Source/makeit.m
+++ PsychSourceGL/Source/makeit.m
@@ -1,5 +1,5 @@
 function makeit
-mex -v -outdir C:\kleinerm\trunk\PsychSourceGL\Projects\Windows\build\ -output Screen -IC:\Programme\QuickTimeSDK\CIncludes -IC:\Programme\MicrosoftVisualStudio\VC98\Include -ICommon\Base -ICommon\Screen -IWindows\Base -IWindows\Screen Windows\Screen\*.c Windows\Base\*.c Common\Base\*.c Common\Screen\*.c gdi32.lib advapi32.lib glu32.lib opengl32.lib qtmlClient.lib
+mex -v -outdir C:\kleinerm\trunk\PsychSourceGL\Projects\Windows\build\ -output Screen -IC:\Programme\QuickTimeSDK\CIncludes -IC:\Programme\MicrosoftVisualStudio\VC98\Include -ICommon\Base -ICommon\Screen -IWindows\Base -IWindows\Screen Windows\Screen\*.c Windows\Base\*.c Common\Base\*.c Common\Screen\*.c user32.lib gdi32.lib advapi32.lib glu32.lib opengl32.lib qtmlClient.lib
 dos('copy C:\kleinerm\trunk\PsychSourceGL\Projects\Windows\build\Screen.dll C:\kleinerm\trunk\Psychtoolbox\PsychBasic\');
 dos('copy C:\kleinerm\trunk\PsychSourceGL\Projects\Windows\build\Screen.dll C:\MATLABR11\toolbox\PsychToolbox\PsychBasic\');
 return;
