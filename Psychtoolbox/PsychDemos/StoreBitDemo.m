3e0d14700f3afc0f5b06dc23f729305ce52fd19c
allen_ingling <Allen.Ingling@nyu.edu>
2006-01-31 20:09:22 +0000

    Movied StoreBit into PsychDemos.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@172 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ Psychtoolbox/PsychDemos/StoreBitDemo.m
@@ -0,0 +1,12 @@
+function oldBit=StoreBit(newBit)
+
+% oldBit=StoreBit([newBit])
+%
+% StoreBit stores and returns a single bit.  The C source for StoreBit
+% demonstates use of the OS X Cocoa API from a MATLAB mex file. 
+
+% HISTORY
+%
+% 9/07/05 awi Wrote it.  
+
+PsychAssertMex;
