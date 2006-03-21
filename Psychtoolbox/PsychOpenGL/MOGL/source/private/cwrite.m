609e2953705117b1e4d1d82fc04faf6b1efc3727
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-03-21 00:55:52 +0000

    * cwrite.m in MOGL/source/private fixed.
      Now we have proper error-handling: autocode.m aka cwrite.m
      creates checking code in all gl_auto.c subfunctions to check
      for unsupported OpenGL commands, aka NULL function-ptrs and
      triggers a proper error handling routine if a Matlab file
      tries to call one of these unsupported functions.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@251 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOpenGL/MOGL/source/private/cwrite.m
+++ Psychtoolbox/PsychOpenGL/MOGL/source/private/cwrite.m
@@ -8,7 +8,7 @@ function cwrite( fid, funcp, C )
 % 21-Mar-2006 -- Extended with checking for unsupported glXXX functions. (MK)
 
 fprintf(fid,'void %s( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] ) {\n\n\t',C.interfacename);
-fprintf(fid,'if (NULL == %s) mogl_glunsupported("%s");\n', funcp.fname, funcp.fname);
+fprintf(fid,'if (NULL == %s) mogl_glunsupported("%s");\n\t', funcp.fname, funcp.fname);
 fprintf(fid,'%s%s(%s);\n',C.arg_out{1},funcp.fname,strrep(commalist(C.arg_in{:}),', ',sprintf(',\n\t\t')));
 fprintf(fid,'\n}\n\n');
 
