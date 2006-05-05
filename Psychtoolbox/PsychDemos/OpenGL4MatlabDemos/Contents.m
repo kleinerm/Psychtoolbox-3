3fdea2e7b8cd39686b4a7c0c75ef2ccf469b22be
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-05-05 21:11:43 +0000

    MinimalisticOpenGLDemo updated to demonstrate basic texture mapping.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@298 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychDemos/OpenGL4MatlabDemos/Contents.m
+++ Psychtoolbox/PsychDemos/OpenGL4MatlabDemos/Contents.m
@@ -6,17 +6,25 @@
 %
 % These demos are all pretty beta and a bit rough, so good luck!
 %
-% UtahTeapotDemo    -- Draws a rotating 3D teapot, demonstrates basic setup
-%                      of lighting, camera and geometry.
+% MinimalisticOpenGLDemo -- A nice starter. First draws a simple scene with
+%                           simple lighting, then draws a simple animation of
+%                           a rotating earth globe, showing basic use of texture
+%                           mapping.
 %
-% SpinningCubeDemo  -- Extends on UtahTeapotDemo by introducing texture mapping.
+% UtahTeapotDemo         -- Draws a rotating 3D teapot, demonstrates basic setup
+%                           of lighting, camera and geometry.
 %
-% SpinningMovieCube -- Extends SpinningCubeDemo with cube faces that play back
-%                      a Quicktime movie file, demonstrating exchange of textures
-%                      between Psychtoolbox and Matlab OpenGL.
+% SpinningCubeDemo       -- Extends on UtahTeapotDemo by introducing texture mapping.
 %
-% GLSLDemo          -- Demo that shows how to use the OpenGL Shading Language (GLSL).
+% SpinningMovieCube      -- Extends SpinningCubeDemo with cube faces that play back
+%                           a Quicktime movie file, demonstrating exchange of textures
+%                           between Psychtoolbox and Matlab OpenGL.
 %
-% MorphDemo         -- This demo shows how to load premade shapes from filesystem and
-%                      how to morph them into each other.
+% MorphDemo         -- This demo shows how to load premade 3D objects from filesystem and
+%                      how to morph them into each other via the Psychtoolbox function
+%                      moglmorpher(). Psychtoolbox can load simple Alias-Wavefront OBJ
+%                      files that can be created in standard 3D software.
 %
+% GLSLDemo          -- Demo that shows how to use the OpenGL Shading Language (GLSL). This
+%                      demo may only work on recent graphics hardware, so dont be surprised
+%                      if it fails on older hardware.
