3fd7720b14916c9e923e0e67664ed15658c9b029
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-05-06 01:43:20 +0000

    * PsychtoolboxVersion.m extended: Provides now info about flavor, SVN-Revision and SVN Web-URL.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@300 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychBasic/PsychtoolboxVersion.m
+++ Psychtoolbox/PsychBasic/PsychtoolboxVersion.m
@@ -1,11 +1,10 @@
 function [versionString, versionStructure]=PsychtoolboxVersion
-% OS X: ___________________________________________________________________
+% OS X, Windows, Linux: ___________________________________________________
 % 
 % [versionString, versionStructure]=PsychtoolboxVersion
 %
-% Return a string identifying this release of the Psychtoolbox.  The string
-% consists of three integers separated by decimal points, for example:
-% 1.0.1.  Each of the three integer fields has a particular meaning.  
+% Return a string identifying this release of the Psychtoolbox.
+% The first three numbers identify the base version of Psychtoolbox:
 %
 % ¥ Leftmost: increments indicate a significant change in the feature
 % set, either through accumulated progress over time or abrupt introduction
@@ -13,7 +12,7 @@ function [versionString, versionStructure]=PsychtoolboxVersion
 %
 % ¥ Middle: Even numbers designate a "stable" release. The objective for
 % even number releases is that the software should run stably, as opposed
-% to introduction of new features. An even number is not a gurantee of
+% to introduction of new features. An even number is not a guarantee of
 % stability, but an expression of intent.  Odd numbers indicate a
 % "developer" release.  Odd number releases are incomplete, the software is
 % made available for the purpose of public collaboration in development.
@@ -24,13 +23,35 @@ function [versionString, versionStructure]=PsychtoolboxVersion
 % Numeric values of the three integer fields contained in versionString are
 % available in fields of the second return argument, "versionStructure".
 %
+% The field 'Flavor' defines the subtype of Psychtoolbox being used:
+% * beta: An experimental release that is already tested by the developers,
+% but not yet sufficiently tested or proven in the field. Beta releases
+% contain lots of new and experimental features that may be useful to you
+% but that may change slightly in behaviour or syntax in the final release,
+% making it necessary for you to adapt your code after a software update.
+% Beta releases are known to be imperfect and fixing bugs in them is not a
+% high priority.
+%
+% * stable: A release with the intention of being well-tested and reliable.
+% Fixing bugs found in stable releases has a high priority and syntax or
+% behaviour of features in a stable release is not likely to change. Code
+% written against a stable release should work after an update without the
+% need for you to modify anything.
+%
+% The revision number and the provided URL allows you to visit the developer
+% website in the Internet and get direct access to all development logs
+% regarding your working copy of Psychtoolbox.
+%
+% Be aware that execution of the PsychtoolboxVersion command can take a
+% lot of time (in the order of multiple seconds to 1 minute).
+%
 % Most Psychtoolbox mex files now provide a built-in 'Version' command
 % which returns version for themselves.  The version string for the
 % built-in version numbers contains a fourth numeric field named "build".
 % The build number is a unique serial number.  Mex files distinquished only
 % by build numbers were compiled from identical C source files. 
 %
-% OS 9 and Windows: _______________________________________________________
+% OS 9 : __________________________________________________________________
 %
 % versionNumber=PsychtoolboxVersion
 %
@@ -44,12 +65,15 @@ function [versionString, versionStructure]=PsychtoolboxVersion
 %   2/9/01      awi     added fullfile command for platform-independent pathname   
 %   6/29/02     dgp     Use new PsychtoolboxRoot function to cope with user-changed folder names.
 %   7/12/04     awi     ****** OS X-specific fork from the OS 9 version *******
-%                        Noted mex file versioning. 
+%                       Noted mex file versioning. 
 %   7/26/04     awi     Partitioned help and added OS X section.  Enhanced
 %                       OS9+Windows section.
-%   10/4/05	   awi   Note here that dgp changed "IsWindows" to "IsWin" at unknown date prior
-%							between 7/26/04 and 10/4/05.    
-
+%   10/4/05	   awi      Note here that dgp changed "IsWindows" to "IsWin" at unknown date prior
+%						between 7/26/04 and 10/4/05.    
+%
+%   5/5/06     mk       Tries to query info from Subversion and displays info about last date
+%                       of change, SVN revision and flavor. This code is pretty experimental and
+%                       probably also somewhat fragile. And its sloooow.
 global Psychtoolbox
 
 if IsOS9
@@ -72,7 +96,12 @@ elseif IsOSX | IsLinux | IsWin
         Psychtoolbox.version.minor=0;
         Psychtoolbox.version.point=0;
         Psychtoolbox.version.string='';
- 		file=fullfile(PsychtoolboxRoot,'Contents.m');
+        Psychtoolbox.version.flavor='';
+        Psychtoolbox.version.revision=0;
+        Psychtoolbox.version.revstring='';
+        Psychtoolbox.version.websvn='';
+        
+        file=fullfile(PsychtoolboxRoot,'Contents.m');
 		f=fopen(file,'r');
 		fgetl(f);
 		s=fgetl(f);
@@ -81,15 +110,63 @@ elseif IsOSX | IsLinux | IsWin
         Psychtoolbox.version.major=cvv(1);
         Psychtoolbox.version.minor=cvv(2);
         Psychtoolbox.version.point=cvv(3);
-        Psychtoolbox.version.string=sprintf('%d.%d.%d', Psychtoolbox.version.major, Psychtoolbox.version.minor, Psychtoolbox.version.point);
-        ss=s(n:end);
-		Psychtoolbox.date=ss(min(find(ss-' ')):end);
+
+        % Additional parser code for SVN information. This is slooow!
+        if IsWin | IsLinux
+            svncmdpath='';
+        else
+            svncmdpath='/usr/local/bin/';
+        end;
+        
+        % Find revision string for Psychtoolbox that defines the SVN revision
+        % to which this working copy corresponds:
+        [status , result] = system([svncmdpath 'svnversion -c ' PsychtoolboxRoot]);
+        if status==0
+            % Parse output of svnversion: Find revision number of the working copy.
+            colpos=strfind(result, ':');
+            if isempty(colpos)
+                Psychtoolbox.version.revision=sscanf(result, '%d',1);
+            else
+                cvv = sscanf(result, '%d:%d',2);
+                Psychtoolbox.version.revision=cvv(2);
+            end
+            if isempty(strfind(result, 'M'))
+                Psychtoolbox.version.revstring = sprintf('Corresponds to SVN Revision %d', Psychtoolbox.version.revision);
+            else
+                Psychtoolbox.version.revstring = sprintf('Corresponds to SVN Revision %d but is *locally modified* !', Psychtoolbox.version.revision);
+            end
+            
+            % Ok, now find the flavor and such... This is super expensive - needs network access...
+            %[status , result] = system([svncmdpath 'svn info --xml -r ' num2str(Psychtoolbox.version.revision) '  ' PsychtoolboxRoot]);
+            status=-1;
+            if status<0 | status>0
+                % Fallback path:
+                [status , result] = system([svncmdpath 'svn info --xml ' PsychtoolboxRoot]);
+            end
+            
+            startdel = strfind(result, '/osxptb/') + length('/osxptb/');
+            findel = min(strfind(result(startdel:length(result)), '/Psychtoolbox')) + startdel - 2;
+            Psychtoolbox.version.flavor = result(startdel:findel);
+            
+            % And the date of last commit:
+            startdel = strfind(result, '<date>') + length('<date>');
+            findel = strfind(result, 'T') - 1;
+            Psychtoolbox.date = result(startdel:findel);            
+            % Build final SVN URL: This is the location where one can find detailled info about this working copy:
+            Psychtoolbox.version.websvn = sprintf('http://svn.berlios.de/wsvn/osxptb/?rev=%d&sc=0', Psychtoolbox.version.revision);
+            % Build final version string:
+            Psychtoolbox.version.string = sprintf('%d.%d.%d-post - Flavor: %s - %s\nFor more info visit:\n%s', Psychtoolbox.version.major, Psychtoolbox.version.minor, Psychtoolbox.version.point, ...
+                                                  Psychtoolbox.version.flavor, Psychtoolbox.version.revstring, Psychtoolbox.version.websvn);
+        else        
+            % Fallback path if svn commands fail for some reason. Output as much as we can.
+            fprintf('PsychtoolboxVersion: WARNING - Could not query additional version information from SVN -- svn tools not properly installed?!?\n');
+            Psychtoolbox.version.string=sprintf('%d.%d.%d', Psychtoolbox.version.major, Psychtoolbox.version.minor, Psychtoolbox.version.point);
+            ss=s(n:end);
+            Psychtoolbox.date=ss(min(find(ss-' ')):end);
+        end    
     end
     versionString=Psychtoolbox.version.string;
     versionStructure=Psychtoolbox.version;
 else
     error('Unrecognized Psychtoolbox platform');
 end
-   
-
-
