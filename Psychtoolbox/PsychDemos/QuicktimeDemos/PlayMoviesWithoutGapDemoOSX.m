372517d10402bb86a4dfb888c75149904e59e002
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-06-05 03:28:12 +0000

    * New demo PlayMoviesWithoutGapDemoOSX - Demonstrate use of new   background prefetch mode of Screen('OpenMovie').
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@344 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychDemos/QuicktimeDemos/PlayMoviesWithoutGapDemoOSX.m
+++ Psychtoolbox/PsychDemos/QuicktimeDemos/PlayMoviesWithoutGapDemoOSX.m
@@ -7,40 +7,37 @@ function PlayMoviesWithoutGapDemoOSX(moviename)
 % directory whose names match the provided pattern, e.g., the '*.mpg'
 % pattern would play all MPEG files in the current directory.
 %
-% This demo uses automatic asynchronous playback for synchronized playback
-% of video and sound. Each movie plays until end, then rewinds and plays
-% again from the start. Pressing the Cursor-Up/Down key pauses/unpauses the
-% movie and increases/decreases playback rate.
-% The left- right arrow keys jump in 1 seconds steps. SPACE jumps to the
-% next movie in the list. ESC ends the demo.
+% Pressing ESC ends the demo.
+%
+% Movies are played one after each other. We try to minimize perceptible
+% gaps between end of movie i and start of movie i+1 by asynchronous
+% loading: While movie i is played back, we ask Psychtoolbox to load the
+% next movie i+1 in the background, so startup time for movie i+1 will be
+% minimized. This feature is currently only supported on MacOS-X and it
+% doesn't work well with movies that have sound tracks. The reason for this
+% is currently unknown, but it leads to frequent lockups of the playback
+% engine. How well this feature works depends strongly on the computational
+% horsepower of your machine. Slow machines will probably stutter...
 %
 % This demo needs MacOS-X 10.3.9 or 10.4.x with Quicktime-7 installed!
 
 % History:
-% 10/30/05  mk  Wrote it.
+% 7/5/06  mk  Wrote it.
+
+% Async movie loading is only OS-X for now...
+AssertOSX;
 
 if nargin < 1
     moviename = '*.mov'
 end;
 
+% Adapt to different keyboard name mapping on Win vs. OS-X:
 if IsWin
-   space=KbName('space');
    esc=KbName('esc');
-   right=KbName('right');
-   left=KbName('left');
-   up=KbName('up');
-   down=KbName('down');
-   shift=KbName('right_shift');
 end;
 
 if IsOSX
-   space=KbName('SPACE');
    esc=KbName('ESCAPE');
-   right=KbName('RightArrow');
-   left=KbName('LeftArrow');
-   up=KbName('UpArrow');
-   down=KbName('DownArrow');
-   shift=KbName('RightShift');
 end;
 
 try
@@ -50,13 +47,10 @@ try
 
     % Open onscreen window:
     screen=max(Screen('Screens'));
-    [win, scr_rect] = Screen('OpenWindow', screen);
+    [win, scr_rect] = Screen('OpenWindow', screen, background);
 
     % Retrieve duration of a single video refresh interval:
     ifi = Screen('GetFlipInterval', win);
-    
-    % Clear screen to background color:
-    Screen('FillRect', win, background);
     
     % Initial display and sync to timestamp:
     vbl=Screen('Flip',win);
@@ -69,33 +63,34 @@ try
     % Playbackrate defaults to 1:
     rate=1;
     
+    % Load first movie. This is a synchronous (blocking) load:
     iteration = 1;
     moviename=moviefiles(mod(iteration, size(moviefiles,1))+1).name;
     [movie movieduration fps imgw imgh] = Screen('OpenMovie', win, moviename);
+
     % Start playback of movie. This will start
     % the realtime playback clock and playback of audio tracks, if any.
-    % Play 'movie', at a playbackrate = 1, with endless loop=1 and
-    % 1.0 == 100% audio volume.
+    % Play 'movie', at a playbackrate = 1, with 1.0 == 100% audio volume.
     Screen('PlayMovie', movie, rate, 0, 1.0);
+
     prefetched=0;
     newmovie = -1;
 
     % Endless loop, runs until ESC key pressed:
     while (abortit<2)
-        % Open movie file and retrieve basic info about movie:
+        % Show basic info about movie:
         fprintf('ITER=%i::', iteration);
         fprintf('Movie: %s  : %f seconds duration, %f fps, w x h = %i x %i...\n', moviename, movieduration, fps, imgw, imgh);
 
         i=0;
 
-        % Asynchronously load the next movie file while current one is
-        % playing. Get moviename of next file:
+        % Get moviename of next file:
         iteration=iteration + 1;
         moviename=moviefiles(mod(iteration, size(moviefiles,1))+1).name;
 
         t1 = GetSecs;
 
-        % Infinite playback loop: Fetch video frames and display them...
+        % Playback loop: Fetch video frames and display them...
         while(1)
             i=i+1;
             if (abs(rate)>0)
@@ -103,32 +98,28 @@ try
                 % time and sound.
                 % tex either the texture handle or zero if no new frame is
                 % ready yet. pts = Presentation timestamp in seconds.
-                %fprintf('PreGet %f\n', GetSecs);
                 [tex pts] = Screen('GetMovieImage', win, movie, 1);
-                %fprintf('PostGet %f\n', GetSecs);
-
+                
                 % Valid texture returned?
                 if tex<0
+                    % No. This means that the end of this movie is reached.
+                    % We exit the loop and prepare next movie:
                     break;
                 end;
 
                 if (tex>0)
-                    % Draw the new texture immediately to screen:
+                    % Yes. Draw the new texture immediately to screen:
                     Screen('DrawTexture', win, tex);
 
                     % Update display:
                     vbl=Screen('Flip', win);
 
                     % Release texture:
-                    %fprintf('PreClose %f\n', GetSecs);
-
                     Screen('Close', tex);
-                    %fprintf('PostClose %f\n', GetSecs);
-
                 end;
             end;
 
-            % Check for abortion:
+            % Check for abortion by user:
             abortit=0;
             [keyIsDown,secs,keyCode]=KbCheck;
             if (keyIsDown==1 & keyCode(esc))
@@ -137,59 +128,30 @@ try
                 break;
             end;
 
-            if (keyIsDown==1 & keyCode(space))
-                % Exit while-loop: This will load the next movie...
-                break;
-            end;
-
-            if (keyIsDown==1 & keyCode(right))
-                % Advance movietime by one second:
-                Screen('SetMovieTimeIndex', movie, Screen('GetMovieTimeIndex', movie) + 1);
-            end;
-
-            if (keyIsDown==1 & keyCode(left))
-                % Rewind movietime by one second:
-                Screen('SetMovieTimeIndex', movie, Screen('GetMovieTimeIndex', movie) - 1);
-            end;
-
-            if (keyIsDown==1 & keyCode(up))
-                % Increase playback rate by 1 unit.
-                if (keyCode(shift))
-                    rate=rate+0.1;
-                else
-                    while KbCheck; WaitSecs(0.01); end;
-                    rate=round(rate+1);
-                end;
-                Screen('PlayMovie', movie, rate, 1, 1.0);
-            end;
-
-            if (keyIsDown==1 & keyCode(down))
-                % Decrease playback rate by 1 unit.
-                if (keyCode(shift))
-                    rate=rate-0.1;
-                else
-                    while KbCheck; WaitSecs(0.01); end;
-                    rate=round(rate-1);
-                end;
-                Screen('PlayMovie', movie, rate, 1, 1.0);
-            end;
-
+            % We start background loading of the next movie 0.5 seconds
+            % after start of playback of the current movie:
             if prefetched==0 & pts > 0.5
                 % Initiate background async load operation:
-                %fprintf('PreLoad %f\n', GetSecs);
-
+                % We simply set the async flag to 1 and don't query any
+                % return values:
                 Screen('OpenMovie', win, moviename, 1);
-                %fprintf('PostLoad %f\n', GetSecs);
-
                 prefetched=1;
             end;
 
+            % If asynchronous load of next movie has been started already
+            % and we are less than 0.5 seconds from the end of the current
+            % movie, then we try to finish the async load operation and
+            % start playback of the new movie in order to give processing a
+            % headstart:
             if prefetched==1 & movieduration - pts < 0.5
                 % Less than 0.5 seconds until end of current movie. Try to
                 % start playback for next movie:
                 
                 % Open the movie - this should take zero-time as the movie
-                % should be already prefetched:
+                % should have been already prefetched during playback of
+                % the current movie. In case loading did not finish, this
+                % command will pause Matlabs execution until the movie is
+                % really ready.
                 [newmovie newmovieduration newfps newimgw newimgh] = Screen('OpenMovie', win, moviename);
                 
                 % Start it:
@@ -197,32 +159,47 @@ try
                 
                 prefetched=2;
             end;
-
         end;
     
         telapsed = GetSecs - t1
         finalcount=i
-
-        while KbCheck; end;
         
-        % Done. Stop playback:
+        % Done with old movie. Stop its playback:
         Screen('PlayMovie', movie, 0);
 
         % Close movie object:
         Screen('CloseMovie', movie);
         
-        % Ok, now our newmovie becomes movie:
+        % Ok, now our 'newmovie' becomes the current 'movie':
         movie = newmovie;
         movieduration = newmovieduration;
         fps = newfps;
         imgw = newimgw;
         imgh = newimgh;
         prefetched = 0;
+        % As playback of the new movie has been started already, we can
+        % simply reenter the playback loop:
     end;
     
-
+    % End of movie playback. Shut down and exit:
+    if prefetched == 1
+        % A prefetch operation for a movie is still in progress. We need to
+        % finalize this cleanly by waiting for the movie to open and then
+        % closing it.
+        [newmovie newmovieduration newfps newimgw newimgh] = Screen('OpenMovie', win, moviename);
+        prefetched = 2;
+    end;
+    
+    if prefetched == 2
+        % Playback of a new prefetched movie has been started. We need to stop and
+        % close it:
+        Screen('PlayMovie', newmovie, 0);
+        Screen('CloseMovie', newmovie);
+        prefetched = 0;
+    end;
+    
     % Close screens.
-    Screen('CloseAll');
+    Screen('Close', win);
 
     % Done.
     return;
