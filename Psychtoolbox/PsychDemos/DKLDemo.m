1c005cd9a0ec69f9c5ec523958586b0613c5a346
kleinerm <mario.kleiner@tuebingen.mpg.de>
2006-08-28 23:25:44 +0000

    CR+LF linefeed cleanup in new files.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@432 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychDemos/DKLDemo.m
+++ Psychtoolbox/PsychDemos/DKLDemo.m
@@ -1,1 +1,126 @@
-% DKLDemo%% Demonstrates the DKL color space routines.% Produces a picture of the DKL isoluminant% plane.  Click mouse to finish.%% You can chooose the image size and what cone/luminance% data you want to use to define the space.%% 4/9/05	dhb		Wrote it.% Clearclear; close all% Define parameters.  Image size should be an odd number.whichScreen = 0;imageSize = 513;whichCones = 'SmithPokorny';% Load spectral data and set calibration fileswitch (whichCones)	case 'SmithPokorny',		load T_cones_sp		load T_xyzJuddVos		S_cones = S_cones_sp;		T_cones = T_cones_sp;		T_Y = 683*T_xyzJuddVos(2,:);		S_Y = S_xyzJuddVos;		T_Y = SplineCmf(S_Y,T_Y,S_cones);	case 'StockmanSharpe'		load T_cones_ss2		load T_ss2000_Y2		S_cones = S_cones_ss2;		T_cones = T_cones_ss2;		T_Y = 683*T_ss2000_Y2;		S_Y = S_ss2000_Y2;		T_Y = SplineCmf(S_Y,T_Y,S_cones);endcal = LoadCalFile(whichScreen);calLMS = SetColorSpace(cal,T_cones,S_cones);calLMS = SetGammaMethod(calLMS,1);calLum = SetColorSpace(cal,T_Y,S_Y);% Define background.  Here we just take the% monitor mid-point.bgLMS = PrimaryToSensor(calLMS,[0.5 0.5 0.5]');% Get matrix that transforms between incremental% cone coordinates and DKL coordinates % (Lum, RG, S).M_ConeIncToDKL = ComputeDKL_M(bgLMS,T_cones,T_Y);M_DKLToConeInc = inv(M_ConeIncToDKL);% Now find incremental cone directions corresponding% to DKL isoluminant directions.rgConeInc = M_DKLToConeInc*[0 1 0]';sConeInc = M_DKLToConeInc*[0 0 1]';% These directions are not scaled in an interesting way,% need to scale them.  Here we'll find units so that % a unit excursion in the two directions brings us to% the edge of the monitor gamut, with a little headroom.bgPrimary = SensorToPrimary(calLMS,bgLMS);rgPrimaryInc = SensorToPrimary(calLMS,rgConeInc+bgLMS)-bgPrimary;sPrimaryInc = SensorToPrimary(calLMS,sConeInc+bgLMS)-bgPrimary;rgScale = MaximizeGamutContrast(rgPrimaryInc,bgPrimary);sScale = MaximizeGamutContrast(sPrimaryInc,bgPrimary);rgConeInc = 0.95*rgScale*rgConeInc;sConeInc = 0.95*sScale*sConeInc;% If we find the RGB values corresponding to unit excursions% in rg and s directions, we should find a) that the luminance% of each is the same and b) that they are all within gamut.% In gamut means that the primary coordinates are all bounded% within [0,1].rgPlusLMS = bgLMS+rgConeInc;rgMinusLMS = bgLMS-rgConeInc;sPlusLMS = bgLMS+sConeInc;sMinusLMS = bgLMS-sConeInc;rgPlusPrimary = SensorToPrimary(calLMS,rgPlusLMS);rgMinusPrimary = SensorToPrimary(calLMS,rgMinusLMS);sPlusPrimary = SensorToPrimary(calLMS,sPlusLMS);sMinusPrimary = SensorToPrimary(calLMS,sMinusLMS);if (any([rgPlusPrimary(:) ; rgMinusPrimary(:) ; ...		sPlusPrimary(:) ; sMinusPrimary(:)] < 0))	fprintf('Something out of gamut low that shouldn''t be.\n');endif (any([rgPlusPrimary(:) ; rgMinusPrimary(:) ; ...		sPlusPrimary(:) ; sMinusPrimary(:)] > 1))	fprintf('Something out of gamut high that shouldn''t be.\n');endbgLum = PrimaryToSensor(calLum,bgPrimary)rgPlusLum = PrimaryToSensor(calLum,rgPlusPrimary)rgMinusLum = PrimaryToSensor(calLum,rgMinusPrimary)sPlusLum = PrimaryToSensor(calLum,sPlusPrimary)sMinusLum = PrimaryToSensor(calLum,sMinusPrimary)lums = sort([bgLum rgPlusLum rgMinusLum sPlusLum sMinusLum]);fprintf('Luminance range in isoluminant plane is %0.2f to %0.2f\n',...	lums(1), lums(end));% Now we have the coordinates we desire, make a picture of the% isoluminant plane.[X,Y] = meshgrid(0:imageSize-1,0:imageSize-1);X = X-(imageSize-1)/2; Y = Y-(imageSize-1)/2;X = X/max(abs(X(:))); Y = Y/max(abs(Y(:)));XVec = reshape(X,1,imageSize^2); YVec = reshape(Y,1,imageSize^2);imageLMS = bgLMS*ones(size(XVec))+rgConeInc*XVec+sConeInc*YVec;[imageRGB,badIndex] = SensorToSettings(calLMS,imageLMS);bgRGB = SensorToSettings(calLMS,bgLMS);imageRGB(:,find(badIndex == 1)) = bgRGB(:,ones(size(find(badIndex == 1))));rPlane = reshape(imageRGB(1,:),imageSize,imageSize);gPlane = reshape(imageRGB(2,:),imageSize,imageSize);bPlane = reshape(imageRGB(3,:),imageSize,imageSize);theImage = zeros(imageSize,imageSize,3);theImage(:,:,1) = rPlane;theImage(:,:,2) = gPlane;theImage(:,:,3) = bPlane;% Show the image.  Click mouse to finish.if (IsOS9)    [w,r] = Screen(whichScreen,'OpenWindow',bgRGB',[],32);    Screen(w,'SetClut',[0:255]'*ones(1,3));    Screen(w,'PutImage',theImage);    Ask(w,'DKL isolumiant plane.  Click mouse to finish.');    Screen(w,'Close');end
+% DKLDemo
+%
+% Demonstrates the DKL color space routines.
+% Produces a picture of the DKL isoluminant
+% plane.  Click mouse to finish.
+%
+% You can chooose the image size and what cone/luminance
+% data you want to use to define the space.
+%
+% 4/9/05	dhb		Wrote it.
+
+% Clear
+clear; close all
+
+% Define parameters.  Image size should be an odd number.
+whichScreen = 0;
+imageSize = 513;
+whichCones = 'SmithPokorny';
+
+% Load spectral data and set calibration file
+switch (whichCones)
+	case 'SmithPokorny',
+		load T_cones_sp
+		load T_xyzJuddVos
+		S_cones = S_cones_sp;
+		T_cones = T_cones_sp;
+		T_Y = 683*T_xyzJuddVos(2,:);
+		S_Y = S_xyzJuddVos;
+		T_Y = SplineCmf(S_Y,T_Y,S_cones);
+	case 'StockmanSharpe'
+		load T_cones_ss2
+		load T_ss2000_Y2
+		S_cones = S_cones_ss2;
+		T_cones = T_cones_ss2;
+		T_Y = 683*T_ss2000_Y2;
+		S_Y = S_ss2000_Y2;
+		T_Y = SplineCmf(S_Y,T_Y,S_cones);
+end
+cal = LoadCalFile(whichScreen);
+calLMS = SetColorSpace(cal,T_cones,S_cones);
+calLMS = SetGammaMethod(calLMS,1);
+calLum = SetColorSpace(cal,T_Y,S_Y);
+
+% Define background.  Here we just take the
+% monitor mid-point.
+bgLMS = PrimaryToSensor(calLMS,[0.5 0.5 0.5]');
+
+% Get matrix that transforms between incremental
+% cone coordinates and DKL coordinates 
+% (Lum, RG, S).
+M_ConeIncToDKL = ComputeDKL_M(bgLMS,T_cones,T_Y);
+M_DKLToConeInc = inv(M_ConeIncToDKL);
+
+% Now find incremental cone directions corresponding
+% to DKL isoluminant directions.
+rgConeInc = M_DKLToConeInc*[0 1 0]';
+sConeInc = M_DKLToConeInc*[0 0 1]';
+
+% These directions are not scaled in an interesting way,
+% need to scale them.  Here we'll find units so that 
+% a unit excursion in the two directions brings us to
+% the edge of the monitor gamut, with a little headroom.
+bgPrimary = SensorToPrimary(calLMS,bgLMS);
+rgPrimaryInc = SensorToPrimary(calLMS,rgConeInc+bgLMS)-bgPrimary;
+sPrimaryInc = SensorToPrimary(calLMS,sConeInc+bgLMS)-bgPrimary;
+rgScale = MaximizeGamutContrast(rgPrimaryInc,bgPrimary);
+sScale = MaximizeGamutContrast(sPrimaryInc,bgPrimary);
+rgConeInc = 0.95*rgScale*rgConeInc;
+sConeInc = 0.95*sScale*sConeInc;
+
+% If we find the RGB values corresponding to unit excursions
+% in rg and s directions, we should find a) that the luminance
+% of each is the same and b) that they are all within gamut.
+% In gamut means that the primary coordinates are all bounded
+% within [0,1].
+rgPlusLMS = bgLMS+rgConeInc;
+rgMinusLMS = bgLMS-rgConeInc;
+sPlusLMS = bgLMS+sConeInc;
+sMinusLMS = bgLMS-sConeInc;
+rgPlusPrimary = SensorToPrimary(calLMS,rgPlusLMS);
+rgMinusPrimary = SensorToPrimary(calLMS,rgMinusLMS);
+sPlusPrimary = SensorToPrimary(calLMS,sPlusLMS);
+sMinusPrimary = SensorToPrimary(calLMS,sMinusLMS);
+if (any([rgPlusPrimary(:) ; rgMinusPrimary(:) ; ...
+		sPlusPrimary(:) ; sMinusPrimary(:)] < 0))
+	fprintf('Something out of gamut low that shouldn''t be.\n');
+end
+if (any([rgPlusPrimary(:) ; rgMinusPrimary(:) ; ...
+		sPlusPrimary(:) ; sMinusPrimary(:)] > 1))
+	fprintf('Something out of gamut high that shouldn''t be.\n');
+end
+bgLum = PrimaryToSensor(calLum,bgPrimary)
+rgPlusLum = PrimaryToSensor(calLum,rgPlusPrimary)
+rgMinusLum = PrimaryToSensor(calLum,rgMinusPrimary)
+sPlusLum = PrimaryToSensor(calLum,sPlusPrimary)
+sMinusLum = PrimaryToSensor(calLum,sMinusPrimary)
+lums = sort([bgLum rgPlusLum rgMinusLum sPlusLum sMinusLum]);
+fprintf('Luminance range in isoluminant plane is %0.2f to %0.2f\n',...
+	lums(1), lums(end));
+
+% Now we have the coordinates we desire, make a picture of the
+% isoluminant plane.
+[X,Y] = meshgrid(0:imageSize-1,0:imageSize-1);
+X = X-(imageSize-1)/2; Y = Y-(imageSize-1)/2;
+X = X/max(abs(X(:))); Y = Y/max(abs(Y(:)));
+XVec = reshape(X,1,imageSize^2); YVec = reshape(Y,1,imageSize^2);
+imageLMS = bgLMS*ones(size(XVec))+rgConeInc*XVec+sConeInc*YVec;
+[imageRGB,badIndex] = SensorToSettings(calLMS,imageLMS);
+bgRGB = SensorToSettings(calLMS,bgLMS);
+imageRGB(:,find(badIndex == 1)) = bgRGB(:,ones(size(find(badIndex == 1))));
+rPlane = reshape(imageRGB(1,:),imageSize,imageSize);
+gPlane = reshape(imageRGB(2,:),imageSize,imageSize);
+bPlane = reshape(imageRGB(3,:),imageSize,imageSize);
+theImage = zeros(imageSize,imageSize,3);
+theImage(:,:,1) = rPlane;
+theImage(:,:,2) = gPlane;
+theImage(:,:,3) = bPlane;
+
+% Show the image.  Click mouse to finish.
+if (IsOS9)
+    [w,r] = Screen(whichScreen,'OpenWindow',bgRGB',[],32);
+    Screen(w,'SetClut',[0:255]'*ones(1,3));
+    Screen(w,'PutImage',theImage);
+    Ask(w,'DKL isolumiant plane.  Click mouse to finish.');
+    Screen(w,'Close');
+end
