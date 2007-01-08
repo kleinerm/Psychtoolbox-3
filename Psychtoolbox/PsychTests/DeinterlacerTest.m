d78f7f4fe630ebf1a34ab09a3244db99dcb9f6be
kleinerm <mario.kleiner@tuebingen.mpg.de>
2007-01-08 00:45:25 +0000

    * A simple GLSL deinterlacing shader (Early alpha quality) and   some demo to demonstrate it. Only tested on ATI X1600 so far...
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@621 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ Psychtoolbox/PsychTests/DeinterlacerTest.m
@@ -0,0 +1,162 @@
+function DeinterlacerTest(sf, imgname, dryrun)
+% DeinterlacerTest([sf][,imgname][,noshader])
+%
+% Tests (and thereby demonstrates) the Psychtoolbox builtin GLSL realtime
+% deinterlaceshader, an OpenGL GLSL Fragmentshader for on-the-fly
+% deinterlacing of video images which are given as PTB textures and drawn
+% via Screen('MakeTexture').
+%
+% Parameters: 'imgname' is optional. If provided, the routine loads an
+% imagefile of that name as test input, otherwise it creates a synthetic
+% test pattern in Matlab.
+%
+% sf - Optional scalefactor, by which the image is scaled in size.
+%
+% dryrun - If set to 1, the shader isn't used, just for comparison.
+%
+% What you should see (assuming you did not provide an input image file):
+%
+% First an image with alternating red- and green color gradients. This is
+% the interlaced test pattern where even rows contain only red, odd rows
+% contain only green gradients.
+%
+% Press a key.
+%
+% Then, by pressing and releasing a key, the image should alternate between
+% an all red image (only red gradients of twice the thickness), and an all
+% green image: This is the alternating deinterlaced even, odd, even, odd
+% ... half-fields of the interlaced image.
+%
+% End the demo by pressing ESCape key twice.
+%
+% If you see something else, or just a black screen, or it aborts with
+% error, then your hardware is probably not capable of running the
+% deinterlace-shader and you're out of luck.
+%
+% Good luck.
+
+
+AssertOpenGL;
+filter=0;
+
+KbName('UnifyKeyNames');
+escape=KbName('ESCAPE');
+
+try
+    screenid = max(Screen('Screens'));
+    [win winrect] = Screen('OpenWindow', screenid, 0);
+
+    if nargin<3
+        dryrun=0;
+    end
+
+    if nargin<1
+        sf = 1;
+    end
+
+    if nargin<2
+        imgname=[];
+    end
+
+    % Image filename provided as input?
+    if isempty(imgname)
+        % Nope. Create synthetic test image:
+        img = uint8(zeros(256,256,3));
+        for y=0:size(img,1)-1
+            for x=1:256
+                % Even row: Red gradient
+                img(y+1, x, mod(y,2) + 1)=(x-1);
+            end
+
+            if mod(floor(y/2),2)==0
+                img(y+1, :, mod(y,2) + 1)=255 - img(y+1, :, mod(y,2) + 1);
+            end
+        end
+    else
+        % Read image from filesystem:
+        img = imread(imgname);
+    end
+
+    for i=1:3
+        img(:,:,i)=transpose(img(:,:,i));
+    end
+    
+    % Build corresponding texture:
+    tex = Screen('MakeTexture', win, img);
+
+    dstrect = Screen('Rect', tex);
+    dstrect = ScaleRect(dstrect, sf, sf);
+    dstrect = CenterRect(dstrect, winrect);
+
+    % Show original (interlaced) image:
+    Screen('DrawTexture', win, tex, [], dstrect, [], filter);
+    Screen('Flip', win);
+
+    % Wait for keypress:
+    KbWait; while KbCheck; end;
+
+    % Load deinterlace-shader:
+    deinterlacer = LoadGLSLProgramFromFiles('EXPDeinterlaceShader',1);
+
+    % Bind and initialize it:
+    glUseProgram(deinterlacer);
+    % Input image will be bound to texture unit zero:
+    glUniform1i(glGetUniformLocation(deinterlacer, 'Image1'), 0);
+    % Get handle for the field selection parameter:
+    useoddfield=glGetUniformLocation(deinterlacer, 'UseOddField');
+
+    if dryrun
+        glUseProgram(0);
+    end
+
+    glFinish;
+    count = 0;
+    tstart = GetSecs;
+
+    % Run until user presses ESCape or for 1000 iterations.
+    while count < 1000
+        % Show the even half-field: Tell deinterlacer we want the even field:
+        if ~dryrun
+            glUniform1f(useoddfield, 0);
+        end
+
+        % Draw it:
+        Screen('DrawTexture', win, tex, [], dstrect, [], filter);
+        Screen('Flip', win, 0, 0, 0);
+
+        % Wait for keypress:
+        KbWait; while KbCheck; end;
+
+        % Show the odd half-field: Tell deinterlacer we want the odd field:
+        if ~dryrun
+            glUniform1f(useoddfield, 1);
+        end
+
+        % Draw it:
+        Screen('DrawTexture', win, tex, [], dstrect, [], filter);
+        Screen('Flip', win, 0, 0, 0);
+
+        % Wait for keypress:
+        KbWait;
+        [dummy dummy keycode]=KbCheck;
+        if keycode(escape)
+            break;
+        end
+        while KbCheck; end;
+
+        count = count + 1;
+    end
+    glFinish;
+    telapsed = GetSecs - tstart
+    tavg = telapsed / count / 2 * 1000
+    hz = count / telapsed * 2
+
+    % Done. Disable deinterlacer:
+    glUseProgram(0);
+
+    % Shut down:
+    Screen('CloseAll');
+catch
+    Screen('CloseAll');
+    psychrethrow(psychlasterror);
+end
