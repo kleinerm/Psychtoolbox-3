cfc0b27e6eb98a4a25156e921e8d67e0969daf9c
kleinerm <mario.kleiner@tuebingen.mpg.de>
2007-04-16 23:42:16 +0000

    * This Bits++ test works on MS-Windows with Geforce 7800.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@743 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychHardware/BitsPlusToolbox/BitsPlusTests/BitsPlusImagingTest.m
+++ Psychtoolbox/PsychHardware/BitsPlusToolbox/BitsPlusTests/BitsPlusImagingTest.m
@@ -20,12 +20,12 @@ Screen('Preference', 'Verbosity', 5);
 
 % First load the graphics hardwares gamma table with an identity mapping,
 % so it doesn't interfere with Bits++
-LoadIdentityClut(whichScreen);
+LoadIdentityClut(window);
 
 % Now enable finalizer hook chains and load them with the special Bits++
 % command. 
 Screen('HookFunction', window, 'PrependBuiltin', 'LeftFinalizerBlitChain', 'Builtin:RenderClutBits++', ''); 
-%Screen('HookFunction', window, 'Enable', 'LeftFinalizerBlitChain');
+Screen('HookFunction', window, 'Enable', 'LeftFinalizerBlitChain');
 
 % This only on quad-buffered stereo contexts:
 %Screen('HookFunction', window, 'PrependBuiltin', 'RightFinalizerBlitChain', 'Builtin:RenderClutBits++', ''); 
@@ -40,26 +40,22 @@ for colorval = linspace(0.0, 1.0, 20)
     % The setting 2 means: Don't load hardware gamma table, but just
     % store clut for later use by the special Bits++ blitter at
     % Screen('Flip') time.
-    % Screen('LoadNormalizedGammaTable', window, uniclut, 2);
+    Screen('LoadNormalizedGammaTable', window, uniclut, 2);
 
-    
-    texturePtr = BitsPlusClut2Texture(window, uniclut * 255);
-    Screen('DrawTexture', window, texturePtr, [], Screen('Rect', texturePtr), [], 0);
-    
-    %BitsPlusSetClut(window, uniclut*16384);
     % Show it. Shows stimulus and updates CLUT by drawing the T-Lock stuff
     % into top left corner of display.
     Screen('Flip', window);
 
-    if KbCheck
-      %  break;
-    end
-    
     KbWait;
     while KbCheck; end;
 end
 
 KbWait;
+
+linear_lut =  repmat(linspace(0, 1, 256)', 1, 3);
+Screen('LoadNormalizedGammaTable', window, linear_lut, 2);
+Screen('Flip', window);
+
 
 % Disable Bits++ encoders: This will revert the behaviour of CLUT handling
 % to normal...
@@ -67,7 +63,4 @@ Screen('HookFunction', window, 'Disable', 'LeftFinalizerBlitChain');
 Screen('HookFunction', window, 'Disable', 'RightFinalizerBlitChain');
 
 % Close the window.
-Screen(window,'Close');
-
-% Blank the screen
-BitsPlusBlank(whichScreen);
+Screen('CloseAll');
