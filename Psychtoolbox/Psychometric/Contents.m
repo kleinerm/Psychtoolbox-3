c9731c983666ff7a19fb96a43a7b672cd2a01588
brainard <brainard@cattell.psych.upenn.edu>
2007-08-03 01:13:10 +0000

    Add Naka-Rushton function support, except that I forgot to type svn add first.
    DHB
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@820 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/Psychometric/Contents.m
+++ Psychtoolbox/Psychometric/Contents.m
@@ -4,6 +4,7 @@
 %
 %  AreaUnderROC         - Compute the area under a ROC function.
 %  ComputeLogistic      - Compute value of logistic function.
+%  ComputeNakaRushton   - Compute values of the Naka-Rushton function.
 %  ComputeOptimalBeta   - Compute the optimal decision criterion for Normal.
 %  ComputeWeibTAFC      - Compute Weibull for TAFC psychometric functions.
 %  ComputeWeibYN        - Compute Weibull for Y/N psychometric functions.
@@ -16,6 +17,8 @@
 %  FitCumNormYN         - Fit comulative normal for YN experiments (uses optimization toolbox).
 %  FitLogistic          - Fit a logistic function.
 %  FitLogitYN           - Fit a logistic function for YN experiments.
+%  FitNakeRushton       - Fit a Naka-Rushton function to data.
+%  FitNakaRushtonFun    - Error function for FitNakaRushton.
 %  FitWeibAlphTAFC      - Fit Weibull with beta fixed, for TAFC (uses optimization toolbox).
 %  FitWeibTAFC          - Fit Weibull for TAFC experiments (uses optimization toolbox).
 %  FitWeibYN            - Fit Weibull for YN experiments (uses optimization toolbox).
@@ -26,8 +29,8 @@
 %  NormLogLikelyRatio   - Comptue log likelihood ratio for two Normal distributions.
 %  NormalROC            - Generate the ROC curve for a normal distribution.
 %  SimWeibTAFCObs       - Simulate a Weibull observer.
-%  WeibAlphTAFCFitFun   - 
-%  WeibTAFCFitFun       - 
-%  WeibYNFitFun         - 
+%  WeibAlphTAFCFitFun   - Error function for FitWeibAlphTAFC.
+%  WeibTAFCFitFun       - Error function for FitWeibTAFC.
+%  WeibYNFitFun         - Error function for FitWeibYN.
 
-% Copyright (c) 1996 by David Brainard & Denis Pelli
+% Copyright (c) 1996-2007 by David Brainard & Denis Pelli
