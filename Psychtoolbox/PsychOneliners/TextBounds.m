ad61581848a2fd88eaaf5d388d12d40e63ce0f0a
kleinerm <mario.kleiner@tuebingen.mpg.de>
2007-12-22 00:17:18 +0000

    * Improvements to TextBounds command and test script TextBoundsTest.   -> Should be more efficient and robust now, and demonstrate a few nifty things.
    -> Safe for beta.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@940 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOneliners/TextBounds.m
+++ Psychtoolbox/PsychOneliners/TextBounds.m
@@ -10,63 +10,39 @@ function bounds=TextBounds(w,text)
 % contain all the strings. The prior contents of the scratch window
 % are lost. Usually it should be an offscreen window, so the user
 % won't see it. The scratch window should be at least twice as wide
-% as the text, because the text is drawn starting at the left-right
-% center of the window to cope with uncertainties about text
-% direction (e.g. Hebrew) and some unusual characters that extend
+% and height as the text, because the text to cope with uncertainties
+% about text direction (e.g. Hebrew) and some unusual characters that extend
 % greatly to the left of their nominal starting point. If you only
 % know your nominal text size and number of characters, you might do
 % this to create your scratch window:
 % 
 % textSize=24;
 % string='Hello world.';
-% if IsOSX
-%     w=Screen('OpenWindow',0);
-% end
-% if IsOS9
-%     w=Screen(-1,'OpenOffscreenWindow',[],[0 0 3*textSize*length(string) 2*textSize],1);
-% end
-% Screen(w,'TextFont','Arial');
-% Screen(w,'TextSize',textSize);
-% Screen(w,'TextStyle',1); % 0=plain (default for new window), 1=bold, etc.
-% bounds=TextBounds(w,string);
+% With 'w' being the handle of the onscreen window, e.g., w=Screen('OpenWindow',0,0);
+% woff=Screen(w,'OpenOffscreenWindow',[],[0 0 3*textSize*length(string) 2*textSize]);
+% Screen(woff,'TextFont','Arial');
+% Screen(woff,'TextSize',textSize);
+% Screen(woff,'TextStyle',1); % 0=plain (default for new window), 1=bold, etc.
+% bounds=TextBounds(woff,string);
 % ...
-% Screen(w,'Close');
+% Screen(woff,'Close');
 % 
 % The suggested window size in that call is generously large because there
 % aren't any guarantees from the font makers about how big the text might
-% be for a specified point size. The pixelSize of 1 (the last argument)
-% minimizes the memory requirements. Set your window's font, size, and 
+% be for a specified point size. Set your window's font, size, and 
 % (perhaps) style before calling TextBounds. 
 % 
 % Be warned that TextBounds and TextCenteredBounds are slow (taking many
 % seconds) if the window is large. They use the whole window, so if the
 % window is 1024x1204 they process a million pixels. The two slowest calls
 % are Screen 'GetImage' and FIND. Their processing time is proportional to
-% the number of pixels in the window. We haven't checked, but it's very
-% likely that processing time is also proportional to pixelSize, so we
-% suggest using a small pixelSize (e.g. 1 bit, using an offscreen
-% window).
+% the number of pixels in the window.
 % 
 % OSX: Also see Screen 'TextBounds'.
-% 
-% OS9: If you just want a quick and dirty "TextHeight", analogous to  
-% the existing Screen 'TextWidth', you might follow Charles Collin's 
-% (ccollin@dal.ca) advice, and approximate the height of text by the width 
-% of two e's, which "usually works well enough."
-% 
-% textHeight=Screen(w,'TextWidth','ee'); % OS9
-% 
-% Screen 'TextWidth' calls Apple's TextWidth. The name 'TextWidth' is 
-% slightly misleading. TextWidth does NOT tell you about the
-% bounding box. It tells you how much the pen position is advanced by
-% drawing the character. The character (e.g. a capital letter in a script
-% font) may in fact extend to the left of the original starting point and
-% to the right of the final end point.
-% 
+%  
 % The user interface would be cleaner if this function opened and closed
 % its own offscreen window, instead of writing in the user's window. 
-% Unfortunately the Mac OS takes on the order of a second to open and to 
-% close an offscreen window, making the overhead prohibitive.
+% Unfortunately, this might cause some prohibitive overhead.
 % 
 % Also see TextCenteredBounds.
 
@@ -80,38 +56,35 @@ function bounds=TextBounds(w,text)
 % 1/18/05  dgp Added Charles Collin's two e suggestion for textHeight.
 % 1/28/05  dgp Cosmetic.
 % 2/4/05   dgp Support both OSX and OS9.
+% 12/22/07 mk  Significant rewrite to adapt to current PTB-3.
 
-Screen(w,'FillRect',0);
-r=Screen(w,'Rect');
-x0=round((r(RectLeft)+r(RectRight))/2);
-y0=round((r(RectTop)+2*r(RectBottom))/3);
+% Clear scratch window to background color black:
+Screen('FillRect',w,0);
+
+% Draw text strings, always with the top-left corner of text bounding box
+% in top-left corner of window:
 if iscell(text)
 	for i=1:length(text)
 		string=char(text(i));
-        if IsOS9
-            Screen(w,'DrawText',string,x0,y0,255);
-        end
-        if IsOSX
-            Screen('DrawText',w,string,x0,y0,255);
-        end
-	end
+        Screen('DrawText',w,string,0,0,255, [], 0);
+    end
 else
 	for i=1:size(text,1)
 		string=char(text(i,:));
-        if IsOS9
-            Screen(w,'DrawText',string,x0,y0,255);
-        end
-        if IsOSX
-            Screen('DrawText',w,string,x0,y0,255);
-        end
+        Screen('DrawText',w,string,0,0,255, [], 0);
 	end
 end
-% Screen(w,'DrawText','',x0,y0); % Set pen position to x0 y0.
-image1=Screen(w,'GetImage');
-[y,x]=find(image1(:,:,1));
+
+% Read back only 1 color channel for efficiency reasons:
+image1=Screen('GetImage', w, [], 'backBuffer', 0, 1);
+
+% Search non-zero (==non background) pixels:
+[y,x]=find(image1(:,:));
+
+% Compute their bounding rect and return it:
 if isempty(y) | isempty(x)
 	bounds=[0 0 0 0];
 else	
 	bounds=SetRect(min(x)-1,min(y)-1,max(x),max(y));
-	bounds=OffsetRect(bounds,-x0,-y0);
 end
+return;
--- Psychtoolbox/PsychTests/TextBoundsTest.m
+++ Psychtoolbox/PsychTests/TextBoundsTest.m
@@ -1,66 +1,153 @@
-function TextBoundsTest
-% TextBoundsTest
+function TextBoundsTest(string, font, textSize, rotAngle)
+% TextBoundsTest([string] [, font] [, textSize] [, rotAngle])
 %
-% Test Screen('TestBounds')
+% Test Screen('TextBounds') as a fast way of determining the bounding
+% rectangle (aka bounding box) of a string of text, as well as the very
+% precise but slow TextBounds.m function.
 %
-% OS9: TextBounds will accept an on- or offscreen window pointer.
-% OSX: TextBounds accepts only an onscreen window.
-% OSX: Screen 'TextBounds' provides width and height, not position.
+% Screen('TextBounds') uses information from the operating system about the
+% bounding polygons of a string of text to compute the bounding box. This
+% is fast, but it relies on the typesetting algorithms of the OS and the
+% information encoded in a font definition file playing well with each
+% other. Usually that's the case, but there are a few special fonts which
+% either don't encode their own boundaries correctly, or which are typeset
+% in an unusual way that the OS facilities can't handle correctly. In such
+% cases, textbounds will be way to big or too small and text may appear cut
+% off.
+%
+% The TextBounds() function implemented in TextBounds.m uses a different
+% approach: It draws the text string into a provided window - usually an
+% offscreen window which is so big that it can easily accomodate the text
+% string in question, ie., it is way too big for the string. Then it reads
+% back the image of that string and computes the exact bounding box of the
+% non-background pixels. This method is obviously independent of OS
+% facilities and (faulty) information in the font files - thereby highly
+% robust and accurate. However the approach is very slow, so you should
+% only use this method if you really need it.
+%
+% This test script both tests functioning of both methods and provides a
+% code example of how to use the information returned by both methods to
+% either frame drawn text, or to actually draw unusual difficult text by
+% first predrawing it into an offscreen window of sufficient size, then
+% blitting the really "meaningful" portions of that offscreen window into
+% the real image window.
+%
+% Optional parameters: (All have meaningful defaults)
+% 'string' The text string to draw. Default: "Wordy"
+% 'font' Name of the font definition file. Default: "Chicago"
+% 'textSize' Size of text font. Default: 64 pts.
+% 'rotAngle' Orientation of test-blitted text. Default: 45 degrees.
+%
 
 % 2/3/05    dgp Wrote it.
-% 10/26/05  awi Cosmetic 
+% 10/26/05  awi Cosmetic
+% 12/22/07  mk  Mostly rewrote it, trying to keep the original intentions.
 
-try;
+if nargin < 1
+    string = [];
+end
+
+if isempty(string)
+    string='Wordy';
+end    
+
+if nargin < 2
+    font = [];
+end
+
+if isempty(font)
+    font = 'Chicago';
+end
+
+if nargin < 3
+    textSize = [];
+end
+
+if isempty(textSize)
+    textSize = 64;
+end
+
+if nargin < 4
+    rotAngle = [];
+end
+
+if isempty(rotAngle)
+    rotAngle = 45;
+end
+
+try
     red=[255 0 0];
     green=[0 255 0];
     black=0;
-    textSize=64;
-    string='Wordy';
-    if IsOSX
-        w=Screen('OpenWindow',0); % Doesn't do FillRect
-        Screen('FillRect',w);
-        Screen('Flip',w);
-	end
     if IsOS9
 		%         w=Screen(-1,'OpenOffscreenWindow',[],[0 0 3*textSize*length(string) 2*textSize],1);
         w=Screen(0,'OpenWindow');
-	end
-    Screen(w,'TextFont','Chicago');
+    else
+        % PTB-3 OS/X, Windows, Linux:
+        
+        % Open a onscreen window for display:
+        w=Screen('OpenWindow',0,0);
+        
+        % Open a offscreen window for use as scratchpad of TextBounds. We
+        % make it big enough to certainly contain the text, ie., twice the
+        % expected height and 3x the expected width. However make sure its
+        % not bigger than the whole screen by clipping it against the
+        % screens size:
+        maxrect = ClipRect([0 0 3*textSize*length(string) 2*textSize], Screen('Rect', w));
+        woff = Screen('OpenOffscreenWindow', w, [], maxrect);
+    end
+    
+    % Set text font and size for onscreen window:
+    Screen(w,'TextFont',font);
     Screen(w,'TextSize',textSize);
-    bounds=TextBounds(w,string);
+    % Set text font and size for offscreen window:
+    Screen(woff,'TextFont',font);
+    Screen(woff,'TextSize',textSize);
+    % Compute text bounds with accurate but slow TextBounds.m function:
+    bounds=TextBounds(woff,string);
     fprintf('TextBounds says the bounds of ''%s'' are [%d %d %d %d].\n',string,bounds);
+
+    % Select target rectangle on screen for text display:
     x=100;
     y=200;
-    bounds=OffsetRect(bounds,x,y);
+    dstbounds=OffsetRect(bounds,x,y);
     if IsOS9
 		Screen(w,'FillRect');
-		Screen(w,'FillRect',red,bounds);
+		Screen(w,'FillRect',red,dstbounds);
         Screen(w,'DrawText',string,x,y);
-	end
-    if IsOSX
-		Screen('FillRect',w);
-		Screen('FillRect',w,red,bounds);
+    else
+        % Fill target area with a red rectangle -- red background:
+        Screen('FillRect',w,red,dstbounds);
+        % Draw text directly over the red rectangle:
         Screen('DrawText',w,string,x,y,black);
-		Screen(w,'DrawText','Hit any key to continue',x,y+400,black);
-	end
-    if IsOSX
+        
+        % Draw the same text by blitting the relevant area of the 'woff'
+        % offscreen window to the screen. As 'TextBounds' used white text
+        % on black background internally, we'll see that onscreen:
+        dstbounds = OffsetRect(bounds, x, y+200);
+        Screen('DrawTexture', w, woff, bounds, dstbounds, rotAngle);
+        
+        % Draw help text:
+		Screen(w,'DrawText','Hit any key to continue',x,y+400,red);
+        Screen('Flip', w);
+        
+        while KbCheck; end;
+        KbWait;
+        
         y=y+200;
         bounds=Screen('TextBounds',w,string);
         fprintf('Screen ''TextBounds'' says the bounds of ''%s'' are [%d %d %d %d].\n',string,bounds);
         bounds=OffsetRect(bounds,x,y);
         Screen('FillRect',w,red,bounds);
         Screen('DrawText',w,string,x,y,black);
-        Screen('DrawText',w,'Hit any key to continue',x,y+200,black);
+        Screen('DrawText',w,'Hit any key to continue',x,y+200,red);
         Screen('Flip',w);
 	end
+    while KbCheck; end;
     KbWait;
-    while KbCheck
-		;
-	end
-	Screen(w,'Close');
-catch;
-    clear screen
-    if exist('lasterr')
-		lasterr
-	end
+
+    Screen('CloseAll');
+catch
+    Screen('CloseAll');
+    psychrethrow(psychlasterror);
 end
