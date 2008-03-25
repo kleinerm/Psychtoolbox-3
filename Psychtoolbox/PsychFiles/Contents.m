4fc053d411997e66134b5873c7bc51b76b44d4d6
kleinerm <mario.kleiner@tuebingen.mpg.de>
2008-03-25 23:26:32 +0000

    Commit for Revision 1000 -- Such a shabby and boring commit :-(
    Files renamed, Contents.m adapted:
    
    sendmail.m renamed to psychsendmail.m,
    SaveAsEps.m renamed to PsychSaveAsEps.m
    
    -> Both names were clashing with new Matlab functions of
       the same name.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1000 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychFiles/Contents.m
+++ Psychtoolbox/PsychFiles/Contents.m
@@ -5,10 +5,10 @@
 %   BreakLines              - Break string into lines and terminators. 
 %   FindFolder              - Find a folder on MATLAB path.
 %   LiteralUnderscore       - Insert escape ("\") before each underscore char in string.
-%   QuoteString		    - Wraps a string in quotes.
+%   QuoteString		        - Wraps a string in quotes.
 %   ReadStructsFromText     - Read struct array from a tab delimited text file.
 %   ReplaceLineTerminators  - Strip and replace line terminators.  
-%   SaveAsEps               - Save matrix as an EPS file.
+%   PsychSaveAsEps          - Save matrix as an EPS file.
 %   WriteStructsToText      - Write a struct array to a tab delimited text file.
 
 % Copyright (c) 1996-2003 by Denis Pelli & David Brainard; 
