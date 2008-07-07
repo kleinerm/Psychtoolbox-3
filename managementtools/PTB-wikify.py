bf379e904b90074dab6646b0f4c0c22ac8a449d6
kleinerm <mario.kleiner@tuebingen.mpg.de>
2008-07-07 20:29:43 +0000

    * Add PTB-wikify.py Python script written by Tobias Wolf.
      This script runs over the PTB distribution tree, parses
      it, and creates/updates the online documentation at
      http://docs.psychtoolbox.org Wiki.
    
    This is the current version, just with the Password for
    the Wiki stripped for obvious reasons ;-)
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1078 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ managementtools/PTB-wikify.py
@@ -0,0 +1,434 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008 Tobias Wolf <towolf@tuebingen.mpg.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+"""
+PTB-wikify.py: Submit help strings in .m-files to a Wikka Installation
+
+Usage: PTB-wikify.py [options] ([m-files]|[directory])
+
+Options:
+ -h, --help
+ -u ..., --username ...   login to WikkaWiki with this UserName
+ -p ..., --password ...   use this Wikka password
+ -U ..., --URL            root URL of the Wiki in the form
+                            http://doc.psychtoolbox.org/ (w/ terminal slash)
+ -r,  --recursive         recursive mode: use only with a single directory
+ -m,  --mexmode           mex mode: also look for .mexglx files and post their help-
+                            strings by calling MATLAB and running them
+                            In recursive mode both M and Mex files are posted.
+                            (edit the source to change _mexext into one of 
+                             .mexglx, .mexmaci, or .dll)
+ -v                       be verbose (massive text output)
+                            this prints out:
+                              - which files were skipped
+                              - a diff of the text before submission
+ -f   --full-diff         output full ndiff (defaults to terse unified)
+                          ndiff contains all text but is easier 
+                          to parse visually for differences.
+                          (only in combination with -v)
+
+Examples:
+  PTB-wikify.py -u DocBot -p dokkbot -U http://wiki/ PsychBasic/*.m 
+  PTB-wikify.py -r -m -U http://wiki/ PsychBasic/
+
+IMPORTANT!!:
+  Always change your working directory to the root of the 
+  tree before running the script, e.g., 
+    cd Psychtoolbox
+    ~/PTB-wikify.py -r PsychDemos/PsychExampleExperiments
+
+   (This is to figure out what the root node is supposed to be.)
+
+"""
+
+import sys, os, re
+import getopt
+from urllib2 import HTTPError
+
+import mechanize
+assert mechanize.__version__ >= (0, 0, 6, "a")
+
+import textwrap
+
+baseurl = "http://docs.psychtoolbox.org/wikka.php?wakka="
+username = "DocBot"
+password = ""
+_recursive = 0
+_mexmode = 0
+_mexext = '.mexmaci'
+_debug = 0
+_fulldiff = 0
+
+global mech
+mech = mechanize.Browser()
+
+def parse(filename):
+    '''Parse helpful initial comment block from .m-file'''
+    docstring = []
+    functionellipsized = False
+    for line in open(filename,'rU'):
+        # weird hack
+        if line.strip()[0:8] + line.strip()[-3:] == 'function...':
+            functionellipsized = True
+        if line.strip().startswith('%'):       # get comment lines
+            docstring.append(line)
+        elif len(docstring): break             # and stop after one block
+            # we allow empty lines and the function def before the block
+        elif len(line.strip()) > 0 \
+                and not line.strip().startswith('function')\
+                and not functionellipsized: break
+
+    # strip first two chars: '% '
+    return textwrap.dedent(''.join([l[1:] for l in docstring]))
+
+def beackern(mkstring):
+    '''Regex cleaning of PTB docstrings'''
+    # expand tabs to four spaces
+    mkstring = mkstring.expandtabs(4)
+    # Enclose text markup characters with: ""
+    mkstring = re.sub(r'(\+\+|(--){2}\b|==|\'\'|@@|[^:]//|>>|<<|##)',r'""\1""',mkstring)
+    # Replace 'Windows: ______' with headline
+    mkstring = re.sub(r'(?m)^(.*):\s*[_-]{3,}',r'=== \1 ===',mkstring)
+    # Replace long _______ with ---- i.e.<HR>
+    mkstring = re.sub(r'(?m)^\s*_{10,}',r'----',mkstring)
+    # Replace some non-WikiWord function to internal links
+    match = r'(\bScreen\b|' \
+            + r'\bmoglmorpher\b|' \
+            + r'\bFlip\b|' \
+            + r'\bRush\b|' \
+            + r'\bIOPort\b|' \
+            + r'\bGestalt\b|' \
+            + r'\bClose\b|' \
+            + r'\bSnd\b|' \
+            + r'\bPsychometric\b|' \
+            + r'\bPreference\b)'
+    mkstring = re.sub(match,r'[[\1]]',mkstring)
+    # purge useless help lines
+    mkstring = re.sub(r'(?m)^.*triple-click me & hit enter.*$\n','',mkstring)
+    # Generate more headlines (Word:$, CAPITAL LETTERS$, etc)
+    mkstring = re.sub(r'(?m)((?<=\n\n).+:((?=\s*\n\n)+))', r'=== \1 ===',mkstring)
+    mkstring = re.sub('(?m)((?<=\n\n)\ *[A-Z][A-Z :!-]+\ *(?=\n))', r'=== \1 ===',mkstring)
+    return mkstring
+
+def login(baseurl,username,password):
+    try:
+        mech.open(baseurl + "UserSettings")
+    except HTTPError, e:
+        sys.exit("%d: %s" % (e.code, e.msg))
+    
+    # important: use correct form number in the HTML page
+    mech.select_form(nr=1)
+    mech["name"] = username
+    mech["password"] = password
+    try:
+        mech.submit()
+    except HTTPError, e:
+        sys.exit("login failed: %d: %s" % (e.code, e.msg))
+
+def makediff(old,new):
+    import difflib, time
+    if not _fulldiff:
+        dm = difflib.unified_diff
+    else: 
+        dm = difflib.ndiff
+    old = re.sub('\015\012',r'\n',old).expandtabs(4)
+    diff = dm(old.splitlines(1), new.splitlines(1))
+    difftext = ''.join(diff)
+
+    return difftext
+
+def post(title,text):
+    '''Browse to the Wiki, select the form, diff it, and post text'''
+    title = re.sub("[^\w]|_","",title)
+    try:
+        resp = mech.open(baseurl+title+"/edit")
+    except HTTPError, e:
+        sys.exit("post failed: %d: %s" % (e.code, e.msg))
+    
+    mech.select_form(nr=1)
+    try:
+        oldbody = mech["body"]
+    except:
+        print 'No id="body" form. Figure this out first. cf. page text above.'
+        for form in mech.forms():
+                print form
+        sys.exit("post failed while processing page: " + baseurl + title +'/edit')
+
+    # manual override when text contains <!--protected-->
+    if oldbody.find('<!--protected-->')>0:
+        print 'skipped protected page ' + title
+        return
+    # generate diff and output the diffstat
+    diff = makediff(oldbody,text)
+    if diff:
+        minus = str([l.startswith('-') for l in diff.splitlines() \
+                if not l.startswith('---')].count(True))
+        plus = str([l.startswith('+') for l in diff.splitlines() \
+                if not l.startswith('+++')].count(True))
+        if plus == '0' and  minus == '0':
+            changes = ' (nc+)'
+        else:
+            changes = ' ('+plus+' insertions(+), '+minus+' deletions(-))'
+
+        print "posting: " + title + changes
+        if _debug: print diff
+    else:
+        print "posting: " + title + ' (nc)'
+
+    mech["body"] = text
+    try:
+        mech.submit()
+    except HTTPError, e:
+        sys.exit("post failed: %d: %s" % (e.code, e.msg))
+
+def postsinglefiles(files):
+    ''' create pages for all files in there and link to the category '''
+    for name in files:
+        # single out some names
+        head, basename = os.path.split(name)
+        root =  os.path.join(os.path.basename(os.getcwd()))
+        path =   os.path.normpath(os.path.join(root,head))
+        funcname, ext = os.path.splitext(basename)
+        category = os.path.basename(head)
+        cattext = ''
+
+        if not os.path.exists(name) or not ext=='.m':
+            if _mexmode and ext == _mexext:
+                mexhelpextract([funcname])
+            elif _debug: print 'skipping ' + name
+            continue
+        if basename=='Contents.m':
+            funcname = category
+            path = os.path.dirname(path)
+            if len(path) < 3:
+                continue
+            cattext = '{{category}}'
+
+        # load and build the text for the page
+        headline = "===[[" + funcname + "]]===\n"
+        breadcrumb = "==[[" + re.sub("/","]] &#8250; [[", path) + "]]==\n\n"
+
+        # read the .m-file and strip all the crap
+        docstring = parse(name)
+
+        # scrub the text real good, to get us some nice wiki formatting
+        if docstring:
+            body = beackern(docstring)
+        else: 
+            body = 'Adrian, this function is not yet documented.\n\n\n MissingDocs'
+
+        text = headline \
+                + breadcrumb \
+                + body \
+                + '\n\n\n%%(php;Path)' \
+                + os.path.join(path,basename) \
+                + '%%\n' \
+                + cattext
+
+        post(funcname,text)
+
+def mexhelpextract(mexnames):
+    print 'processing mex files: ' + mexnames.__repr__()
+    from ConfigParser import RawConfigParser as ConfigParser, Error as error
+    for mexname in mexnames:
+        # ConfigParser for the three elements per subfunctions written to tmpdir
+        # [SubFunction]
+        # usage: 'xyz'
+        # help: 'xyz'
+        # seealso: 'xyz'
+        config = ConfigParser({'usage':[], 'help':[], 'seealso':[]})
+        # assemble command line for matlab
+        matlabcmd = 'addpath(\'%s\');%s(\'%s\',\'%s\'); exit' % \
+            (_tmpdir, \
+             os.path.splitext(os.path.basename(_mexscript))[0], \
+             mexname, \
+             _tmpdir)
+        cmd = 'matlab -nodisplay -r "%s" > /dev/null' % matlabcmd
+        # and execute matlab w/ the temporary script we wrote earlier
+        try:
+            print 'running MATLAB for %s in %s' % (mexname,_tmpdir)
+            stdin, stderr = os.popen4(cmd)
+            print stderr.read()
+            stdin.close()
+            stderr.close()
+        except: print 'could not dump help for %s into %s' % (mexname,_tmpdir)
+
+        cfgfile = config.read(os.path.join(_tmpdir,mexname))
+        if cfgfile == []:
+            print "skipping " + mexname + " (no output)"
+            continue
+        subfunctions = config.sections()
+        print 'processing subfunctions: ' + subfunctions.__repr__()
+        for subfunction in subfunctions:
+            # read in the strings for this subfunction
+            usage = config.get(subfunction,'usage')
+            help = config.get(subfunction,'help')
+            seealso = config.get(subfunction,'seealso')
+
+            headline = '===[['+subfunction+' '+mexname+'(\''+subfunction+'\')]]===\n'
+            breadcrumb = "==[[Psychtoolbox]] &#8250; [[" \
+                                + mexname + "]].{mex*,dll} subfunction==\n\n"
+
+            # scrub the text for main text only
+            body = beackern(help)
+
+            docstring = '' \
+                    + '%%(matlab;Usage)' \
+                    + usage \
+                    + '%%\n\n' \
+                    + body \
+                    + '\n\n'
+            if seealso:
+                docstring = docstring + '<<=====See also:=====\n' + seealso + '<<'
+
+            text = headline \
+                    + breadcrumb \
+                    + docstring \
+                    #+ '\n\n\n%%(php;Path)' \
+                    #+ os.path.join(path,basename) \
+                    #+ '%%\n' \
+                    #+ cattext
+
+            post(subfunction,text)
+
+def recursivewalk(rootfolder):
+    '''traverse the root directory and post all .m-files'''
+    for root, dirs, files in os.walk(rootfolder):
+
+        if re.search(os.sep+'\.svn|'+os.sep+'private',root):
+                continue
+        print 'Entering folder ' + root
+
+        # .m-files are processed with postsinglefiles
+        mfiles = [os.path.join(root,f) for f in files \
+                if f[-2:] in ('.m','.M')]
+        postsinglefiles(mfiles)
+
+        # mex files are processed via matlab
+        if _mexmode:
+            mexnames = [os.path.splitext(f)[0] for f in files \
+                    if f[-len(_mexext):].lower() == _mexext]
+            mexhelpextract(mexnames)
+
+    print "Exiting: Done with this tree."
+    sys.exit(0)
+
+def usage():
+    print __doc__
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:U:frmv", \
+            ["help", "username=", "password=", "url=", "full-diff", "mexmode", "recursive"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-u", "--username"):
+            global username
+            username = arg
+        elif opt in ("-p", "--password"):
+            global password
+            password = arg
+        elif opt in ("-U", "--url"):
+            global baseurl
+            baseurl = arg
+        elif opt in ("-f", "--full-diff"):
+            global _fulldiff
+            _fulldiff = 1
+        elif opt in ("-r", "--recursive"):
+            global _recursive
+            _recursive = 1
+        elif opt in ("-m", "--mexmode"):
+            import tempfile
+            global _mexmode, _mexext, _tmpdir, _mexscript
+            _mexmode = 1
+            _tmpdir = tempfile.mkdtemp(prefix="PTB-doc-mexfunctionhelp-")
+            _mexscript = os.path.join(_tmpdir, 'PsychtoolboxMexhelpextract.m')
+            fid = open(_mexscript, 'w')
+            script = '''\
+            function PsychtoolboxMexhelpextract(mexname,tmpdir)
+            try
+                subfunctions=eval([mexname '(''DescribeModulefunctionshelper'')']);
+            catch
+                return;
+            end
+
+            try
+                if isempty(subfunctions)
+                    return;
+                end
+
+                if ~iscell(subfunctions)
+                    return;
+                end
+            catch
+                return;
+            end
+            
+            fid = fopen(fullfile(tmpdir,mexname),'wt');
+            if fid == -1
+                return;
+            end
+
+            try
+                for i=1:size(subfunctions,2)
+                    fprintf(fid,'[%s]\\n\\n',subfunctions{i});
+                    docs = eval([mexname '(''DescribeModulefunctionshelper'',1,subfunctions{i})']);
+                    fprintf(fid,'usage: %s\\n\\n',docs{1});
+                    fprintf(fid,'help: %s\\n\\n',regexprep(docs{2},'\\n','\\n  '));
+                    fprintf(fid,'seealso: %s\\n\\n',docs{3});    
+                end
+            catch
+                % Nothing to do.
+            end
+            
+            fclose(fid);
+            return
+            '''
+            fid.write(textwrap.dedent(script))
+            fid.close()
+            
+        elif opt == '-v':
+            global _debug
+            _debug = 1
+
+    login(baseurl,username,password)
+
+    if args:
+        if _recursive:
+            if len(args) == 1 and os.path.isdir(args[0]):
+                recursivewalk(args[0])
+            else:
+                usage()
+                print 'Error: Recursive mode works with one directory only\n\n'
+                sys.exit(1)
+
+        else:
+            postsinglefiles(args)
+    else:
+        usage()
+        print 'Error: No files specified to submit to Wiki\n\n'
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
