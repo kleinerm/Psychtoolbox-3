63a7b2619664debf36f8e50b4cfdb613abeabaaf
flerfs <chrg@sas.upenn.edu>
2008-12-23 18:48:14 +0000

    Small adjustment of y chromaticity for CIE C in MunsellGetxyY.  New value matches what we looked up on the CIE Colorimetry standard.
    DHB, IJK
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1180 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetric/PsychMunsell/MunsellGetxyY.m
+++ Psychtoolbox/PsychColorimetric/PsychMunsell/MunsellGetxyY.m
@@ -8,12 +8,13 @@ function [xyY,Xx,trix,vx,Xy,triy,vy,XY,triY,vY] = MunsellGetxyY(angle,value,chro
 %
 % For chroma below 2.0, we interpolate x,y,Y for chroma of 2, and
 % then adjust x,y by linear interpolation between chroma 0 and 2,
-% using the chromaticity of a zero chroma sample as (0.3101, 0.3163).
+% using the chromaticity of a zero chroma sample as (0.3101, 0.3162).
 % This is the chromaticity of illuminant C.  We think this is right.
 %
 % 11/21/08  dhb, ijk  Wrote it.
 % 12/01/08  ijk       Added interpolation for low chroma samples.
 %           ijk, dhb  Tried to make it go fast by allowing precomputation.
+% 12/22/08  ijk, dhb  Change y chromaticity of CIE C to .3162, to match standard (was .3163).
 
 % Thank heavens Matlab provides n-d interpolation, so we don't have to write it.
 if (nargin == 4)
@@ -26,7 +27,7 @@ if (nargin == 4)
         [y,Xy,triy,vy] = MunsellGriddata3(munsellData(:,1),munsellData(:,2),munsellData(:,3),munsellData(:,5),angle,value,2,'linear');
         [Y,XY,triY,vY] = MunsellGriddata3(munsellData(:,1),munsellData(:,2),munsellData(:,3),munsellData(:,6),angle,value,2,'linear');
         x = 0.3101+(chroma*((x-0.3101)/2));
-        y = 0.3163+(chroma*((y-0.3163)/2));
+        y = 0.3162+(chroma*((y-0.3162)/2));
     end
 else
     if(chroma >= 2.000)
@@ -44,7 +45,7 @@ else
         y = MunsellGriddata3([],[],[],vy,angle,value,2,'linear',[],Xy,triy);
         Y = MunsellGriddata3([],[],[],vY,angle,value,2,'linear',[],XY,triY);
         x = 0.3101+(chroma*((x-0.3101)/2));
-        y = 0.3163+(chroma*((y-0.3163)/2));
+        y = 0.3162+(chroma*((y-0.3162)/2));
     end
 end
 xyY = [x y Y]';
