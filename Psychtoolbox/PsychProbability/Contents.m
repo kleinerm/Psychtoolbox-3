c8f9917c53976f46f8b10b1f3a134d2524bf1a4d
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-02-14 17:54:48 +0000

    * Contents.m files updated.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1234 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychProbability/Contents.m
+++ Psychtoolbox/PsychProbability/Contents.m
@@ -15,7 +15,7 @@
 %   CoinFlipPDF         - Bernoulli distribution PDF.
 %   ColMean             - Take column means, works for one row matrices.
 %   ColStd              - Take column stds, works for one row matrices.
-%   combvec             - Generate all possible combinations of input vectors.
+%   CombVec             - Generate all possible combinations of input vectors.
 %   CovToCorr           - Compute matrix of correlations from covariance.
 %   FindNeighborCorr    - Find nearest neighbor correlations in data set.
 %   MultiNormalDraw     - Draw vectors from multivariate normal.
@@ -24,33 +24,33 @@
 %   NormalDraw          - Draw from univariate normal.
 %   NormalPDF           - Univariate normal PDF.
 %   NormalProb          - Compute probability of univariate normal in interval.
-%   nrandperm           - Randomly select elements from a random permutation.
-%   randdim             - Randomize matrix along any dimension.
+%   NRandPerm           - Randomly select elements from a random permutation.
+%   RandDim             - Randomize matrix along any dimension.
 %   Randi               - Get random integer sample. Same as Ranint, but Denis prefers it.
-%   randlim             - Generates a matrix of random numbers between a lower and upper limit.
+%   RandLim             - Generates a matrix of random numbers between a lower and upper limit.
 %   RandSample          - Get a random sample from a list.
-%   randsel             - Randomly select elements from matrix, elements are replenished.
+%   RandSel             - Randomly select elements from matrix, elements are replenished.
 %   Ranint              - Get random integer sample. Same as Randi, but David prefers it.
 %   Sample              - Get a random sample from a list.
 %   Shuffle             - Randomly reorder the entries of vector/matrix.
-%   uniqueandn          - Returns unique elements of array and the number of times each element occurs in the array.
-%   uniquenosorting     - Returns elements of array in order of appearance or disappearance.
-%   urandsel            - Randomly select elements from matrix, elements are not replenished.
+%   UniqueAndN          - Returns unique elements of array and the number of times each element occurs in the array.
+%   UniqueNoSorting     - Returns elements of array in order of appearance or disappearance.
+%   URandSel            - Randomly select elements from matrix, elements are not replenished.
 %
 %
-%   Combining COMBVEC and RANDDIM
-%   Using COMBVEC and RANDDIM, it is very simple to generate all trials
+%   Combining CombVec and RandDim
+%   Using CombVec and RandDim, it is very simple to generate all trials
 %   in an experiment with a factorial design and then randomize these
 %   trials:
 %     cond_a = [1 2]; cond_b = [120 180]; ntrial = 2;
-%     trial  = combvec(cond_a,cond_b);
+%     trial  = CombVec(cond_a,cond_b);
 %     trial  =
 %          1     2     1     2
 %        120   120   180   180
 %
 %     trial           = repmat(trial,1,ntrial);   % each combination of conditions occurs ntrial times
 %     trial(end+1,:)  = [1:size(trial,2)];        % append trial number
-%     trial           = randdim(trial,2);         % shuffle trial order
+%     trial           = RandDim(trial,2);         % shuffle trial order
 %
 %     trial =
 %          2     1     1     2     1     1     2     2
--- Psychtoolbox/PsychSignal/Contents.m
+++ Psychtoolbox/PsychSignal/Contents.m
@@ -2,26 +2,27 @@
 %
 % help Psychtoolbox % For an overview, triple-click me & hit enter.
 %
-%   allall               - Apply function ALL to all elements of an N-D matrix.
-%   altround             - Round elements to the nearest multiple of a factor.
-%   anyall               - Apply function ANY to all elements of an N-D matrix.
-%   atand2               - Inverse tangent function which output is in the range -180--180 degrees, instead of MATLAB's -90--90.
-%   atand360             - Inverse tangent function which output is in the range 0--360 degrees, instead of MATLAB's -90--90.
+%   AllAll               - Apply function ALL to all elements of an N-D matrix.
+%   AltRound             - Round elements to the nearest multiple of a factor.
+%   AnyAll               - Apply function ANY to all elements of an N-D matrix.
+%   ATand2               - Inverse tangent function which output is in the range -180--180 degrees, instead of MATLAB's -90--90.
+%   ATand360             - Inverse tangent function which output is in the range 0--360 degrees, instead of MATLAB's -90--90.
 %   Bandpass             - Filter images, one dimension.
 %   Bandpass2            - Filter images, two dimensions, radial frequency.
+%   BoolFromSNR          - Creates a boolean (selection) vector from an SNR (Signal-to-Noise Ratio)
 %   ComputeRMSE          - Compute root mean squared error.
 %   Expand               - Expand a matrix.
 %   HalfRect             - Half wave rectification.
 %   MatMax               - Maximum in entire matrix.
 %   MatMin               - Minimum in entire matrix.
 %   MatMinMax            - Max and min in entire matrix.
-%   maxall               - Apply function MAX to all elements of an N-D matrix.
-%   maxind               - Finds matrix' maximum and returns indices to all occurrences of it. 
-%   meanall              - Apply function MEAN to all elements of an N-D matrix.
-%   minall               - Apply function MIN to all elements of an N-D matrix.
-%   minind               - Finds matrix' minimum and returns indices to all occurrences of it. 
+%   MaxAll               - Apply function MAX to all elements of an N-D matrix.
+%   MaxInd               - Finds matrix' maximum and returns indices to all occurrences of it. 
+%   MeanAll              - Apply function MEAN to all elements of an N-D matrix.
+%   MinAll               - Apply function MIN to all elements of an N-D matrix.
+%   MinInd               - Finds matrix' minimum and returns indices to all occurrences of it. 
 %   OrientationBandpass  - Filter images, two dimensions, orientation.
 %   Scale                - Affine scaling puts data in range [0,1].
-%   stdall               - Apply function STD to all elements of an N-D matrix.
-%   sumall               - Apply function SUM to all elements of an N-D matrix.
+%   StdAll               - Apply function STD to all elements of an N-D matrix.
+%   SumAll               - Apply function SUM to all elements of an N-D matrix.
 %   Trunc                - Truncate a matrix into range [0,1].
