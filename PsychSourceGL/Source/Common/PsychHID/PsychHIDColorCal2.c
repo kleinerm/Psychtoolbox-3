f625dca097dbbeab7505b56b761b0e8ff35374ae
flerfs <chrg@sas.upenn.edu>
2009-02-24 02:44:15 +0000

    - Woops, forgot to add a source file.
    	CGB
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1255 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ PsychSourceGL/Source/Common/PsychHID/PsychHIDColorCal2.c
@@ -0,0 +1,51 @@
+/*
+ *  PsychHIDColorCal2.c
+ *  PsychToolbox
+ *
+ *  Created by Christopher Broussard on 2/23/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "PsychHID.h"
+
+static char useString[] = "output = PsychHID('ColorCal2', 'command', [varargin])";
+static char synopsisString[] = "Controls the ColorCal 2 USB device.";
+static char seeAlsoString[] = "";
+
+// Global ColorCal 2 device object.
+IOUSBDeviceInterface **g_ColorCal2Device = NULL;
+
+
+PsychError PSYCHHIDColorCal2(void)
+{
+	char *command;
+	
+	// Setup the help features.
+	PsychPushHelp(useString, synopsisString, seeAlsoString);
+    if (PsychIsGiveHelp()) {
+		PsychGiveHelp();
+		return PsychError_none;
+	}
+	
+	// Makes sure that the number of input arguments contains at least 1 command.
+	PsychErrorExit(PsychRequireNumInputArgs(1));
+	
+	// Grab the command.
+	PsychAllocInCharArg(1, TRUE, &command);
+	
+	// Open the ColorCal 2 device if it isn't already.
+	if (g_ColorCal2Device == NULL) {
+		if (PSYCHHIDColorCal2OpenDevice() == false) {
+			PsychErrMsgTxt("Failed to open the ColorCal 2 device.  Make sure it is plugged in.");
+		}
+	}
+	
+	if (PsychMatch(command, "thing")) {
+	}
+	else {
+	}
+		
+	
+	return PsychError_none;
+}
