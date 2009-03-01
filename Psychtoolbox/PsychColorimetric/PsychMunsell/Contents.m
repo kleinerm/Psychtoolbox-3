2ab88762a704a21492af20d7aac1299d4b66a19b
flerfs <chrg@sas.upenn.edu>
2009-03-01 01:23:28 +0000

    Updated contents
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1261 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetric/PsychMunsell/Contents.m
+++ Psychtoolbox/PsychColorimetric/PsychMunsell/Contents.m
@@ -5,6 +5,7 @@
 %
 %   MunsellAngleToHue   - Convert a hue angle to symbolic Munsell hue.
 %   MunsellConversionTest - Test program to see if this all works OK.
+%   MunsellGetxyY       - Munsell space to xyY conversion (CIE illuminant C).
 %   MunsellGriddata3    - Local version of griddata3 that allows for precomputing slow stuff.
 %   MunsellHueToAngle   - Convert symbolic MunsellHue to hue angle.
 %   MunsellPreprocessTable - Get table of Munsell renotation to xyY (under illuminant C).
