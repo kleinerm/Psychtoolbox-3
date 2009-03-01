9164f4cd3d68cc34c18167d845bcd8bd971c88dc
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-03-01 01:27:34 +0000

    * CreateProceduralGabor() functions accepts a new flag   'disableNorm': This allows to get rid of the additional   normalization term in our procedural gabor shaders.
      This term is very useful for normalization, but not
      standard in a typical def. of a gabor function, so we
      allow to optionally disable that term if users wish
      to do so and want to implement their own normalization.
    
    => Associated vertex shaders updated.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1263 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychGLImageProcessing/CreateProceduralGabor.m
+++ Psychtoolbox/PsychGLImageProcessing/CreateProceduralGabor.m
@@ -1,8 +1,19 @@
-function [gaborid, gaborrect] = CreateProceduralGabor(windowPtr, width, height, nonSymmetric, backgroundColorOffset)
-% [gaborid, gaborrect] = CreateProceduralGabor(windowPtr, width, height [, nonSymmetric=0][, backgroundColorOffset =(0,0,0,0)])
+function [gaborid, gaborrect] = CreateProceduralGabor(windowPtr, width, height, nonSymmetric, backgroundColorOffset, disableNorm)
+% [gaborid, gaborrect] = CreateProceduralGabor(windowPtr, width, height [, nonSymmetric=0][, backgroundColorOffset =(0,0,0,0)][, disableNorm=0])
 %
 % Creates a procedural texture that allows to draw Gabor stimulus patches
 % in a very fast and efficient manner on modern graphics hardware.
+%
+% This works on GPU's with support for the OpenGL shading language and
+% vertex- and fragment shaders. See ProceduralGaborDemo and
+% ProceduralGarboriumDemo for examples on how to use this function.
+% ProceduralGaborDemo shows drawing of a single gabor and also allows to
+% perform a speed benchmark and a correctness test to verify correct
+% working and accuracy of this approach. ProceduralGarboriumDemo shows how
+% to draw large numbers of gabor patches with different paramters in a very
+% fast and efficient way.
+%
+% Parameters and their meaning:
 %
 % 'windowPtr' A handle to the onscreen window.
 % 'width' x 'height' The maximum size (in pixels) of the gabor. More
@@ -21,38 +32,74 @@ function [gaborid, gaborrect] = CreateProceduralGabor(windowPtr, width, height, 
 % color to add to the final RGBA colors of the drawn gabor, prior to
 % drawing it.
 %
+% 'disableNorm' Optional, defaults to 0. If set to a value of 1, the
+% special multiplicative normalization term normf = 1/(sqrt(2*pi) * sc)
+% will not be applied to the computed gabor. By default (setting 0), it
+% will be applied. This term seems to be a reasonable normalization of the
+% total amplitude of the gabor, but it is not part of the standard
+% definition of a gabor. Therefore we allow to disable this normalization.
+%
+%
 % The function returns a procedural texture handle 'gaborid' that you can
 % pass to the Screen('DrawTexture(s)', windowPtr, gaborid, ...) functions
 % like any other texture handle. The 'gaborrect' is a rectangle which
 % describes the size of the gabor support.
 %
-% A typical invocation to draw a gabor patch looks like this:
+% A typical invocation to draw a single gabor patch may look like this:
 %
 % Screen('DrawTexture', windowPtr, gaborid, [], dstRect, Angle, [], [],
 % modulateColor, [], kPsychDontDoRotation, [phase+180, freq, sc,
-% contrast]);
+% contrast, aspectratio, 0, 0, 0]);
 %
-% Draws the gabor 'gaborid' into window 'windowPtr', at position 'dstRect'
-% or in the center if dstRect is set to []. Make sure 'dstRect' has the
+% Draws the gabor 'gaborid' into window 'windowPtr', at position 'dstRect',
+% or in the center if 'dstRect' is set to []. Make sure 'dstRect' has the
 % size of 'gaborrect' to avoid spatial distortions! You could do, e.g.,
 % dstRect = OffsetRect(gaborrect, xc, yc) to place the gabor centered at
-% screen position (xc,yc). 'Angle' is the optional orientation angle,
-% default is zero degrees. 'modulateColor' is the base color of the gabor
-% patch - it defaults to white, ie. the gabor has only luminance, but no
-% color. If you'd set it to [255 0 0] you'd get a reddish gabor. 'phase' is
-% the phase of the gabors sine grating, 'freq' is its spatial frequency,
-% 'sc' is the spatial constant of the gaussian hull function, 'contrast' is
-% the contrast of your gabor.
+% screen position (xc,yc).
+%
+% The definition of the gabor mostly follows the definition of Wikipedia,
+% but you can check the source code of ProceduralGaborDemo for a reference
+% Matlab implementation which is exactly equivalent to what this routine
+% does.
+%
+% Wikipedia's definition (better readable): http://en.wikipedia.org/wiki/Gabor_filter
+% See http://tech.groups.yahoo.com/group/psychtoolbox/message/9174 for
+% Psychtoolbox forum message 9174 with an in-dephs discussion of this
+% function.
 %
 %
-% Make sure to use the Screen('DrawTextures', ...); function properly to
-% draw many different gabors simultaneously - this is much faster!
+% 'Angle' is the optional orientation angle in degrees (0-360), default is zero degrees.
+%
+% 'modulateColor' is the base color of the gabor patch - it defaults to
+% white, ie. the gabor has only luminance, but no color. If you'd set it to
+% [255 0 0] you'd get a reddish gabor.
+%
+% 'phase' is the phase of the gabors sine grating in degrees.
+%
+% 'freq' is its spatial frequency in cycles per pixel.
+%
+% 'sc' is the spatial constant of the gaussian hull function of the gabor, ie.
+% the "sigma" value in the exponential function.
+%
+% 'contrast' is the amplitude of your gabor in intensity units - A factor
+% that is multiplied to the evaluated gabor equation before converting the
+% value into a color value. 'contrast' may be a bit of a misleading term
+% here...
+%
+% 'aspectratio' Defines the aspect ratio of the hull of the gabor. This
+% parameter is ignored if the 'nonSymmetric' flag hasn't been set to 1 when
+% calling the CreateProceduralGabor() function.
+%
+% Make sure to use the Screen('DrawTextures', ...); function properly,
+% instead of the Screen('DrawTexture', ...); function, if you want to draw
+% many different gabors simultaneously - this is much faster!
 %
 % 
 
 % History:
 % 11/25/2007 Written. (MK)
 % 01/03/2008 Enable support for asymmetric gabor shading. (MK)
+% 03/01/2009 Add 'disableNorm' flag to allow disabling of normalization term (MK).
 
 debuglevel = 1;
 
@@ -79,6 +126,14 @@ else
     end
 end
 
+if nargin < 6 || isempty(disableNorm)
+    disableNorm = 0;
+end
+
+if ~isscalar(disableNorm) || ~ismember(disableNorm, [0, 1])
+    error('The "disableNorm" flag must be 0 or 1. Invalid flag passed!');
+end
+
 if ~nonSymmetric
     % Load standard symmetric support shader - Faster!
     gaborShader = LoadGLSLProgramFromFiles('BasicGaborShader', debuglevel);
@@ -89,10 +144,16 @@ end
 
 % Setup shader:
 glUseProgram(gaborShader);
+
 % Set the 'Center' parameter to the center position of the gabor image
 % patch [tw/2, th/2]:
 glUniform2f(glGetUniformLocation(gaborShader, 'Center'), width/2, height/2);
 glUniform4f(glGetUniformLocation(gaborShader, 'Offset'), backgroundColorOffset(1),backgroundColorOffset(2),backgroundColorOffset(3),backgroundColorOffset(4));
+
+% Assign disable flag for normalization:
+glUniform1f(glGetUniformLocation(gaborShader, 'disableNorm'), disableNorm);
+
+% Setup done:
 glUseProgram(0);
 
 % Create a purely virtual procedural texture 'gaborid' of size width x height virtual pixels.
