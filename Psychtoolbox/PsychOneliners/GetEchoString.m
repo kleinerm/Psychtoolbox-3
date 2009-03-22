f1bef5bf6448fc32330ebd4d2b66efc02591a7c3
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-03-22 23:19:14 +0000

    * Bugfix and enhancements for GetEchoString(), contributed   by Todd Horowitz.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1289 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOneliners/GetEchoString.m
+++ Psychtoolbox/PsychOneliners/GetEchoString.m
@@ -1,5 +1,5 @@
 function string = GetEchoString(windowPtr, msg, x, y, textColor, bgColor)
-% string = GetEchoString(window, msg, x, y, textColor, bgColor)
+% string = GetEchoString(window, msg, x, y, [textColor], [bgColor])
 % 
 % Get a string typed at the keyboard. Entry is terminated by
 % <return> or <enter>.
@@ -20,29 +20,41 @@ function string = GetEchoString(windowPtr, msg, x, y, textColor, bgColor)
 % 2/28/98   dgp       Use GetChar instead of obsolete GetKey. Use SWITCH and LENGTH.
 % 3/27/98   dhb       Put an abs around char in switch.
 % 12/26/08  yaosiang  Port GetEchoString from PTB-2 to PTB-3.
+% 03/20/08  tsh       Added FlushEvents at the start and made bgColor and
+%                     textcolor optional
 
-% Write the message and a space
-Screen('DrawText', windowPtr, [msg '  '], x, y, textColor, bgColor);
+if nargin < 6
+    bgColor = [];
+end
+
+if nargin < 5
+    textColor = [];
+end
+
+% Flush GetChar queue to remove stale characters:
+FlushEvents('keyDown');
+
+% Write the message
+Screen('DrawText', windowPtr, msg, x, y, textColor, bgColor);
 Screen('Flip', windowPtr);
 
 string = '';
 while true
-  char = GetChar;    
-  switch (abs(char))
-    case {13, 3, 10}
-      break;
-    case 8
-      if length(string) > 0
-        string = string(1:length(string)-1);
-      end
-      output = [msg string];
-      Screen('DrawText', windowPtr, output, x, y, textColor, bgColor);
-      Screen('Flip', windowPtr);
-    otherwise
-      string = [string, char];
-  end
+    char = GetChar;
+    switch (abs(char))
+        case {13, 3, 10}
+            % ctrl-C, enter, or return
+            break;
+        case 8
+            % backspace
+            if ~isempty(string)
+                string = string(1:length(string)-1);
+            end
+        otherwise
+            string = [string, char];
+    end
 
-  output = [msg string];
-  Screen('DrawText', windowPtr, output, x, y, textColor, bgColor);
-  Screen('Flip', windowPtr);
+    output = [msg, ' ', string];
+    Screen('DrawText', windowPtr, output, x, y, textColor, bgColor);
+    Screen('Flip', windowPtr);
 end
