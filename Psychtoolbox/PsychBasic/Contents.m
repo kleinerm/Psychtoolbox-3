2aa79b7b39f58c64acc684999166a311b694b414
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-06-01 20:49:45 +0000

    * New function PsychWatchDog.m - Octave specific for now:
      Implements watchdog mechanism. The watchdog, once enabled
      by usercode by calls to PsychWatchDog, gets active whenever
      Octave is in interactive mode, e.g., waiting for keypress
      or input via input() command, or at the command prompt,
      e.g., after termination of a script by user action, regular
      end or error.
    
      It tests about every 0.1 secs if the user presses CTRL + .,
      or another configurable key combo, as well as for some
      settable timeout. On timeout or key combo press, it closes
      all open onscreen windows etc. via call to sca;
    
    => Useful for debugging as "emergency stop" or automatic
       error handler.
    
    => Makes us of Octave specific functionality, unsupported by
       Matlab.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1403 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychBasic/Contents.m
+++ Psychtoolbox/PsychBasic/Contents.m
@@ -48,6 +48,7 @@
 %     PsychCV              - Miscellaneous C routines for computer vision and related stuff.
 %     PsychtoolboxDate     - Current version date, e.g. '1 August 1998'
 %     PsychtoolboxVersion  - Current version number, e.g. 2.32
+%     PsychWatchDog        - Watchdog mechanism and error handler for Psychtoolbox.
 %     RestrictKeysForKbCheck - Restrict operation of KbCheck et al. to a subset of keys on the keyboard.
 %     Screen               - Control the video display. ** Type "Screen" for a list. ** 
 %     SetMouse             - Set mouse position.
