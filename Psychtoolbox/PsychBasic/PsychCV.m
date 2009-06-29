48574dab0cf9ff26a31fa13cb299697f567072ed
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-06-29 21:15:49 +0000

    * Screen.mexmac for OS/X + Matlab on PowerPC rebuilt. * New file PsychCV.m with help for PsychCV and proper AssertMex   handling for missing PsychCV mex file.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1439 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ Psychtoolbox/PsychBasic/PsychCV.m
@@ -0,0 +1,16 @@
+function PsychCV(varargin)
+% PsychCV is a MEX file for computer-vision applications. PsychCV has
+% many functions; type "PsychCV" for a list:
+% 	PsychCV
+%
+% Please note that PsychCV is only supported on recent platforms, e.g.,
+% Matlab versions 7.4 (R2007a) and later or Octave-3 on Windows, and only
+% on Intel based Macintosh computers under OS/X but not on PowerPC
+% machines.
+%
+
+fprintf('Please note that PsychCV is only supported on some recent platforms.\n');
+fprintf('If you get some MEX file not found error, then your hardware/operating system/Matlab\n');
+fprintf('version is not supported by PsychCV, because it is too old and outdated.\n\n');
+
+AssertMex('PsychCV.m');
