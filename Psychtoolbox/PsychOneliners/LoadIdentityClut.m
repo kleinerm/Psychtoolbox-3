4773cf3a1a22ac75f90334895473fcf70e5477c0
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-09-08 20:39:21 +0000

    * LoadIdentityClut.m: Updated with workarounds for the   latest flaws in OS/X 10.5.8 and 10.6.0 wrt. loading of   identity gamma tables on different NVidia post G80   GPU's.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1515 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOneliners/LoadIdentityClut.m
+++ Psychtoolbox/PsychOneliners/LoadIdentityClut.m
@@ -12,6 +12,7 @@ function oldClut = LoadIdentityClut(windowPtr, loadOnNextFlip)
 % History:
 % ??/??/??   mk  Written.
 % 05/31/08   mk  Add code to save backup copy of LUT's for later restore.
+% 09/09/09   mk  Add more LUT workarounds for Apple's latest f$%#%$#.
 
 global ptb_original_gfx_cluts;
 
@@ -23,6 +24,13 @@ end
 % If not specified, we'll set the clut to load right away.
 if nargin == 1
     loadOnNextFlip = 0;
+end
+
+% Query OS type and version:
+if IsOSX
+    % Which OS/X version?
+    compinfo = Screen('Computer');
+    osxversion = sscanf(compinfo.system, '%*s %*s %i.%i.%i');
 end
 
 % Query what kind of graphics hardware is
@@ -42,18 +50,24 @@ if ~isempty(strfind(gfxhwtype, 'NVIDIA'))
     
     % Is it a Geforce-8000 or later (G80 core or later) and is this OS/X?
     if ~isempty(strfind(winfo.GPUCoreId, 'G80')) & IsOSX %#ok<AND2>
-        % Which OS/X version?
-        compinfo = Screen('Computer');
-        osxversion = sscanf(compinfo.system, '%*s %*s %i.%i.%i');
-
-        % 10.5.0 or later?
-        if (osxversion(1) == 10) & (osxversion(2) >= 5) & (osxversion(3) >=0) %#ok<AND2>
+        % 10.5.x Leopard?
+        if (osxversion(1) == 10) & (osxversion(2) == 5) & (osxversion(3) >=0) %#ok<AND2>
             % Yes. One of the releases with an embarassing amount of bugs,
             % brought to you by Apple. Need to apply an especially ugly
             % clut to force these cards into an identity mapping:
-            fprintf('NVidia Geforce 8000 or later on OS/X 10.5 or later detected. Enabling special LUT hacks for totally broken operating systems.\n');
+            fprintf('NVidia Geforce 8000 or later on OS/X 10.5.x detected. Enabling special type-I LUT hacks for totally broken operating systems.\n');
             gfxhwtype = 2;
         end
+        
+        % 10.6.x Snow Leopard?
+        if (osxversion(1) == 10) & (osxversion(2) >= 6) & (osxversion(3) >=0) %#ok<AND2>
+            % Yes. One of the releases with an embarassing amount of bugs,
+            % brought to you by Apple. Need to apply an especially ugly
+            % clut to force these cards into an identity mapping:
+            fprintf('NVidia Geforce 8000 or later on OS/X 10.6.x detected. Enabling special type-II LUT hacks for totally broken operating systems.\n');
+            gfxhwtype = 3;
+        end
+        
     end
 else
     if ~isempty(strfind(gfxhwtype, 'ATI')) | ~isempty(strfind(gfxhwtype, 'AMD')) %#ok<OR2>
@@ -77,7 +91,8 @@ end
 
 if gfxhwtype == 1
     % This works on OS/X 10.4.9 on Intel MacBookPro with ATI Mobility
-    % Radeon X1600: We assume this is the correct setup for ATI hardware:
+    % Radeon X1600, also on 10.5.8 with ATI Radeon HD-2600: We assume this
+    % is the correct setup for ATI hardware:
     oldClut = Screen('LoadNormalizedGammaTable', windowPtr, ((1/256:1/256:1)' * ones(1, 3)), loadOnNextFlip);
 end
 
@@ -88,12 +103,25 @@ if gfxhwtype == 2
     % have a functioning Quality control department, or are they all
     % fully occupied fiddling with their iPhones?!?
 
-    % Sort of ok:lut = zeros(1, 1024);
-    % nogood:    luti = (0:1/255:1) + 0.001;
+    % This works on 10.5.8 with Geforce-9200M in the MacMini according to
+    % CRS, and with 10.5.8 with Geforce-8800  in the MacPro according to
+    % me. We assume this works on all G80 et al. GPU's:
+    loadlut = (linspace(0,(1023/1024),1024)' * ones(1, 3));    
+    oldClut = Screen('LoadNormalizedGammaTable', windowPtr, loadlut, loadOnNextFlip);
+end
 
-    % better:
-    luti = (1/256:1/256:1) - 0.003;
-    loadlut = luti' * ones(1, 3);    
+if gfxhwtype == 3
+    % This works on OS/X 10.6.0 with NVidia Geforce-9200M according to CRS
+    % and with 10.5.8 with Geforce-8800  in the MacPro according to
+    % me. We assume this works on all G80 et al. GPU's:
+    for i=0:1023
+        if ((i / 1024.0) < 0.5)
+            loadlut(i+1) = (i / 1024.0);
+        else
+            loadlut(i+1) = (i / 1024.0) - (1.0/256.0);
+        end
+    end
+    loadlut = loadlut' * ones(1, 3);
     oldClut = Screen('LoadNormalizedGammaTable', windowPtr, loadlut, loadOnNextFlip);
 end
 
