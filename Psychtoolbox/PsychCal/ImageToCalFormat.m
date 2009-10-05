ce8e269ddf43423519d2e853ccae5b51c27364e5
flerfs <chrg@sas.upenn.edu>
2009-10-05 14:52:48 +0000

    - Improvements to the help text for ImageToCalFormat and CalFormatToImage.
    	DHB
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1530 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/ImageToCalFormat.m
+++ Psychtoolbox/PsychCal/ImageToCalFormat.m
@@ -1,19 +1,24 @@
-function [calFormat,n,m] = ImageToCalFormat(image)
-% [calFormat,n,m] = ImageToCalFormat(image)
+function [calFormat,nX,nY] = ImageToCalFormat(image)
+% [calFormat,nX,nY] = ImageToCalFormat(image)
 %
-% Take an m by n by 3 image and convert it
-% to a format that may be used to Psychtoolbox
+% Take an nX (col dimension) by nY (row dimension) by k image
+% and convert it to a format that may be used to Psychtoolbox
 % calibration routines.
+%
+% Note that the order nX,nY makes sense for thinking about images
+% in terms of x and y coordinates, but that the order is backwards
+% from the MATLAB convention of row dim then column dim.
 %
 % See also CalFormatToImage
 %
-% 8/04/04	dhb		Wrote it.
-% 7/16/07   dhb     Update help line.
+% 8/04/04  dhb  Wrote it.
+% 7/16/07  dhb  Update help line.
+% 10/2/09  dhb  Try again on making help clear.
 
-[m,n,k] = size(image);
+[nY,nX,k] = size(image);
 
-if m*n == 1
-	calFormat = squeeze(reshape(image,m*n,1,k));
+if nY*nX == 1
+	calFormat = squeeze(reshape(image,nY*nX,1,k));
 else
-	calFormat = squeeze(reshape(image,m*n,1,k))';
+	calFormat = squeeze(reshape(image,nY*nX,1,k))';
 end
