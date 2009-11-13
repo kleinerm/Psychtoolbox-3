b30dbfcdf9a9f346888b93c1dc2ff79b5441ef57
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-11-13 03:48:10 +0000

    * LoadIdentityClut.m: The Radeon HDxxx GPU's (R600 et al.) also   need type-0 LUT's under Linux, just as under Windows-Vista.
      Yet another workaround for broken drivers :-(
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1569 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOneliners/LoadIdentityClut.m
+++ Psychtoolbox/PsychOneliners/LoadIdentityClut.m
@@ -148,8 +148,8 @@ else
             % A good default at least on OS/X is type 1:
             gfxhwtype = 1;
 
-            if IsWin & ~isempty(strfind(winfo.GPUCoreId, 'R600')) %#ok<AND2>
-                % At least the Radeon HD 3470 under Windows Vista needs type 0
+            if (IsWin | IsLinux) & ~isempty(strfind(winfo.GPUCoreId, 'R600')) %#ok<OR2,AND2>
+                % At least the Radeon HD 3470 under Windows Vista and Linux needs type 0
                 % LUT's. Let's assume for the moment this is true for all R600
                 % cores, ie., all Radeon HD series cards.
                 fprintf('ATI Radeon HD-2000 or later on MS-Windows detected. Enabling special type-0 LUT hacks for totally broken drivers.\n');
