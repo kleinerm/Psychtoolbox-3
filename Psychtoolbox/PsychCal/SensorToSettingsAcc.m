a6eceef27e07f1730e5cd83aacf9209e5995fd6f
flerfs <chrg@sas.upenn.edu>
2009-11-23 20:15:05 +0000

    Updated PsychCal scripts.
    	DHB
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1588 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/SensorToSettingsAcc.m
+++ Psychtoolbox/PsychCal/SensorToSettingsAcc.m
@@ -9,13 +9,16 @@ function [finalSettings,badIndex,quantized,perError,settings] = SensorToSettings
 % This depends on the standard calibration globals.
 %
 % 11/12/93   dhb      Wrote it.
-% 3/30/94	   dhb, jms Fixed logic bug in error computation.
-%					              Return finalSettings as best during iteration
+% 3/30/94	 dhb, jms Fixed logic bug in error computation.
+%					  Return finalSettings as best during iteration
 % 8/4/96     dhb      Update for stuff bag routines.
 % 8/21/97    dhb      Update for structures.
-% 3/10/98	   dhb      Change nBasesOut to nPrimaryBases.
+% 3/10/98	 dhb      Change nBasesOut to nPrimaryBases.
 % 4/5/02     dhb, ly  New calling interface.
 % 1/26/04    ly, dhb  Get rid of unused variable called "error".
+% 11/22/09   dhb      Check basis dimension and do the simple fast thing if it is 1.
+%                     This will speed things up when there is no point in trying the
+%                     iterative algorithm.
 
 % Algorithm parameters
 nIterations = 10;
@@ -35,27 +38,33 @@ if (isempty(nPrimaryBases))
 	error('No nPrimaryBases field present in calibration structure');
 end
 
-% THINK ABOUT OUT OF GAMUT ISSUE.  THIS COMMENTED
-% OUT CODE WAS AN INITIAL STAB AT IT.
-%primary = SensorToPrimary(cal,sensor);
-%[nDevice,null] = size(primary);
-%gamut = PrimaryToGamut(cal,primary);
-%target = PrimaryToSensor(cal,gamut);
-target = sensor;
-
-aimfor = target;
-for i = 1:nIterations 
-  primary = SensorToPrimary(cal,aimfor);
-  [gamut,badIndex] = PrimaryToGamut(cal,primary);
-  settings(:,i) = GamutToSettings(cal,gamut);
-  [tmpQuantized,primaryE] = SettingsToSensorAcc(cal,settings(:,i));
-	quantized(:,i) = tmpQuantized;
-  calError(:,i) = quantized(:,i) - aimfor;
-	perError(:,i) = quantized(:,i) - target;
-  aimfor = target - dampingFactor*calError(:,i);
+if (nPrimaryBases == 1)
+    [finalSettings,badIndex] = SensorToSettings(cal,sensor);
+    quantized = [];
+    perError = [];
+    settings = [];
+else
+    % THINK ABOUT OUT OF GAMUT ISSUE.  THIS COMMENTED
+    % OUT CODE WAS AN INITIAL STAB AT IT.
+    %primary = SensorToPrimary(cal,sensor);
+    %[nDevice,null] = size(primary);
+    %gamut = PrimaryToGamut(cal,primary);
+    %target = PrimaryToSensor(cal,gamut);
+    target = sensor;
+    aimfor = target;
+    for i = 1:nIterations
+        primary = SensorToPrimary(cal,aimfor);
+        [gamut,badIndex] = PrimaryToGamut(cal,primary);
+        settings(:,i) = GamutToSettings(cal,gamut);
+        [tmpQuantized,primaryE] = SettingsToSensorAcc(cal,settings(:,i));
+        quantized(:,i) = tmpQuantized;
+        calError(:,i) = quantized(:,i) - aimfor;
+        perError(:,i) = quantized(:,i) - target;
+        aimfor = target - dampingFactor*calError(:,i);
+    end
+    
+    % Find minimum error that was encountered and return those settings
+    summaryError = diag(perError'*perError);
+    [null,minIndex] = min(summaryError);
+    finalSettings = settings(:,minIndex);
 end
-
-% Find minimum error that was encountered and return those settings
-summaryError = diag(perError'*perError);
-[null,minIndex] = min(summaryError);
-finalSettings = settings(:,minIndex);
--- Psychtoolbox/PsychCal/SettingsToSensorAcc.m
+++ Psychtoolbox/PsychCal/SettingsToSensorAcc.m
@@ -10,14 +10,23 @@ function [sensor,primaryE] = SettingsToSensorAcc(cal,settings)
 % 11/12/93  dhb   Wrote it.
 % 11/15/93  dhb   Added deviceE output.
 % 8/4/96    dhb   Update for stuff bag routines.
-% 8/21/97	  dhb	  Update for structures.
+% 8/21/97	dhb	  Update for structures.
 % 3/10/98   dhb	  Change nBasesOut to nPrimaryBases.
 % 4/5/02    dhb, ly  Update for new calling interface.
+% 11/22/09  dhb   Check basis dimension and do the simple fast thing if it is 1.
+%                 This will speed things up when there is no point in trying the
+%                 fancier algorithm.
 
 nPrimaryBases = cal.nPrimaryBases;
 if (isempty(nPrimaryBases))
-	error('No nPrimaryBases field present in calibration structure');
+    error('No nPrimaryBases field present in calibration structure');
 end
-settingsE = ExpandSettings(settings,nPrimaryBases);
-primaryE = SettingsToPrimary(cal,settingsE);
-sensor = PrimaryToSensor(cal,primaryE);
+
+if (nPrimaryBases == 1)
+    sensor = SettingsToSensor(cal,settings);
+    primaryE = [];
+else
+    settingsE = ExpandSettings(settings,nPrimaryBases);
+    primaryE = SettingsToPrimary(cal,settingsE);
+    sensor = PrimaryToSensor(cal,primaryE);
+end
