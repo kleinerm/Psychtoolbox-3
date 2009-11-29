344333e9d24bd9360632fee8429d272fdd77e278
flerfs <chrg@sas.upenn.edu>
2009-11-29 00:55:35 +0000

    Cosmetic.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1589 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/CompareMonCal.m
+++ Psychtoolbox/PsychCal/CompareMonCal.m
@@ -10,11 +10,11 @@ function isSame = CompareMonCal(cal1,cal2,IGNOREDATE)
 % Checks date/time, screen, and computer.  Could check the
 % actual data, but that seems like overkill.
 % 
-% 9/17/97  pbe  Wrote it. 
+% 9/17/97  pbe       Wrote it. 
 % 9/18/97  pbe, dhb  Modify interface, change name.
-% 1/16/98  dhb  Add any around string compares, necessary for desired effect.
-% 1/21/98  dhb  Add IGNOREDATE flag.
-% 3/10/98	 dhb	Change name to CompareMonCal.
+% 1/16/98  dhb       Add any around string compares, necessary for desired effect.
+% 1/21/98  dhb       Add IGNOREDATE flag.
+% 3/10/98  dhb	     Change name to CompareMonCal.
 % 7/3/98   dhb, pbe  Change for cal.describe format.
 
 if (nargin < 3 | isempty(IGNOREDATE))
