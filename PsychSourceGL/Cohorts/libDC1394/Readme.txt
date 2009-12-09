1321ca4b728cdeaf1a4e50502f32c94fc94ab1fa
kleinerm <mario.kleiner@tuebingen.mpg.de>
2009-12-09 20:17:24 +0000

    * GetKeyboardIndices: Allow filtering of returned indices   by keyboard serial number, locationID and productName.   Return optional detailed info about keyboard.
    * Add sources, docu and precompiled OS/X libraries for
      libDC1394 under PsychSourceGL/Cohors/libDC1394/
      just that we have this documented and there isn't any
      guessing and library hunting involved is someone wants
      to rebuild Screen with libdc1394 support.
    
      -> Of course its better to download and use the most recent
         sources from the official sourceforge home of libdc1394,
         this just as a child-protection.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1592 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ PsychSourceGL/Cohorts/libDC1394/Readme.txt
@@ -0,0 +1,49 @@
+libdc1394 libraries:
+
+LibDC1394 is free software, licensed under LGPL, therefore
+compatible with PTB's license and included here.
+
+The official website of libdc1394 is currently:
+
+http://sourceforge.net/projects/libdc1394/
+
+=> Screen makes use of libdc1394 for its Firewire video capture engine
+   under GNU/Linux and MacOS/X.
+
+How to rebuild:
+
+On OS/X you'd follow the follwing instructions. On Linux you'll usually
+just download the latest development libraries with your distributions
+package management system.
+
+The libraries bundled in PsychSourceGL are the ones the official PTB
+distro is linked against, but you can of course download more recent
+versions of the libraries from their official home anytime and use
+these.
+
+1. Unpack the PsychSourceGL/Cohorts/libDC1394/libdc1394...tar.gz
+   file into a local directory.
+
+2. Follow the installation instructions inside the directory to
+   build and install the library on your system.
+
+3. By default, we install the library build products to
+   /usr/local/lib and link statically against /usr/local/lib/libdc1394.a
+   and have the header files in /usr/local/include
+
+   -> Change the XCode project settings or other makefile settings to
+      adapt for different paths or dynamic linking.
+
+4. Rebuild Screen.
+
+-> Done.
+
+
+In PsychSourceGL/Cohorts/ARToolkit:
+
+For OS/X builds, the precompiled static link library libdc1394.a (.a's) is
+stored inside PsychSourceGL/Projects/MacOSX/PsychToolbox/libDC1394/
+This one is just stored here for convenience to save you the time for
+recompiling yourself.
+
+==> All content can be derived from the included .tar.gz file!
--- Psychtoolbox/PsychHardware/GetKeyboardIndices.m
+++ Psychtoolbox/PsychHardware/GetKeyboardIndices.m
@@ -1,22 +1,34 @@
-function [keyboardIndices, productNames]= GetKeyboardIndices;
-% keyboardIndices= GetKeyboardIndices
-%
-% OS X: ___________________________________________________________________
+function [keyboardIndices, productNames, allInfos]= GetKeyboardIndices(product, serialNumber, locationID)
+% [keyboardIndices, productNames, allInfos] = GetKeyboardIndices([productName][, serialNumber][, locationID])
 %
 % The PsychHID assigns each USB HID device connected to you computer a
 % unique index. GetKeyboardIndices returns the indices for those HID
 % devices which are keyboards.  The product names of each keyboard are
 % returned in a second argument which is useful to identify the keyboard
-% associated with an index.  For complete information on a keyboard use
-% PsychHID('Devices').
+% associated with an index. The third return argument is a cell-array with
+% complete information about the keyboard device, e.g., allInfos{1} returns
+% all known info about the 1st detected keyboard.
 %
-% OS 9: ___________________________________________________________________
+% If you have multiple keyboards connected you can restrict the set of
+% returned keyboard by specifying the following optional match-critera:
 %
-% GetKeyboardIndices does not exist in OS 9. 
+% product      = Product name of target devices, as returned in 'productNames'.
 %
-% WINDOWS: ________________________________________________________________
+% serialNumber = Serial number of target devices. This is a text string,
+%                not a number!
+%
+% locationID   = Numeric id of where the device is connected to the
+%                computer. The number is supposed to be unique for a given
+%                connection port. E.g., the same number will be returned
+%                whenever the device is connected to the same USB port of
+%                the computer. The value should be persistent across
+%                reboots of the machine, but may not be persistent across
+%                operating system upgrades - or may not be persistent at
+%                all in case of os bugs. Your mileage may vary...
+%
+% WINDOWS, LINUX: _________________________________________________________
 % 
-% GetKeyboardIndices does not exist in Windows.
+% GetKeyboardIndices does not yet exist in Windows and Linux.
 % 
 % _________________________________________________________________________
 %
@@ -24,15 +36,52 @@ function [keyboardIndices, productNames]= GetKeyboardIndices;
 
 
 % HISTORY
-% 7/6/03    awi     Wrote it. 
+% 7/6/03    awi     Wrote it.
+% 12/8/09    mk     Add matching logic for selection of subsets of devices.
 
+if nargin < 1
+    product = [];
+end
+
+if nargin < 2
+    serialNumber = [];
+end
+
+if nargin < 3
+    locationID = [];
+end
+
+% Init:
 keyboardIndices=[];
 productNames=cell(0);
+allInfos=cell(0);
+
+% Enumerate all HID devices:
 d=PsychHID('Devices');
+
+% Iterate through all of them:
 for i =1:length(d);
+    % Keyboard or keyboard-like device?
     if d(i).usagePageValue==1 && d(i).usageValue == 6
-        keyboardIndices(end+1)=i;
-        productNames{end+1}=d(i).product;
+        % Check if additional match-criteria provided. Skip this keyboard device on mismatch:
+        if ~isempty(product) && ~strcmpi(d(i).product, product)
+            continue;
+        end
+        
+        if ~isempty(serialNumber) && ~strcmpi(d(i).serialNumber, serialNumber)
+            continue;
+        end
+        
+        if ~isempty(locationID) && (d(i).locationID ~= locationID)
+            continue;
+        end
+
+        % Match! Append to list of output keybords, including optional
+        % detailed information:
+        keyboardIndices(end+1)=i; %#ok<AGROW>
+        productNames{end+1}=d(i).product; %#ok<AGROW>
+        allInfos{end+1}=d(i); %#ok<AGROW>
     end
 end
 
+return;
