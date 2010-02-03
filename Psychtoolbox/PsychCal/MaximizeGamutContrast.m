07d1a34db66a408e39ce4521426ecefc3ae99d35
flerfs <chrg@sas.upenn.edu>
2010-02-03 16:40:59 +0000

    Input arg range checking.  Also make MATLAB lint checker happy.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1636 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/MaximizeGamutContrast.m
+++ Psychtoolbox/PsychCal/MaximizeGamutContrast.m
@@ -14,22 +14,30 @@ function [s,sPos,sNeg] = MaximizeGamutContrast(dir,white)
 % 12/3/98  dhb    Add directional return values.
 %          dhb    Remove redundant calculations.
 % 9/7/00   mpr    Made zero handling code more compact; did away with unnecessary variable.
+% 1/31/10  dhb    Range check on input white point.
+%          dhb    Remove calls to find as per MATLAB lint checker.
 
 % Expand out dir to handle all inputs together.
 n = size(dir,2);
 useWhite = white*ones(1,n);
+
+% Make sure useWhite is in range [0,1]. Otherwise this routine
+% will not behave right.
+if (any(useWhite > 1) || any(useWhite < 0))
+    error('Passed white point must be in range [0,1] for all primaries\n');
+end
 
 % Set any dir entry that are zero to something very small
 % This avoids a printed divide by zero error.
 verySmall = 1e-15;
 index = find(abs(dir) < verySmall);
 dir(index) = verySmall*ones(size(index)).*sign(dir(index));
-dir(find(~dir)) = verySmall*ones(size(find(~dir)));
+dir(~dir) = verySmall*ones(size(find(~dir)));
 
 % Deal with small values of useWhite as well
 index = find(abs(useWhite) <= verySmall);
 useWhite(index) = 2*verySmall*ones(size(index)).*sign(useWhite(index));
-useWhite(find(~useWhite)) = 2*verySmall*ones(size(find(~useWhite)));
+useWhite(~useWhite) = 2*verySmall*ones(size(find(~useWhite)));
 
 % Find all of the candidate s values
 s1 = (1-useWhite) ./ dir;		% white + s1*dir = 1
@@ -37,8 +45,8 @@ s2 = (-useWhite) ./ dir;		% white + s2*dir = 0
 
 % Get all of the s values together
 allS = [s1 ; s2];
-indexPos = find(allS >= 0);
-indexNeg = find(allS < 0);
+indexPos = allS >= 0;
+indexNeg = allS < 0;
 
 % Return the minimum absolute value
 s = min(abs(allS));
