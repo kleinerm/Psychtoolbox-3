e0985d2acd4edeaa1f48bd52fc466ebc4a8466a3
flerfs <chrg@sas.upenn.edu>
2010-03-08 14:43:19 +0000

    Fix printed list of fit type options.  -- DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1660 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/RefitCalGamma.m
+++ Psychtoolbox/PsychCal/RefitCalGamma.m
@@ -13,6 +13,7 @@
 %                   function CalibrateFitGamma.  See comments there.
 % 11/20/09 dhb      More terms in crtSumPow.
 % 3/07/10  dhb      Call CalibrateFitLinMod as well.
+% 3/08/10  dhb      Update list of fit type options.
 
 % Enter load code
 defaultFileName = 'monitor';
@@ -40,8 +41,9 @@ fprintf('Old gamma fit type was: %s\n',cal.describe.gamma.fitType);
 oldType = cal.describe.gamma.fitType;
 fprintf('Possible fit types are defined by routine CalibrateFitGamma\n');
 fprintf('See "help CalibrateFitGamma for most up to date options\n');
-fprintf('Current (September 25, 2008) options are:\n');
+fprintf('Current (March 08, 2010) options are:\n');
 fprintf('\tsimplePower\n');
+fprintf('\tcrtLinear\n');
 fprintf('\tcrtPolyLinear\n');
 fprintf('\tcrtGamma\n');
 fprintf('\tcrtSumPow\n');
