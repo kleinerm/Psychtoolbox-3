2616c89808c2acd83e4fc978946fac0f3cee1f8f
kleinerm <mario.kleiner@tuebingen.mpg.de>
2010-05-09 05:48:20 +0000

    * Build scripts for Linux + Octave:
      Add calls to striplibsfrommexfile.m for each build mex file
      as post-processing.
    
    * New function striplibsfrommexfile.m: Strips a compiled
      MEX/OCT binary plugin file for the Octave-3 Linux build
      from certain redundant but harmful dynamic library dependencies.
    
    -> So far this was done in an error-prone, time-consuming
       manual process via a hex editor after each rebuild. This
       should automate it and remove the quite frequent screwups
       MK made when manually stripping the files.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1690 f5efbe48-b203-0410-9429-fc5e098e5a54

--- PsychSourceGL/Source/linuxmakeitoctave3_ubuntugutsy.m
+++ PsychSourceGL/Source/linuxmakeitoctave3_ubuntugutsy.m
@@ -20,36 +20,42 @@ if mode==0
 % With ARVideo - incomplete:    mex -v -g --output ../Projects/Linux/build/Screen.mex -DPTBVIDEOCAPTURE_ARVIDEO -DPTBVIDEOCAPTURE_LIBDC -DPTBOCTAVE3MEX -I../Cohorts/ARToolkit/include -I/usr/X11R6/include -ICommon/Base -ICommon/Screen -ILinux/Base -ILinux/Screen -L/usr/X11R6/lib Common/Base/*.cc Linux/Base/*.c Linux/Screen/*.c Common/Screen/*.c Common/Base/*.c -lc -lrt -lGL -lX11 -lXext /usr/lib/libXxf86vm.a /usr/lib/libGLU.a /usr/local/lib/libdc1394.a /usr/local/lib/libraw1394.a ../Projects/Linux/ARToolkit/libARvideo.a /usr/lib/libgstreamer-0.10.a 
     mex -v -g --output ../Projects/Linux/build/Screen.mex -DPTBVIDEOCAPTURE_LIBDC -DPTBOCTAVE3MEX -I/usr/X11R6/include -ICommon/Base -ICommon/Screen -ILinux/Base -ILinux/Screen -L/usr/X11R6/lib Common/Base/*.cc Linux/Base/*.c Linux/Screen/*.c Common/Screen/*.c Common/Base/*.c -lc -lrt -lGL -lX11 -lXext /usr/lib/libXxf86vm.a /usr/lib/libGLU.a /usr/local/lib/libdc1394.a /usr/local/lib/libraw1394.a 
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/Screen.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/Screen.mex']);
 end;
 
 if mode==1
     % Build GetSecs.mex:
     mex -v -g --output ../Projects/Linux/build/GetSecs.mex -DPTBMODULE_GetSecs -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/GetSecs -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/GetSecs/*.c -lc -lrt 
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/GetSecs.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/GetSecs.mex']);
 end;
 
 if mode==2
     % Build WaitSecs.mex:
     mex -v -g --output ../Projects/Linux/build/WaitSecs.mex -DPTBMODULE_WaitSecs -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/WaitSecs -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/WaitSecs/*.c -lc -lrt 
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/WaitSecs.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/WaitSecs.mex']);
 end;
 
 if mode==3
     % Build PsychPortAudio.mex:
     mex -v -g --output ../Projects/Linux/build/PsychPortAudio.mex -DPTBMODULE_PsychPortAudio -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/PsychPortAudio -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/PsychPortAudio/*.c /usr/local/lib/libportaudio.a -lc -lrt -lasound
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/PsychPortAudio.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/PsychPortAudio.mex']);
 end
 
 if mode==4
     % Build Eyelink.mex:
     mex -v -g --output ../Projects/Linux/build/Eyelink.mex -DPTBMODULE_Eyelink -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/Eyelink -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/Eyelink/*.c -leyelink_core -lc -lrt
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/Eyelink.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');    
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/Eyelink.mex']);
 end
 
 if mode==5
     % Build IOPort.mex:
     mex -v -g --output ../Projects/Linux/build/IOPort.mex -DPTBMODULE_IOPort -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/IOPort -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/IOPort/*.c -lc -lrt
     unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/IOPort.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');    
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/IOPort.mex']);
 end
 
 if mode==6
@@ -61,6 +67,7 @@ if mode==6
     catch
     end
     unix('mv moglcore.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/moglcore.mex']);
     cd(curdir);
 end;
 
---
+++ PsychSourceGL/Source/striplibsfrommexfile.m
@@ -0,0 +1,94 @@
+function striplibsfrommexfile(filename, testrun)
+% striplibsfrommexfile(filename [, testrun=0]);
+%
+% Remove a certain set of dynamic library dependencies from MEX/OCT file
+% given via 'filename'. Optional 'testrun' if set to non-zero value will
+% perform a dry-run.
+%
+% The stripped libraries (see source of this file) are library dependencies
+% added by default by Octave's build process for OCT/MEX files. They make
+% sense for mathematical routines, but are totally redundant for PTB files.
+%
+% They also do harm, as they are versioned for some odd reason, so trying
+% to load the mex files on Linux distributions with different versions of
+% those libraries installed will fail.
+%
+% -> This script loads the binary code, searches for occurence of the
+% superfluous library name strings and null's them out to remove the
+% dependency. Then it rewrites the stripped mex binary.
+%
+
+if nargin < 2
+    testrun = 0;
+end
+
+if exist(filename, 'file')
+    % Read original image:
+    [fd, msg] = fopen(filename, 'r+');
+    if fd == -1
+        error('ERROR! MEX file %s could not be read [%s]! Can''t strip it!\n', filename, msg);
+    end
+    image = uint8(fread(fd));
+    
+    % Strip all following libraries from image:
+    image = stripLibrary(image, 'libreadline');
+    image = stripLibrary(image, 'libncurses');
+    image = stripLibrary(image, 'libfft');
+    image = stripLibrary(image, 'libhdf');
+    image = stripLibrary(image, 'libgfortran');
+    image = stripLibrary(image, 'libz');
+    
+    % Write stripped image:
+    frewind(fd);
+    if ~testrun
+        fwrite(fd, image);
+    end
+    
+    if (fclose(fd) ~= 0)
+        error('ERROR! MEX file %s could not be closed [%s]! Can''t strip it!\n', filename, ferror(fd));
+    end
+
+    fprintf('MEX file %s stripped from superfluous libraries.\n', filename);
+else
+    fprintf('WARNING! MEX file %s does not exist! Can''t strip it!\n', filename);
+end
+
+return
+
+function image = stripLibrary(image, library)
+
+    dodebug = 0;
+
+    % Find first character of library in image:
+    pStart = strfind(image', library);
+    if ~isempty(pStart)
+        % Iterate over all occurences, kill each of them:
+        for cStart = pStart
+            % Kill occurence at cStart:
+            
+            if dodebug
+                disp(char(image(cStart:cStart + 20)))
+            end
+
+            % Start at the start:
+            p = cStart;
+
+            % Strip-Loop: Strip until zero-byte:
+            while (image(p) ~= 0)
+                % Null-out this character:
+                image(p) = 0;
+                % Next one...
+                p = p + 1;
+            end
+
+            if dodebug
+                disp(char(image(cStart:cStart + 20)))
+            end
+        end
+    end
+    
+    if ~isa(image, 'uint8')
+        error('After strip op, image is no longer of uint8 class as required! Stripping failed!!');
+    end
+    
+return
