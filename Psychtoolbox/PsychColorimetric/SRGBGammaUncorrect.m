0145b4a7ad2c7e04d6caad985197c206dcaace75
flerfs <chrg@sas.upenn.edu>
2010-08-16 20:04:34 +0000

    Fixed header and edited comments.  DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1772 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetric/SRGBGammaUncorrect.m
+++ Psychtoolbox/PsychColorimetric/SRGBGammaUncorrect.m
@@ -1,17 +1,16 @@
-function rgb = SRGBGammaCorrect(RGB)
-% rgb = SRGBGammaCorrect(RGB)
+function rgb = SRGBGammaUncorrect(RGB)
+% rgb = SRGBGammaUncorrect(RGB)
 %
 % Undo standard sRGB gamma correction, taking [0-255] -> [0-1].
-%
-% Conversion as speciedi at:
-% http://www.srgb.com/basicsofsrgb.htm
 %
 % See XYZToSRGBPrimary for comment on evolution of the standard
 % and of this implementation.
 %
+% See comments inn XYZToSRGBPrimary for info on standard.
 %
 % 2/9/06	dhb	 Wrote it.
 % 7/8/10    dhb  Rewrote to match current standard.
+% 8/16/10   dhb  Fix typo in header.
 
 % Apply sRGB gamma correction according to formulae
 cutoff = 0.03928;
