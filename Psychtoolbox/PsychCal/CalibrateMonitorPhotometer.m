2cde6f7399bb2e82a21b7f370d892cf8e2dbf127
kleinerm <mario.kleiner@tuebingen.mpg.de>
2010-10-22 20:47:02 +0000

    CalibrateMonitorPhotometer: Use GetNumber() instead of input() for user input of luminance values.
    -> More robust against broken operating systems.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1809 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychCal/CalibrateMonitorPhotometer.m
+++ Psychtoolbox/PsychCal/CalibrateMonitorPhotometer.m
@@ -25,8 +25,17 @@ function [ gammaTable1, gammaTable2, displayBaseline, displayRange, displayGamma
 % %Then when you're ready to use the gamma table:
 % load MyGammaTable
 % Screen('LoadNormalizedGammaTable', win, gammaTable*[1 1 1]);
-% 
+%
+%
+% History:
 % Version 1.0: Patrick Mineault (patrick.mineault@gmail.com)
+% 22.10.2010 mk Switch numeric input from use of input() to use of
+%               GetNumber(). Restore gamma table after measurement. Make
+%               more robust.
+
+global vals;
+global inputV;
+
     if(nargin < 1)
         numMeasures = 9;
     end
@@ -36,25 +45,37 @@ function [ gammaTable1, gammaTable2, displayBaseline, displayRange, displayGamma
            'A screen of higher luminance will be shown. Repeat %d times. ' ...
            'Press enter to start'], numMeasures));
        
-
-
-    origclut = repmat([0:255]'/255,1,3);
-
-    [win rect] = Screen('OpenWindow', 0, 0);
-    Screen('LoadNormalizedGammaTable', win, origclut );
+    origclut = repmat([0:255]'/255,1,3); %#ok<NBRAK>
+    psychlasterror('reset');
     
-    vals = [];
-    inputV = [0:256/(numMeasures - 1):256];
-    inputV(end) = 255;
-    for i = inputV
-        Screen('FillRect',win,i);
-        Screen('Flip',win);
+    try
+        win = Screen('OpenWindow', 0, 0);
+
+        BackupCluts;        
+        Screen('LoadNormalizedGammaTable', win, origclut );
+
+        vals = [];
+        inputV = [0:256/(numMeasures - 1):256]; %#ok<NBRAK>
+        inputV(end) = 255;
+        for i = inputV
+            Screen('FillRect',win,i);
+            Screen('Flip',win);
+
+            % MK: Deprecated as not reliable: resp = input('Value?');
+            fprintf('Value? ');
+            resp = GetNumber;
+            fprintf('\n');
+            vals = [vals resp]; %#ok<AGROW>
+        end
         
-        resp = input('Value?');
-        vals = [vals resp];
+        RestoreCluts;
+        Screen('CloseAll');
+    catch
+        RestoreCluts;
+        Screen('CloseAll');
+        psychrethrow(psychlasterror);
     end
-    Screen('CloseAll');
-    
+
     displayRange = (max(vals) - min(vals));
     displayBaseline = min(vals);
     
@@ -62,24 +83,32 @@ function [ gammaTable1, gammaTable2, displayBaseline, displayRange, displayGamma
     vals = (vals - displayBaseline)/(max(vals) - min(vals));
     inputV = inputV/255;
     
+    if ~exist('fittype'); %#ok<EXIST>
+        fprintf('This function needs fittype() for automatic fitting. This function is missing on your setup.\n');
+        fprintf('Therefore i can''t proceed, but the input values for a curve fit are available to you by\n');
+        fprintf('defining "global vals;" and "global inputV" on the command prompt, with "vals" being the displayed\n');
+        fprintf('values and "inputV" being user input from the measurement. Both are normalized to 0-1 range.\n\n');
+        error('Required function fittype() unsupported. You need the curve-fitting toolbox for this to work.\n');
+    end
+    
     %Gamma function fitting
     g = fittype('x^g');
     fittedmodel = fit(inputV',vals',g);
     displayGamma = fittedmodel.g;
-    gammaTable1 = ((([0:255]'/255))).^(1/fittedmodel.g);
+    gammaTable1 = ((([0:255]'/255))).^(1/fittedmodel.g); %#ok<NBRAK>
     
-    firstFit = fittedmodel([0:255]/255);
+    firstFit = fittedmodel([0:255]/255); %#ok<NBRAK>
     
     %Spline interp fitting
     fittedmodel = fit(inputV',vals','splineinterp');
-    secondFit = fittedmodel([0:255]/255);
+    secondFit = fittedmodel([0:255]/255); %#ok<NBRAK>
     
     figure;
-    plot(inputV, vals, '.', [0:255]/255, firstFit, '--', [0:255]/255, secondFit, '-.');
+    plot(inputV, vals, '.', [0:255]/255, firstFit, '--', [0:255]/255, secondFit, '-.'); %#ok<NBRAK>
     legend('Measures', 'Gamma model', 'Spline interpolation');
     title(sprintf('Gamma model x^{%.2f} vs. Spline interpolation', displayGamma));
     
     %Invert interpolation
     fittedmodel = fit(vals',inputV','splineinterp');
     
-    gammaTable2 = fittedmodel([0:255]/255);
+    gammaTable2 = fittedmodel([0:255]/255); %#ok<NBRAK>
