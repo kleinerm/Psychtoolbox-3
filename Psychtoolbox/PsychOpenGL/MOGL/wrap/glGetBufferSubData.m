4c9d011e36d435475863d510edecfc20dcca1f87
kleinerm <mario.kleiner@tuebingen.mpg.de>
2010-11-03 20:29:26 +0000

    * glGetBufferSubData(): Made functional.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1818 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ Psychtoolbox/PsychOpenGL/MOGL/wrap/glGetBufferSubData.m
@@ -0,0 +1,24 @@
+function glGetBufferSubData( target, offset, dsize, data )
+
+% glGetBufferSubData  Interface to OpenGL function glGetBufferSubData
+%
+% usage:  glGetBufferSubData( target, offset, dsize, data )
+%
+% Caution: Caller needs to allocate 'data' as a matrix or vector of
+% suitable format and capacity for the data, otherwise bad things will
+% happen!
+%
+% C function:  void glGetBufferSubData(GLenum target, GLint offset, GLsizei dsize, GLvoid* data)
+
+% 05-Mar-2006 -- created (generated automatically from header files)
+% 03-Nov-2010 -- Made functional (MK).
+
+% ---protected---
+
+if nargin~=4,
+    error('invalid number of arguments');
+end
+
+moglcore('glGetBufferSubData', target, offset, dsize, data);
+
+return
