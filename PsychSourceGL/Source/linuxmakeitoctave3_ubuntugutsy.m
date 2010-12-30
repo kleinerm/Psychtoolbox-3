997678b66ae6e9461bd52cd58296ec10d565c0dd
kleinerm <mario.kleiner@tuebingen.mpg.de>
2010-12-30 04:12:56 +0000

    * All mex files for Linux / Build script: Remove hard-coded   path to /home/kleinerm -- Make paths more flexible.
    => All Octave + Linux mex files rebuilt, for no specific reason,
       just to test it :-)
    
    => Rebuild & test on Matlab pending.
    
    
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1891 f5efbe48-b203-0410-9429-fc5e098e5a54

--- PsychSourceGL/Source/linuxmakeitoctave3_ubuntugutsy.m
+++ PsychSourceGL/Source/linuxmakeitoctave3_ubuntugutsy.m
@@ -25,42 +25,42 @@ fprintf('Building plugin type %i ...\n\n', mode);
 if mode==0
     % Build Screen.mex:
     mex -v -g --output ../Projects/Linux/build/Screen.mex -DPTB_USE_GSTREAMER -DPTBVIDEOCAPTURE_LIBDC -DPTBOCTAVE3MEX -I/usr/X11R6/include -I/usr/include/gstreamer-0.10 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/libxml2 -ICommon/Base -ICommon/Screen -ILinux/Base -ILinux/Screen -L/usr/X11R6/lib  Common/Base/*.cc Linux/Base/*.c Linux/Screen/*.c Common/Screen/*.c Common/Base/*.c -lc -lrt -lGL -lGLU -lX11 -lXext -lgstreamer-0.10 -lgstbase-0.10 -lgstapp-0.10 -lgobject-2.0 -lgmodule-2.0 -lxml2 -lgthread-2.0 -lglib-2.0 -lXxf86vm -ldc1394 -lraw1394 -lusb-1.0
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/Screen.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv ../Projects/Linux/build/Screen.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/Screen.mex']);
 end;
 
 if mode==1
     % Build GetSecs.mex:
     mex -v -g --output ../Projects/Linux/build/GetSecs.mex -DPTBMODULE_GetSecs -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/GetSecs -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/GetSecs/*.c -lc -lrt 
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/GetSecs.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv ../Projects/Linux/build/GetSecs.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/GetSecs.mex']);
 end;
 
 if mode==2
     % Build WaitSecs.mex:
     mex -v -g --output ../Projects/Linux/build/WaitSecs.mex -DPTBMODULE_WaitSecs -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/WaitSecs -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/WaitSecs/*.c -lc -lrt 
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/WaitSecs.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv ../Projects/Linux/build/WaitSecs.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/WaitSecs.mex']);
 end;
 
 if mode==3
     % Build PsychPortAudio.mex:
     mex -v -g --output ../Projects/Linux/build/PsychPortAudio.mex -DPTBMODULE_PsychPortAudio -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/PsychPortAudio -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/PsychPortAudio/*.c /usr/local/lib/libportaudio.a -lc -lrt -lasound
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/PsychPortAudio.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv ../Projects/Linux/build/PsychPortAudio.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/PsychPortAudio.mex']);
 end
 
 if mode==4
     % Build Eyelink.mex:
     mex -v -g --output ../Projects/Linux/build/Eyelink.mex -DPTBMODULE_Eyelink -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/Eyelink -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/Eyelink/*.c -leyelink_core -lc -lrt
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/Eyelink.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');    
+    unix(['mv ../Projects/Linux/build/Eyelink.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/Eyelink.mex']);
 end
 
 if mode==5
     % Build IOPort.mex:
     mex -v -g --output ../Projects/Linux/build/IOPort.mex -DPTBMODULE_IOPort -DPTBOCTAVE3MEX -ICommon/Base -ILinux/Base -ICommon/IOPort -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/IOPort/*.c -lc -lrt
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/IOPort.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');    
+    unix(['mv ../Projects/Linux/build/IOPort.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/IOPort.mex']);
 end
 
@@ -72,7 +72,7 @@ if mode==6
        mex -v -g --output moglcore.mex -DLINUX -DGLEW_STATIC -DPTBOCTAVE3MEX -I/usr/X11R6/include -L/usr/X11R6/lib -lc -lGL -lGLU -lglut moglcore.c gl_auto.c gl_manual.c glew.c mogl_rebinder.c
     catch
     end
-    unix('mv moglcore.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv moglcore.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     cd(curdir);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/moglcore.mex']);
 end;
@@ -80,7 +80,7 @@ end;
 if mode==7
     % Build PsychKinectCore.mex:
     mex -v -g --output ../Projects/Linux/build/PsychKinectCore.mex -DPTBMODULE_PsychKinectCore -DPTBOCTAVE3MEX -I/usr/include/libusb-1.0 -I/usr/include/libfreenect -ICommon/Base -ILinux/Base -ICommon/PsychKinect -ICommon/Screen Common/Base/*.cc Linux/Base/*.c Common/Base/*.c Common/PsychKinect/*.c -lc -lrt -lfreenect -lusb-1.0
-    unix('mv /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/PsychSourceGL/Projects/Linux/build/PsychKinectCore.mex /home/kleinerm/projects/OpenGLPsychtoolbox/trunk/Psychtoolbox/PsychBasic/Octave3LinuxFiles/');
+    unix(['mv ../Projects/Linux/build/PsychKinectCore.mex ' PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/']);
     striplibsfrommexfile([PsychtoolboxRoot 'PsychBasic/Octave3LinuxFiles/PsychKinectCore.mex']);
 end;
 
