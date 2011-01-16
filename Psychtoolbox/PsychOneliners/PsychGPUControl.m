24e035257176754960de281e5ce52c46638c3ada
kleinerm <mario.kleiner@tuebingen.mpg.de>
2011-01-16 18:07:42 +0000

    * PsychGPUControl: Implement control command that allows to   setup Compiz on Linux to either redirect or unredirect   fullscreen onscreen windows, i.e., to enable or disable   desktop composition for such fullscreen windows.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@1933 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychOneliners/PsychGPUControl.m
+++ Psychtoolbox/PsychOneliners/PsychGPUControl.m
@@ -36,11 +36,24 @@ function rc = PsychGPUControl(cmd, varargin)
 % driver and GPU.
 %
 %
+% rc = PsychGPUControl('FullScreenWindowDisablesCompositor', flag [, screenIds]);
+% - Select if desktop composition should be disabled for displays where
+% a Psychtoolbox fullscreen onscreen window is displayed. 'flag' == 1 means
+% to disable composition for fullscreen windows, 0 means to enable composition
+% for fullscreen windows. You usually want composition to be disabled, as this
+% is currently the only way to get decent timing and precise visual stimulus
+% onset timestamping. The optional vector of 'screenIds' selects which screens
+% should be affected by the change. If left out or set to [], all detected
+% screens will be changed.
+%
+% 
 
 % History:
 %  3.01.2010  mk  Written.
 % 19.04.2010  mk  Add quotes around path to command to protect against
 %                 blanks in path to executable.
+% 16.01.2011  mk  Add function to control desktop composition on Linux with
+%                 Compiz.
 
 if nargin < 1
 	error('Subfunction command argument missing!');
@@ -89,6 +102,48 @@ if strcmpi(cmd, 'SetGPUPerformance')
 	% Command code 0 means: Control performance state: 0 = AUTO, 1 = MAX, 2 = MIN.
 	cmdpostfix = sprintf(' 0 %i', perfflag);
 	rc = executeRadeoncmd(cmdpostfix);
+	return;
+end
+
+if strcmpi(cmd, 'FullScreenWindowDisablesCompositor')
+	if isempty(varargin)
+		error('FullScreenWindowDisablesCompositor: flag argument missing!');
+	end
+
+	compositorOff = varargin{1};
+	if ~ismember(compositorOff, 0:1)
+		error('FullScreenWindowsDisableCompositor: Invalid flag argument, not an integer of value 0 or 1!');
+	end
+
+	if length(varargin) < 2 || isempty(varargin{2})
+		screenIds = Screen('Screens');
+	else
+		screenIds = varargin{2};
+	end
+
+	% Which OS?
+	if ~IsLinux
+		% Nothing to do on non-Linux as compositor handling is
+		% implemented in Screen internally:
+		rc = 1;
+		return;
+	end
+
+	% We only know how to do this for Compiz, so we try that. Settings are persistent
+	% across sessions and take effect immediately:
+	rc = [];
+	for screenId=screenIds
+		if compositorOff
+			% Enable un-redirection: Fullscreen windows aren't subject to treatment by compositor,
+			% but can do (e.g. page-flipping) whatever they want:
+			rc(end+1) = system(sprintf('gconftool-2 -s --type bool /apps/compiz/general/screen%i/options/unredirect_fullscreen_windows true', screenId));
+			fprintf('PsychGPUControl:FullScreenWindowDisablesCompositor: Desktop composition for fullscreen windows on screen %i disabled.\n', screenId)
+		else
+			% Disable un-redirection: Fullscreen windows get composited as all other windows:
+			rc(end+1) = system(sprintf('gconftool-2 -s --type bool /apps/compiz/general/screen%i/options/unredirect_fullscreen_windows false', screenId));
+			fprintf('PsychGPUControl:FullScreenWindowDisablesCompositor: Desktop composition for fullscreen windows on screen %i enabled.\n', screenId);
+		end
+	end
 	return;
 end
 
