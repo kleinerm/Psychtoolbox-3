0a0c8e664b568799f1aeb2b96fbbf87fea4939f4
njabakhanji <nuha@sr-research.com>
2011-05-12 17:40:07 +0000

    NEW: Return result of last calibration
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2062 f5efbe48-b203-0410-9429-fc5e098e5a54

---
+++ PsychSourceGL/Source/Common/Eyelink/EyelinkCalMessage.c
@@ -0,0 +1,69 @@
+/* 
+ /osxptb/trunk/PsychSourceGL/Source/OSX/Eyelink/EyelinkCalMessage.c
+ 
+ PROJECTS: Eyelink 
+ 
+ AUTHORS:
+nuha@sr-research.com nj
+ 
+ 
+ HISTORY:
+ 
+11-03-04  nj		created
+ 
+ TARGET LOCATION:
+ 
+ Eyelink.mexmac resides in:
+ EyelinkToolbox
+ */
+
+#include "PsychEyelink.h"
+
+
+static char useString[] = "[result, messageString] = Eyelink('CalMessage')";
+
+static char synopsisString[] =
+"Returns text associated with result of last calibration,"
+"validation, or drift correction. This usually specifies"
+"errors or other statistics.";
+
+static char seeAlsoString[] = "";
+
+/*
+ ROUTINE: EYELINKcalmessage 
+ PURPOSE:
+ Returns text associated with result of last calibration, validation, or drift correction. This usually specifies
+ errors or other statistics.*/
+
+PsychError EyelinkCalMessage(void)
+{
+	int		result;
+	char	strMessage[256];
+	
+	// Clear strings
+	memset(strMessage, 0, sizeof(strMessage));
+	
+	
+	//all sub functions should have these two lines
+	PsychPushHelp(useString, synopsisString, seeAlsoString);
+	if(PsychIsGiveHelp()){PsychGiveHelp();return(PsychError_none);};
+	
+	//check to see if the user supplied superfluous arguments
+	PsychErrorExit(PsychCapNumInputArgs(0));
+	PsychErrorExit(PsychRequireNumInputArgs(0));
+	PsychErrorExit(PsychCapNumOutputArgs(2));
+	
+	// Verify eyelink is up and running
+	EyelinkSystemIsConnected();
+	EyelinkSystemIsInitialized();
+	
+//	PsychAllocInCharArg(1, TRUE, &strMessage);
+	
+	result = eyelink_cal_message(strMessage);
+	
+	
+	PsychCopyOutDoubleArg(1, TRUE, result);
+	PsychCopyOutCharArg(2,TRUE, strMessage);	
+
+	return(PsychError_none);
+}
