ed8aef0c8e3365799019beaf14dc1340314d7807
brainard <brainard@cattell.psych.upenn.edu>
2011-08-12 19:56:23 +0000

    Start to write CIE method, but need to type in some tables before I can go much further.  DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2246 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetricData/LensTransmittance.m
+++ Psychtoolbox/PsychColorimetricData/LensTransmittance.m
@@ -1,6 +1,6 @@
 
-function lensTransmit = LensTransmittance(S,species,source)
-% lensTransmit = LensTransmittance(S,[species],[source])
+function [lensTransmit,lensDensity] = LensTransmittance(S,species,source,ageInYears)
+% [lensTransmit,lensDensity] = LensTransmittance(S,[species],[source],[ageInYears)
 %
 % Return an estimate of the transmittance of the lens.
 %
@@ -9,16 +9,23 @@ function lensTransmit = LensTransmittance(S,species,source)
 %
 % Allowable sources:
 %   StockmanSharpe (Default) - Stockman, Sharpe, & Fach (1999).
+%   CIE                      - Formula from CIE 170-1:2006.
 %   WyszeckiStiles           - W&S, Table 1(2.4.6), p. 109.  First data set in table.
 %   None                     - Unity transmittance.
 %
 % The answer is returned in a row vector.  This function
-% depends on data contained in directory PsychColorimetricData:PsychColorimetricMatFiles.
+% depends on data contained in directory
+% PsychColorimetricData:PsychColorimetricMatFiles.
+%
+% The CIE source will take again in years.  The
+% default is 32.  The acceptable range is 20-80.
+% 
 %
 % 7/8/03  dhb  Made this a separate function.
 % 7/11/03 dhb  Species arg, change name.
 % 7/23/03 dhb  Add Stockman estimate.
 % 7/26/03 dhb  Extend functions, rather than zero truncate.
+% 8/12/11 dhb  Start to write CIE version.  Return lensDensity too.
 
 % Default
 if (nargin < 2 || isempty(species))
@@ -26,6 +33,9 @@ if (nargin < 2 || isempty(species))
 end
 if (nargin < 3 || isempty(source))
 	source = 'StockmanSharpe';
+end
+if (nargin < 4 || isempty(ageInYears))
+	ageInYears = 32;
 end
 
 % Load correction for lens density
@@ -36,12 +46,22 @@ switch (species)
 				lensTransmit = ones(S(3),1)';
 			case 'WyszeckiStiles',
 				load den_lens_ws;
-				den_lens = SplineSrf(S_lens_ws,den_lens_ws,S,1);
-				lensTransmit = 10.^(-den_lens)';
+				lensDensity = SplineSrf(S_lens_ws,den_lens_ws,S,1);
+				lensTransmit = 10.^(-lensDensity)';
 			case 'StockmanSharpe',
 				load den_lens_ssf;
-				den_lens = SplineSrf(S_lens_ssf,den_lens_ssf,S,1);
-				lensTransmit = 10.^(-den_lens)';
+				lensDensity = SplineSrf(S_lens_ssf,den_lens_ssf,S,1);
+				lensTransmit = 10.^(-lensDensity)';
+            case 'CIE'
+                if (ageInYears < 20)
+                    error('Specified age must be 10 or older');
+                elseif (ageInYears <= 60)
+                    wls = MakeItWls(S);
+                elseif (ageInYears <= 80)
+                else
+                    error('Specified age must be 80 or younger');
+                end
+                
 			otherwise,
 				error('Unsupported lens density estimate specified');
 		end
