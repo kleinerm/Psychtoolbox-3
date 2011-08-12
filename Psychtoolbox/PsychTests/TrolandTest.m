eb477db8bd509120870456ce2e0dcd634fa93df9
brainard <brainard@cattell.psych.upenn.edu>
2011-08-12 15:44:20 +0000

    Expand comments, tidy up printout.  DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2241 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychTests/TrolandTest.m
+++ Psychtoolbox/PsychTests/TrolandTest.m
@@ -1,9 +1,15 @@
 % TrolandTest
 %
 % Test the conversion routines between various "intensity"
-% measures.
+% measures, for monochromatic lights.
+%
+% Since these routines were written, there are additional
+% routines that I think work for full spectra.  See
+%   TrolandsToRetIrradiance
+%   RetIrradianceToTrolands
 %
 % 8/16/96  dhb, abp  Made ourselves write it late at night.
+% 8/12/11  dhb       Expanded comment.
 
 % Load in vLambda if its not there
 if (exist('vLambda') ~= 1)
@@ -28,7 +34,8 @@ trolands1 = PowerToTrolands(wl,power1,S_vLambda,vLambda);
 quanta2 = TrolandsToQuanta(wl,trolands1,S_vLambda,vLambda);
 
 % Display results
-disp(sprintf('quanta = %g, quanta1 = %g, quanta2 = %g',...
-	quanta,quanta1,quanta2));
-disp(sprintf('trolands = %g, trolands1 = %g',trolands,trolands1));
-disp(sprintf('power = %g, power1 = %g',power,power1));
+fprintf('Quantities in each line below should agree.\n');
+fprintf('\tquanta = %g, quanta1 = %g, quanta2 = %g\n',...
+	quanta,quanta1,quanta2);
+fprintf('\ttrolands = %g, trolands1 = %g\n',trolands,trolands1);
+fprintf('\tpower = %g, power1 = %g\n',power,power1);
