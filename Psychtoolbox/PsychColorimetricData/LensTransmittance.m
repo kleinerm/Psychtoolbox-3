f6fc67e99a8ab60235aac6ad996fb59adbb77fbe
brainard <brainard@cattell.psych.upenn.edu>
2011-08-13 02:07:47 +0000

    Finished modifications, pending bugs.  DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2249 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetricData/LensTransmittance.m
+++ Psychtoolbox/PsychColorimetricData/LensTransmittance.m
@@ -1,6 +1,6 @@
 
-function [lensTransmit,lensDensity] = LensTransmittance(S,species,source,ageInYears)
-% [lensTransmit,lensDensity] = LensTransmittance(S,[species],[source],[ageInYears)
+function [lensTransmit,lensDensity] = LensTransmittance(S,species,source,ageInYears,pupilSizeMM)
+% [lensTransmit,lensDensity] = LensTransmittance(S,[species],[source],[ageInYears],[pupilSizeMM])
 %
 % Return an estimate of the transmittance of the lens.
 %
@@ -17,8 +17,12 @@ function [lensTransmit,lensDensity] = LensTransmittance(S,species,source,ageInYe
 % depends on data contained in directory
 % PsychColorimetricData:PsychColorimetricMatFiles.
 %
-% The CIE source will take again in years.  The
-% default is 32.  The acceptable range is 20-80.
+% The CIE source will take age in years and pupil size in mm.
+% The default age is 32.  The acceptable range is 20-80.
+% The default pupil size is 3 mm.  Sizes less than 3 are treated
+% as 3.  Sizes greater than 7 are treated as 7.  The interpolation
+% between 3 and 7 doesn't appear to be part of the standard but
+% I coded it in anyway.
 % 
 %
 % 7/8/03  dhb  Made this a separate function.
@@ -26,6 +30,7 @@ function [lensTransmit,lensDensity] = LensTransmittance(S,species,source,ageInYe
 % 7/23/03 dhb  Add Stockman estimate.
 % 7/26/03 dhb  Extend functions, rather than zero truncate.
 % 8/12/11 dhb  Start to write CIE version.  Return lensDensity too.
+%         dhb  Finish. Add pupil size.
 
 % Default
 if (nargin < 2 || isempty(species))
@@ -36,6 +41,9 @@ if (nargin < 3 || isempty(source))
 end
 if (nargin < 4 || isempty(ageInYears))
 	ageInYears = 32;
+end
+if (nargin < 5 || isempty(pupilSizeMM))
+	pupilSizeMM = 3;
 end
 
 % Load correction for lens density
@@ -53,13 +61,37 @@ switch (species)
 				lensDensity = SplineSrf(S_lens_ssf,den_lens_ssf,S,1);
 				lensTransmit = 10.^(-lensDensity)';
             case 'CIE'
+                % Load CIE age dependent and age independent components
+                load den_lens_cie_1
+                load den_lens_cie_2
+                lensDensity1 = SplineSrf(S_lens_cie_1,den_lens_cie_1,S,1);
+                lensDensity2 = SplineSrf(S_lens_cie_2,den_lens_cie_2,S,1);
+                
+                % Combine them according to age using CIE formulae
                 if (ageInYears < 20)
-                    error('Specified age must be 10 or older');
+                    error('Specified age must be 20 or older');
                 elseif (ageInYears <= 60)
-                    wls = MakeItWls(S);
+                    lensDensity = lensDensity1*(1+0.02*(ageInYears-32))+lensDensity2;
                 elseif (ageInYears <= 80)
+                    lensDensity = lensDensity1*(1.56+0.0667*(ageInYears-60))+lensDensity2;
                 else
                     error('Specified age must be 80 or younger');
+                end
+                
+                % This is the answer for pupil size <= 3mm. But, can
+                % correct for pupil diameter.
+                %
+                % The CIE report says for pupil size > 7 mm, multiply (!) the density
+                % values by 0.86207.  SO that's what we do for that case.
+                %
+                % It seems silly not to return anything for values between
+                % 3 and 7 mm, so for this case I linearly interpolate the
+                % factor.
+                if (pupilSizeMM > 3 && pupilSizeMM < 7)
+                    factor = (7-pupilSize)/4 + 0.86207*(pupilSize-3)/4;
+                    lensDensity = factor*lensDensity;
+                elseif (pupilSizeMM >= 7)
+                    lensDensity = 0.86207*lensDensity;
                 end
                 
 			otherwise,
