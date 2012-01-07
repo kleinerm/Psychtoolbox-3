62a73ac7521e489603053fa8d75967ee6364304c
kleinerm <mario.kleiner@tuebingen.mpg.de>
2012-01-07 07:44:48 +0000

    Linux: Improve Priority() setup.
    PsychLinuxConfiguration() modified:
    
    If /etc/security/limits.d/ directory exists, we add a new
    config file PsychBasic/99-psychtoolboxlimits.conf to that
    directory instead of messing with /etc/security/limits.conf.
    This is a cleaner solution.
    
    Also we don't allow memlocking and rt scheduling for
    all users, but only for members of Unix group 'psychtoolbox'.
    We create this group automatically and give instructions
    to users how they can add themselves and others to that group.
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2422 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychLinuxConfiguration.m
+++ Psychtoolbox/PsychLinuxConfiguration.m
@@ -14,9 +14,16 @@ function PsychLinuxConfiguration
 %
 % Realtime optimizations are achieved by extending the
 % /etc/security/limits.conf file with entries that allow
-% all users to lock application memory into physical RAM,
-% eliminating/minimizing interference from the VM subsystem,
-% and to run with realtime priorities up to level 50.
+% members of the Unix user group "psychtoolbox" to lock
+% application memory into physical RAM, eliminating/minimizing
+% interference from the VM subsystem, and to run with realtime
+% priorities up to level 50. The group "psychtoolbox" is created
+% if it does not already exist.
+%
+% If the target system has a /etc/security/limits.d/ directory,
+% then a separate rule file is stored to that directory to
+% achieve the change without messing around with the limits.conf
+% file.
 %
 % root-less hardware access is achieved by copying a special
 % psychtoolbox.rules file into the /etc/udev/rules.d/ directory.
@@ -38,6 +45,11 @@ function PsychLinuxConfiguration
 if ~IsLinux
   return;
 end
+
+% Retrieve login username of current user:
+[ignore, username] = system('whoami');
+username = username(1:end-1);
+addgroup = 0;
 
 % Setup of /etc/udev/rules.d/psychtoolbox.rules file, if needed:
 
@@ -90,6 +102,9 @@ if needinstall && answer == 'y'
   end
 end
 
+% First the fallback implementation if /etc/security/limits.d/ does not
+% exist:
+if ~exist('/etc/security/limits.d/', 'dir')
 % Check if /etc/security/limits.conf has proper entries to allow memory locking
 % and real-time scheduling:
 fid = fopen('/etc/security/limits.conf');
@@ -106,17 +121,17 @@ rtpriook = 0;
 
 while ~feof(fid)
   fl = fgetl(fid);
-  fprintf('%s\n', fl);
+  % fprintf('%s\n', fl);
 
   if fl == -1
     continue;
   end
 
-  if ~isempty(strfind(fl, 'memlock')) && ~isempty(strfind(fl, 'unlimited')) && ~isempty(strfind(fl, '*')) && ~isempty(strfind(fl, '-'))
+  if ~isempty(strfind(fl, 'memlock')) && ~isempty(strfind(fl, 'unlimited')) && ~isempty(strfind(fl, '@psychtoolbox')) && ~isempty(strfind(fl, '-'))
     mlockok = 1;
   end
 
-  if ~isempty(strfind(fl, 'rtprio')) && ~isempty(strfind(fl, '50')) && ~isempty(strfind(fl, '*')) && ~isempty(strfind(fl, '-'))
+  if ~isempty(strfind(fl, 'rtprio')) && ~isempty(strfind(fl, '50')) && ~isempty(strfind(fl, '@psychtoolbox')) && ~isempty(strfind(fl, '-'))
     rtpriook = 1;
   end
 end
@@ -136,21 +151,22 @@ if ~(mlockok && rtpriook)
     drawnow;
 
     % Set amount of lockable memory to unlimited for all users:
-    [rc, msg] = system('sudo /bin/bash -c ''echo "*     -     memlock     unlimited" >> /etc/security/limits.conf''');
+    [rc, msg] = system('sudo /bin/bash -c ''echo "@psychtoolbox     -     memlock     unlimited" >> /etc/security/limits.conf''');
     if rc ~= 0
       fprintf('Failed! The error message was: %s\n', msg);
     end
 
     % Set allowable realtime priority for all users to 50:
-    [rc2, msg] = system('sudo /bin/bash -c ''echo "*     -     rtprio      50" >> /etc/security/limits.conf''');
+    [rc2, msg] = system('sudo /bin/bash -c ''echo "@psychtoolbox     -     rtprio      50" >> /etc/security/limits.conf''');
     if rc2 ~= 0
       fprintf('Failed! The error message was: %s\n', msg);
     end
 
+    % Must add a psychtoolbox user group:
+    addgroup = 1;
+
     if (rc == 0) && (rc2 == 0)
-      fprintf('\n\nSuccess! You should be able to use the Priority() command now without the need\n');
-      fprintf('to run Matlab or Octave as sudo root user. However, you must log out and then\n');
-      fprintf('login again once for the settings to take effect.\n\n');
+      fprintf('\n\nSuccess!\n\n');
     else
       fprintf('\n\nFailed! Maybe ask a system administrator for help?\n\n');
     end
@@ -159,9 +175,56 @@ else
   fprintf('\n\nYour system is already setup for use of Priority().\n\n');
 end
 
-fprintf('Finished. Assuming nothing went wrong, your system should now be ready\n');
-fprintf('for use with Psychtoolbox and without need to run as root user, ie. sudo root.\n');
+else
+% Realtime setup for systems with /etc/security/limits.d/ directory:
+% Simply install a ptb specific config file - a cleaner solution:
+if ~exist('/etc/security/limits.d/99-psychtoolboxlimits.conf', 'file')
+  fprintf('\n\nThe file /etc/security/limits.d/99-psychtoolboxlimits.conf is\n');
+  fprintf('not yet installed on your system. It allows painless realtime operation.\n');
+  answer = input('Should i install the file for you? [y/n] : ', 's');
+  if answer == 'y'
+    fprintf('I will try to install it now to your system. Please enter\n');
+    fprintf('now your system administrator password. You will not see any feedback.\n');
+    drawnow;
+    cmd = sprintf('sudo cp %s/PsychBasic/99-psychtoolboxlimits.conf /etc/security/limits.d/', PsychtoolboxRoot);
+    [rc, msg] = system(cmd);
+    if rc ~= 0
+      fprintf('Failed! The error message was: %s\n', msg);
+    else
+      fprintf('Success!\n\n');
+
+      % Must add a psychtoolbox user group:
+      addgroup = 1;
+    end
+  end
+end
+end
+
+% Need to create a Unix user group 'psychtoolbox' and add user to it?
+if addgroup
+  % This will create the psychtoolbox user group, unless the group
+  % already exists. In such a case it simply does nothing:
+  system('sudo groupadd --force psychtoolbox');
+
+  fprintf('I have created a new Unix user group called "psychtoolbox" on your system.\n');
+else
+  fprintf('\n\nYour system has a Unix user group called "psychtoolbox".\n');
+end
+
+fprintf('All members of that group can use the Priority() command now without the need\n');
+fprintf('to run Matlab or Octave as sudo root user.\n\n');
+fprintf('You need to add each user of Psychtoolbox to that group. You could do this\n');
+fprintf('with the user management tools of your system. Or you can open a terminal window\n');
+fprintf('and type the following command (here as an example to add yourself to that group):\n\n');
+fprintf('sudo usermod -a -G psychtoolbox %s\n\n', username);
+fprintf('After that, the new group member must log out and then login again for the\n');
+fprintf('settings to take effect.\n\n');
+
+fprintf('\nFinished. Your system should now be ready for use with Psychtoolbox.\n');
 fprintf('If you encounter problems, try rebooting the machine. Some of the settings only\n');
 fprintf('become effective after a reboot.\n\n\n');
+fprintf('Press any key to continue.\n');
+pause;
+fprintf('\n\n\n');
 
 return;
