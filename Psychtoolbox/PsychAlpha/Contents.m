82f6bf1a21e829c67a48f99895d28f512bb2573d
kleinerm <mario.kleiner@tuebingen.mpg.de>
2012-01-13 01:50:09 +0000

    Move KinectRawRecordingDemo() to PsychAlpha folder.
    It is very raw and sketchy, works but is ugly and
    not optimized.
    
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2433 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychAlpha/Contents.m
+++ Psychtoolbox/PsychAlpha/Contents.m
@@ -3,6 +3,7 @@
 %  help Psychtoolbox % For an overview, triple-click me & hit enter.
 %  help PsychAlpha   % For Alpha software, triple-click me & hit enter.
 %
+%   KinectRawRecordingDemo   - Half finished, demo on how to record Kinect data.
 %   PrintStruct              - Print structure & cell array to any depth
 %   SoundRecordingTest       - Test recording feature of PsychSound.
 %   SoundTrigger             -
