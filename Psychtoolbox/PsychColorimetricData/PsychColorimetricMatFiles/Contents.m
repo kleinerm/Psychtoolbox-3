a025ed4aac690cbee3f33f400d3249b6f3971bfd
brainard <brainard@cattell.psych.upenn.edu>
2012-03-19 19:21:59 +0000

    Fixed alphabetization.  Added T_ground and a note that we don't know what this is.
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2514 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
+++ Psychtoolbox/PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
@@ -87,7 +87,7 @@
 %   T_cones_ss10        - Stockman-Sharpe (2000) 10-degree cone fundamentals.  See CVRL database.
 %   T_cones_sp          - Smith-Pokorny cone fundamentals.
 %   T_DCS200            - Sensitivities of a Kodak DCS-200 color camera.
-%   T_ground            - Someone added this to the directory but not here.  What is it? (Ground squirrel?)
+%   T_ground            - Not entirely sure what this is, but it might be ground squirrel receptor sensitivities.
 %   T_Lanom             - Demarco et al. anomolous L cone sensitivity.
 %   T_log10coneabsorbance_ss - Stockman-Sharpe (2000) log10 LMS cone photopigment absorbance.
 %                       -   See CVRL database, CIE 170-1:2006, Table 6.6.
