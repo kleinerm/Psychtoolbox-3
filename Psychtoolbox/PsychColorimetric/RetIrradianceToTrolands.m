1f2299c90d72407d9898052f6a8dc99f64f5e22e
brainard <brainard@cattell.psych.upenn.edu>
2012-04-09 13:03:49 +0000

    Fix so that it runs.  A bit more testing wouldn't hurt.  DHB
    git-svn-id: https://psychtoolbox-3.googlecode.com/svn/trunk@2549 f5efbe48-b203-0410-9429-fc5e098e5a54

--- Psychtoolbox/PsychColorimetric/RetIrradianceToTrolands.m
+++ Psychtoolbox/PsychColorimetric/RetIrradianceToTrolands.m
@@ -1,35 +1,41 @@
 function [trolands] =...
 	RetIrradianceToTrolands(irradianceWatts, irradianceS, photopic, species, source)
 % [trolands] =...
-%  	RetIrradianceToTrolands(irradianceWatts, irradianceS, [photopic], [species],[ source])
+%  	RetIrradianceToTrolands(irradianceWatts,irradianceS,[photopic],[species],[source])
 %
-% Compute trolands from retinal irradiance in watts/um2-wlinterval
+% Compute trolands from retinal irradiance in watts/um2-wlinterval.  The answer is
+% returned in trolands/wlinterval and can be summed to get trolands.
 %
 % See Wyszecki and Stiles, 1982, p. 103 for the conversions.
 % 
 % Input variables: irradianceWatts - retinal irradiance in watts/um2-wlinterval.
 %                  irradianceS - the wavelength sampling information for the relativeSpectrum.
 %                  photopic - what kind of trolands: 'Photopic' (Default), 'JuddVos', 'Scotopic'. 
-%                  species - what species determins eye length: 'Human' (Default), 'Monkey'.
-%                  source - source for eye length estimate, passed directly to EyeLength and inherits its default.
+%                  species, source - passed directly to EyeLength to determine length of eye in mm.
+%                     These values inherit the default behaviors of EyeLength.
 %
-%	07/18/03  dhb  Wrote it.
+% 07/18/03  dhb  Wrote it.
+% 04/09/12  dhb  Debug.  This was apparently never quite finished.
+%                Improve comments.
 
 % Fill in default values
-if (nargin < 4 || isempty(photopic))
+if (nargin < 3 || isempty(photopic))
 	photopic = 'Photopic';
 end
-if (nargin < 5 || isempty(species))
-	species = 'Human';
+if (nargin < 4 || isempty(species))
+	species = [];
+end
+if (nargin < 5 || isempty(source))
+    source = [];
 end
 S = irradianceS;
 
 % Convert spectrum to watts/deg2-wlinterval
 eyeLengthMM = EyeLength(species,source);
-mmPerDeg = DegsToRetinalMM(1,eyeLengthMM);
-mm2PerDeg2 = mmPerDegree^2;
-irradianceMM2 = irradianceWatts*1e6
-irradianceDeg2 = irradianceDeg2*mm2PerDeg2;
+mmPerDeg = DegreesToRetinalMM(1,eyeLengthMM);
+mm2PerDeg2 = mmPerDeg^2;
+irradianceMM2 = irradianceWatts*1e6;
+irradianceDeg2 = irradianceMM2*mm2PerDeg2;
 
 % Load appropriate V_lambda for phot/scot
 switch (photopic)
