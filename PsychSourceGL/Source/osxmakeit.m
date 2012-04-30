4fee459efe90192438bc0a9a7a0814e7875dd7c9
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2012-04-30 17:32:51 +0200

    Initial build script and mex files for 64-Bit Matlab on MacOSX.
    Initially defeated by that utter piece of crap that Apple calls and IDE
    (XCode-4.3 -- and i though Xcode 2 was mediocre, this defines a new all
    low) and its various "really funny" bugs, resort to a makeshift M-File
    as "Makefile". Not that it is much better, because Matlab R2012a is a
    strong competitor for crappyness in details that count. Its mex file
    compiler function is so f$#@d that a stock bog-standard install of
    R2012a cannot even compile the most basic example mex files provided
    with Matlab on 10.7.3. Are these guys on drugs? Or do they just not do
    any QA testing?
    
    Anyway, it works, albeit painful, as a stop-gap measure. GetSecs,
    WaitSecs and IOPort compiled successfully for Matlab 64-Bit so far.
    Screen is wip.

---
+++ PsychSourceGL/Source/osxmakeit.m
@@ -0,0 +1,94 @@
+function osxmakeit(mode)
+% This is the MacOS/X version of makeit:
+
+if nargin < 1
+    mode = 0
+end;
+mode = 0
+
+fprintf('Building plugin type %i ...\n\n', mode);
+
+%  LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework CoreAudio" CFLAGS="-framework CoreServices -framework CoreFoundation -framework CoreAudio" CXXFLAGS="-framework CoreServices" -I/usr/include 
+
+if mode==0
+    %TODO: Depends -- All kind of shit...
+    % Build Screen:
+    % mex -v -outdir ../Projects/MacOSX/build -output Screen -largeArrayDims -DPTBMODULE_Screen -DPTB_USE_GSTREAMER -DPTBVIDEOCAPTURE_LIBDC LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework CoreAudio" CFLAGS="-framework CoreServices -framework CoreFoundation -framework CoreAudio" CXXFLAGS="-framework CoreServices" -I/usr/include -I/usr/include/gstreamer-0.10 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/libxml2 -ICommon/Base -ICommon/Screen -IOSX/Base -IOSX/Screen "Common/Base/*.cc" "OSX/Base/*.c" "OSX/Screen/*.c" "Common/Screen/*.c" "Common/Base/*.c" -lc -lrt -ldl -lGL -lGLU -lX11 -lXext -lgstreamer-0.10 -lgstbase-0.10 -lgstapp-0.10 -lgstinterfaces-0.10 -lgobject-2.0 -lgmodule-2.0 -lxml2 -lgthread-2.0 -lglib-2.0 -ldc1394 -lusb-1.0
+    mex -v -outdir ../Projects/MacOSX/build -output Screen -largeArrayDims -DPTBMODULE_Screen LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework ApplicationServices -framework CoreAudio -framework OpenGL -framework CoreVideo" CFLAGS="-framework CoreServices -framework CoreFoundation -framework ApplicationServices -framework CoreAudio -framework OpenGL -framework CoreVideo" CXXFLAGS="-framework CoreServices" -I/usr/include -ICommon/Base -ICommon/Screen -IOSX/Base -IOSX/Screen -IOSX/Fonts -IOSX/EthernetAddress "Common/Base/*.cc" "OSX/Base/*.c" "OSX/Screen/*.c" "OSX/Fonts/*FontGlue*.c" "OSX/Fonts/FontInfo.c" "OSX/EthernetAddress/*.c" "Common/Screen/*.c" "Common/Base/*.c" % -lgstreamer-0.10 -lgstbase-0.10 -lgstapp-0.10 -lgstinterfaces-0.10 -lgobject-2.0 -lgmodule-2.0 -lxml2 -lgthread-2.0 -lglib-2.0 -ldc1394 -lusb-1.0
+    unix(['mv ../Projects/MacOSX/build/Screen.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end;
+
+if mode==1
+    % Build GetSecs:
+    mex -v -outdir ../Projects/MacOSX/build -output GetSecs -largeArrayDims -DPTBMODULE_GetSecs LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework CoreAudio" CFLAGS="-framework CoreServices -framework CoreFoundation -framework CoreAudio" CXXFLAGS="-framework CoreServices" -I/usr/include -ICommon/Base -IOSX/Base -ICommon/GetSecs "Common/Base/*.cc" "OSX/Base/*.c" "Common/Base/*.c" "Common/GetSecs/*.c"
+    unix(['mv ../Projects/MacOSX/build/GetSecs.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end;
+
+if mode==2
+    % Build WaitSecs:
+    mex -v -outdir ../Projects/MacOSX/build -output WaitSecs -largeArrayDims -DPTBMODULE_WaitSecs LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework CoreAudio" CFLAGS="-framework CoreServices -framework CoreFoundation -framework CoreAudio" CXXFLAGS="-framework CoreServices" -I/usr/include -ICommon/Base -IOSX/Base -ICommon/WaitSecs "Common/Base/*.cc" "OSX/Base/*.c" "Common/Base/*.c" "Common/WaitSecs/*.c"
+    unix(['mv ../Projects/MacOSX/build/WaitSecs.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end;
+
+if mode==3
+    %TODO -- Depends: PortAudio
+    % Build PsychPortAudio:
+    mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ../Projects/MacOSX/build/ -output PsychPortAudio -largeArrayDims -DPTBMODULE_PsychPortAudio -ICommon/Base -IOSX/Base -ICommon/PsychPortAudio -ICommon/Screen Common/Base/*.cc OSX/Base/*.c Common/Base/*.c Common/PsychPortAudio/*.c /usr/local/lib/libportaudio.a -lc -lrt -lasound
+    unix(['mv ../Projects/MacOSX/build/PsychPortAudio.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end
+
+if mode==4
+    %TODO -- Depends: eyelink-SDK
+    % Build Eyelink:
+    mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ../Projects/MacOSX/build/ -output Eyelink -largeArrayDims -DPTBMODULE_Eyelink -ICommon/Base -IOSX/Base -ICommon/Eyelink -ICommon/Screen Common/Base/*.cc OSX/Base/*.c Common/Base/*.c Common/Eyelink/*.c -leyelink_core -lc -lrt
+    unix(['mv ../Projects/MacOSX/build/Eyelink.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end
+
+if mode==5
+    % Build IOPort:
+    mex -v -outdir ../Projects/MacOSX/build -output IOPort -largeArrayDims -DPTBMODULE_IOPort LDFLAGS="\$LDFLAGS -framework CoreServices -framework CoreFoundation -framework CoreAudio" CFLAGS="-framework CoreServices -framework CoreFoundation -framework CoreAudio" CXXFLAGS="-framework CoreServices" -I/usr/include  -ICommon/Base -IOSX/Base -ICommon/IOPort "Common/Base/*.cc" "OSX/Base/*.c" "Common/Base/*.c" "Common/IOPort/*.c"
+    unix(['mv ../Projects/MacOSX/build/IOPort.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end
+
+if mode==6
+    %TODO
+    % Build moglcore:
+    curdir = pwd;
+    cd('../../Psychtoolbox/PsychOpenGL/MOGL/source/')
+    try
+    mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ./ -output moglcore -DOSX -DGLEW_STATIC -largeArrayDims -I/usr/X11R6/include -L/usr/X11R6/lib -lc -lGL -lGLU -lglut moglcore.c gl_auto.c gl_manual.c glew.c mogl_rebinder.c
+    catch
+    end
+    unix(['mv moglcore.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+    cd(curdir);
+end
+
+if mode==7
+    %TODO -- Depends: libfreenect
+    % Build PsychKinectCore:
+    mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ../Projects/MacOSX/build/ -output PsychKinectCore -largeArrayDims -DPTBMODULE_PsychKinectCore -I/usr/include/libusb-1.0 -I/usr/include/libfreenect -ICommon/Base -IOSX/Base -ICommon/PsychKinect -ICommon/Screen Common/Base/*.cc OSX/Base/*.c Common/Base/*.c Common/PsychKinect/*.c -lc -lrt -lfreenect -lusb-1.0
+    unix(['mv ../Projects/MacOSX/build/PsychKinectCore.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end;
+
+if mode==8
+    %TODO -- Depends: HIDTOOLS
+    % Build PsychHID:
+    mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ../Projects/MacOSX/build/ -output PsychHID -largeArrayDims -DPTBMODULE_PsychHID -I/usr/include/libusb-1.0 -ICommon/Base -IOSX/Base -ICommon/PsychHID -IOSX/PsychHID -ICommon/Screen Common/Base/*.cc OSX/Base/*.c Common/Base/*.c Common/PsychHID/*.c OSX/PsychHID/*.c -lc -lrt -lusb-1.0 -lX11 -lXi
+    unix(['mv ../Projects/MacOSX/build/PsychHID.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+end;
+
+if mode==9
+    %TODO
+    % Build moalcore:
+    curdir = pwd;
+    cd('../../Psychtoolbox/PsychSound/MOAL/source/')
+    try
+       mex CFLAGS='$CFLAGS -fPIC -std=gnu99 -fexceptions' -v -outdir ./ -output moalcore -largeArrayDims -DOSX -lc -lopenal moalcore.c al_auto.c al_manual.c alm.c 
+    catch
+    end
+    unix(['mv ./moalcore.' mexext ' ' PsychtoolboxRoot 'PsychBasic/']);
+
+    cd(curdir);
+end;
+
+return;
