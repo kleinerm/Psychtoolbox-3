5968be952c967db5768ae63b499d4a66abb3d621
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2012-05-28 02:54:04 +0200

    Update various docs for the 3.0.10 series.
    Delete ExperimentalStuff.m -- Outdated since years!
    Update instructions for access to source code in UseTheSource with our
    new GitHub hosting location.
    Update Installation instructions for GStreamer and Kinect to take OSX
    into account, as well as the possibility to install via HomeBrew
    instead of binary installs.

--- Psychtoolbox/PsychDocumentation/UseTheSource.m
+++ Psychtoolbox/PsychDocumentation/UseTheSource.m
@@ -5,8 +5,10 @@
 % like Screen, WaitSecs, GetSecs, PsychHID, ...) then use the
 % following procedure.
 %
-% The Psychtoolbox source code is stored in a Subversion code
-% management system. To download the source code:
+% The Psychtoolbox source code is stored in a Git code repository at
+% GitHub.
+%
+% To download the source code:
 %
 % 1. Open a Unix terminal window on OS-X or Linux or a NT command
 %    shell on M$-Windows.
@@ -20,12 +22,12 @@
 % 3. The following command would download the current development branch of
 %    the full PTB distribution into the subdirectory /Users/kleinerm/MyPTB :
 %
-%    svn checkout http://psychtoolbox-3.googlecode.com/svn/trunk/Psychtoolbox/ /Users/kleinerm/MyPTB/
+%    svn checkout https://github.com/Psychtoolbox-3/Psychtoolbox-3/ /Users/kleinerm/MyPTB/
 %
 %    This command would download the full 'beta' distribution instead,
 %    which corresponds to the most recent official, tested, production release:
 %
-%    svn checkout http://psychtoolbox-3.googlecode.com/svn/beta/Psychtoolbox/ /Users/kleinerm/MyPTB/
+%    svn checkout https://github.com/Psychtoolbox-3/Psychtoolbox-3/branches/beta/ /Users/kleinerm/MyPTB/
 %
 %    Inside the /Users/kleinerm/MyPTB folder you would see the following
 %    subfolders:
@@ -42,7 +44,8 @@
 %
 %    trunk  - The code that is currently under development. May or may not
 %             compile and work at any given point in time, may or may not
-%             be consistent. For developers and early testers only!
+%             be consistent. For developers and early testers only! This is
+%             a view onto the GitHub Git "master" branch.
 %
 %    beta   - Source code corresponding to the current 'beta' release, derived
 %             from 'trunk'. This is what normal users should use, because
@@ -51,27 +54,23 @@
 %    If you type the command "PsychtoolboxVersion" in Matlab or Octave, it
 %    will tell you the flavor (trunk or beta) and SVN revision number of
 %    your currently installed PTB distribution. This allows you to locate
-%    the corresponding code in the SVN repository.
+%    the corresponding code in the GIT/SVN repository.
 %
-%    Be aware that you can only download code from the repository. If
-%    you want to participate in PTB's development you'll need write access
-%    to the repository. Contact David Brainard or Mario Kleiner if you want
-%    to apply for write access. Make yourself familiar with the Subversion
-%    toolchain or with Git if you want to do serious work.
+%    Be aware that you can only download code from the repository. If you
+%    want to participate in PTB's development contact Mario Kleiner or
+%    David Brainard for instructions. Make yourself familiar with Git if
+%    you want to do serious work.
 %
-%    An even more recent view of current development can be found on GitHub,
-%    where our master and experimental branches are hosted. If you intend
-%    to contribute to C code development or other complex parts of the
-%    software, we will require you to use git instead of Subversion and
+%    An even more recent view of current development can be found on
+%    GitHub, where our master and experimental branches are hosted. If you
+%    intend to contribute to C code development or other complex parts of
+%    the software, we will require you to use git instead of Subversion and
 %    you will work with the git repository at GitHub.
 %
-%    You can browse the source code and all development logs via a
-%    web interface: http://code.google.com/p/psychtoolbox-3/source/list
-%
 %    For almost all source code, i.e., unless otherwise stated in the
-%    source files themselves or accompanying documentation, the
-%    MIT license applies. See the file License.txt in the Psychtoolbox
-%    root folder for more details.
+%    source files themselves or accompanying documentation, the MIT license
+%    applies. See the file License.txt in the Psychtoolbox root folder for
+%    more details.
 %
 %    Components & Libraries which are not covered by the MIT license, but
 %    other MIT compatible free software licenses will carry individual
