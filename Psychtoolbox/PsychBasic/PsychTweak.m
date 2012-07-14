6d520e12b3a323503e336b070b2d656746ceb2ff
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2012-07-14 03:22:56 +0200

    PsychTweak: Add GStreamer tweaks.
    Add setup for GStreamer debug level of verbosity, dumping of media
    processing graphs, and multi-threading behaviour.

--- Psychtoolbox/PsychBasic/PsychTweak.m
+++ Psychtoolbox/PsychBasic/PsychTweak.m
@@ -26,6 +26,30 @@ function varargout = PsychTweak(cmd, varargin) %#ok<STOUT>
 % the toolbox itself, or maybe for some simple demos.
 %
 %
+% Debugging of GStreamer based functions (Videocapture/recording, Movie
+% playback and creation):
+%
+% PsychTweak('GStreamerDebug', debugconfig);
+% -- Select level of verbosity for GStreamer low-level debug output.
+% See <http://docs.gstreamer.com/display/GstSDK/Basic+tutorial+11%3A+Debugging+tools>
+% for explanation of debug options.
+%
+%
+% PsychTweak('GStreamerDumpFilterGraph', targetDirectory);
+% -- Ask GStreamer to dump a snapshot of its current media processing
+% pipeline into a .dot file at each pipeline state change. These .dot files
+% can be read by open-source tools like GraphViz and turned into
+% visualizations of the complete structure and state of the media
+% processing pipeline. 'targetDirectory' must be the full path to an
+% existing directory where the .dot files should be written to.
+%
+% PsychTweak('GStreamerDecodeThreads', numThreads);
+% -- Ask GStreamer to use up to 'numThreads' processing threads for video
+% decoding during movie playback. By default, GStreamer decides itself,
+% based on a given hardware configuration, e.g., the number of processor
+% cores in a system.
+%
+%
 % MS-Windows only tweaks:
 % -----------------------
 %
@@ -145,6 +169,52 @@ if strcmpi(cmd, 'PrepareForVirtualMachine')
         Screen('Preference', 'ConserveVRAM', bitor(Screen('Preference', 'ConserveVRAM'), 64));
     end
     
+    return;
+end
+
+if strcmpi(cmd, 'GStreamerDebug')
+    if length(varargin) < 1
+        error('Must provide a GStreamer debug config string.');
+    end
+    
+    val = varargin{1};
+    if ~ischar(val)
+        error('Must provide a string as argument!');
+    end
+    
+    setenv('GST_DEBUG', val);
+    return;
+end
+
+if strcmpi(cmd, 'GStreamerDumpFilterGraph')
+    if length(varargin) < 1
+        error('Must provide a target directory for GStreamers filtergraph dump.');
+    end
+    
+    val = varargin{1};
+    if ~ischar(val)
+        error('Must provide a string as argument!');
+    end
+    
+    if ~exist(val, 'dir')
+        error('Target directory for GStreamer dumps [%s] does not exist!', val);
+    end
+    
+    setenv('GST_DEBUG_DUMP_DOT_DIR', val);
+    return;
+end
+
+if strcmpi(cmd, 'GStreamerDecodeThreads')
+    if length(varargin) < 1
+        error('Must provide number of video decoder threads.');
+    end
+    
+    val = varargin{1};
+    if ~isscalar(val) || ~isnumeric(val)
+        error('Must provide an integer as argument!');
+    end
+    
+    setenv('PSYCHTOOLBOX_MAX_VIDEODECODER_THREADS', sprintf('%i', round(val)));
     return;
 end
 
