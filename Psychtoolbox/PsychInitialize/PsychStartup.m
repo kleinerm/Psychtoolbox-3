7e94eee1021e9a0885498326ebac4ba9906febe3
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2012-09-14 02:04:49 +0200

    Windows 64Bit: Add GStreamer detection and init code for 64-Bit Matlab.
    On 64-Bit Matlab for Windows, installation of the 64-Bit GStreamer SDK
    runtime from www.gstreamer.com is mandatory for Psychtoolbox Screen()
    mex file to work at all.
    
    Therefore we add detection & setup logic in PsychStartup.m, which
    detects on Win64 if GStreamer is properly installed and sets the linker
    path properly for Screen() to work later on, if so. Otherwise it gives
    warnings if GStreamer SDK is unavailable.
    
    PsychStartup.m gets implanted in a Matlab startup.m file (the startup.m
    file is automatically created if neccessary), so it gets run at start
    of each 64-Bit Matlab session.
    
    The PsychtoolboxPostInstallRoutine now checks if PsychStartup is
    implanted in startup.m via IsPsychStartupImplantedInStartup(), and
    performs the implantation if neccessary.
    
    Currently we only need this voodoo on 64-Bit Matlab for 64-Bit
    MS-Windows, as GStreamer is properly demand-loaded on all other system
    configurations.
    
    This should make the 64-Bit PTB for Windows fully functional.

---
+++ Psychtoolbox/PsychInitialize/PsychStartup.m
@@ -0,0 +1,50 @@
+function PsychStartup
+% PsychStartup -- Perform Psychtoolbox related setup at Matlab/Octave startup.
+%
+% This performs setup of Matlab or Octave at session startup for
+% Psychtoolbox.
+%
+% On MS-Windows with 64-Bit Matlab it currently detects if the GStreamer
+% SDK runtime is installed, as this is required for Screen() to work. It
+% performs GStreamer setup, or outputs a warning if the runtime is missing.
+%
+% This function is normally called from the startup.m Matlab startup file.
+%
+
+% History:
+% 14.09.2012  mk  Written.
+
+% Try-Catch protect the function, so Matlab startup won't fail due to
+% errors in this function:
+try
+    % Setup for 64-Bit Matlab on MS-Windows:
+    if IsWin(1) && ~IsOctave
+        % Need to assign a proper install location for the
+        % www.gstreamer.com GStreamer-SDK runtime libraries, otherwise
+        % loading of the Screen() MEX file would fail due to unresolved
+        % link dependencies:
+        
+        % Find path to SDK-Root folder:
+        sdkroot = [getenv('GSTREAMER_SDK_ROOT_X86_64') 'bin'];
+        
+        if isempty(sdkroot)
+            fprintf('\nPsychStartup: Path to GStreamer runtime is undefined! This probably means that\n');
+            fprintf('PsychStartup: the 64-Bit GStreamer SDK from www.gstreamer.com is not installed.\n');
+            fprintf('PsychStartup: The Psychtoolbox Screen() mex file will fail to work until you fix this!\n');
+            fprintf('PsychStartup:  Read ''help GStreamer'' for instructions.\n\n');
+        else
+            fprintf('\nPsychStartup: Adding path of installed GStreamer runtime to library path. [%s]\n', sdkroot);
+            
+            % Get current path:
+            path = getenv('PATH');
+            
+            % Prepend sdkroot to path:
+            newpath = [sdkroot ';' path];
+            setenv('PATH', newpath);
+        end
+    end
+catch %#ok<*CTCH>
+    fprintf('PTB-WARNING: Call to PsychStartup() failed!!\n');
+end
+
+end
