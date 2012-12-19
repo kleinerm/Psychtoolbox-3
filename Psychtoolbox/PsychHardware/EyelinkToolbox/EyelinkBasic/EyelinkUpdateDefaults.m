35fee8f22225df24206b97e40586148738a4d218
opticka <opticka@optickaI>
2012-12-19 22:32:20 +0000

    Although you are supposed to be allowed to pass custom callbacks, the default callback is hardwired when updating the defaults, therefore all other callbacks will fail as the persistent variables for screen identifier etc are never set up. This fevals the callback function as long as it is a valid file.

--- Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkBasic/EyelinkUpdateDefaults.m
+++ Psychtoolbox/PsychHardware/EyelinkToolbox/EyelinkBasic/EyelinkUpdateDefaults.m
@@ -1,30 +1,22 @@
 function EyelinkUpdateDefaults(el)
-
 % USAGE = EyelinkUpdateDefaults(el) 
 %
 % This function passes changes to the calibration defaults structure
 % to the callback function. To be called after EyelinkInitDefaults if any
 % changes are made to the output structure of that function.
 %
-% 
-% el=EyelinkInitSRDefaults(window);
+% el=EyelinkInitDefaults(window);
 % el.backgroundColour = BlackIndex(window);
 % EyelinkUpdateDefaults(el);
 %
-%
-%
-
 % 27-1-2011 NJ created 
+% 19-12-2012 IA Fix hardcoded callback 
 
-
-
-% Window assigned?
-%if ~isempty(el.window) & ~isempty(el.callback) %#ok<AND2>
-if ~isempty(el.callback)
-    % Yes. Assign it to our dispatch callback:
-%     EyelinkDispatchCallback(el);
-    PsychEyelinkDispatchCallback(el);
+if ~isempty(el.callback) && exist(el.callback,'file')
+	
+	%evaluate the callback function with the new el defaults
+	feval(el.callback, el); 
+		
 end
-
 
 end
