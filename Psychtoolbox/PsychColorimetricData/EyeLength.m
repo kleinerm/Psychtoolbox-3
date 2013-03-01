3447551b29bee6093eb6ab3b89c4151f8da16823
David Brainard <brainard@psych.upenn.edu>
2013-03-01 17:32:28 -0500

    Continue working on radiometric and MPE tests.
    Also modified EyeLength to make it a bit more flexible.
    
    DHB

--- Psychtoolbox/PsychColorimetricData/EyeLength.m
+++ Psychtoolbox/PsychColorimetricData/EyeLength.m
@@ -13,7 +13,7 @@ function eyeLengthMM = EyeLength(species,source)
 % Supported sources:
 %   LeGrand (Human, 16.6832 mm, Default)
 %   Rodieck (Human, 16.1 mm)
-%   17 (Human, 17 mm)
+%   Gulstrand (Human, 17 mm)
 %   PerryCowey (Rhesus)
 %   Packer (Rhesus)
 %   PennDog (Dog)
@@ -26,9 +26,23 @@ function eyeLengthMM = EyeLength(species,source)
 % Passing None is appropriate as an error check -- if a calculation
 % uses the eye length when none is passed, NaN's will show up in
 % the answer.
+% 
+% Finally, if you pass a decimal number as a string, this value
+% will be returned.  Useful for passing arbitrary numbers through
+% routines that rely on this function.
 %
 % 7/15/03  dhb  Wrote it.
 % 2/27/13  dhb  Added 17 mm option
+% 3/1/13   dhb  Changed '17' to 'Gulstrand'
+%          dhb  Added option of passing a number as a string.
+
+% Check if a direct value was passed as a string.  If so,
+% ignore everything else and return it.
+directVal = str2num(source);
+if (~isempty(directVal))
+    eyeLengthMM = directVal;
+    return;
+end
 
 % Fill in defaults
 if (nargin < 1 || isempty(species))
@@ -71,8 +85,8 @@ switch (source)
 				error(sprintf('%s estimates not available for species %s',source,species));
         end
     
-    % I think some people use 17 mm
-    case {'17'}
+    % Gulstrand model eye, as described by Delori et. al., 2007, JOSA A, 24, pp. 1250-1265.
+    case {'Gulstrand'}
         switch (species)
 			case {'Human'}
 				eyeLengthMM = 17;
