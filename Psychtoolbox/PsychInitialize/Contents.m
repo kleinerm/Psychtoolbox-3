3f0db1b63361c72fddd36ffa3e9181451d5ead39
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2013-07-10 02:12:00 +0200

    Priority(): Improve help text, remove OSX 10.4 support.
    Remove handling if OSX Unix updated process, ie. starting and stopping
    the updated daemon. This was needed on OSX 10.4.7 and earlier, no
    longer needed on later OSX versions. As we don't support 10.4 anymore,
    this is now moot.
    
    Delete all functions related to handling of the Unix updated process.

--- Psychtoolbox/PsychInitialize/Contents.m
+++ Psychtoolbox/PsychInitialize/Contents.m
@@ -2,9 +2,4 @@
 %
 % help Psychtoolbox     % For an overview, triple-click me & hit enter.
 %
-%  
-%    finish                                   - Restores the OS X update process on MATLAB exit.
-%    IsStartUpdateImplantedInFinish           - True if finish.m will restart the update process.
-%    PsychStartup                             - Perform Psychtoolbox related setup at Matlab/Octave startup.
-
-
+%    PsychStartup       - Perform Psychtoolbox related setup at Matlab/Octave startup.
