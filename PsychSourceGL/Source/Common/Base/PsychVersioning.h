c4a6798cc7728b9ed9a0f64e0ed30bd2fb8ce27a
Mario Kleiner <mario.kleiner.de@gmail.com>
2014-04-06 03:40:46 +0200

    Psychtoolbox: Reduce module version number to 3.0.11 for final 3.0.11 release.

--- PsychSourceGL/Source/Common/Base/PsychVersioning.h
+++ PsychSourceGL/Source/Common/Base/PsychVersioning.h
@@ -1,24 +1,23 @@
 /*
-	PsychVersioning.h		
-    
-	AUTHORS:
- 
-		Allen.Ingling@nyu.edu		  awi 
-		mario.kleiner at tuebingen.mpg.de mk
+    PsychVersioning.h
 
-	PLATFORMS:
+    AUTHORS:
 
-		All
-		
+        Allen.Ingling@nyu.edu		  awi
+        mario.kleiner at tuebingen.mpg.de mk
 
-	HISTORY:
-	
-		3/13/03		awi		Created.
-		11/3/04		awi		Incremented version number to 1.0.2 from 1.0.1 for 3 November release.
-		11/18/04	awi		changed "Matlab" to "MATLAB"
-		10/05/06	mk		Incremented version number to 3.0.8.
-		04/11/11	mk		Incremented version number to 3.0.9 to mark license change and
-							broken backwards compatibility wrt. video capture etc.
+    PLATFORMS:
+
+        All
+
+    HISTORY:
+
+        3/13/03     awi     Created.
+        11/3/04     awi     Incremented version number to 1.0.2 from 1.0.1 for 3 November release.
+        11/18/04    awi     changed "Matlab" to "MATLAB"
+        10/05/06    mk      Incremented version number to 3.0.8.
+        04/11/11    mk      Incremented version number to 3.0.9 to mark license change and
+                            broken backwards compatibility wrt. video capture etc.
 
 */
 
@@ -29,18 +28,18 @@
 #include "Psych.h"
 
 //these are all constants defined only for reporting versioning.  They are returned by Module('Version')
-#define PSYCHTOOLBOX_MAJOR_VERSION				3
-#define PSYCHTOOLBOX_MINOR_VERSION				0
-#define PSYCHTOOLBOX_POINT_VERSION				12
+#define PSYCHTOOLBOX_MAJOR_VERSION              3
+#define PSYCHTOOLBOX_MINOR_VERSION              0
+#define PSYCHTOOLBOX_POINT_VERSION              11
 //If you fork my source please change this name.  - awi 
-#define PSYCHTOOLBOX_PROJECT_NAME				"OpenGL Psychtoolbox"
+#define PSYCHTOOLBOX_PROJECT_NAME               "OpenGL Psychtoolbox"
 #if PSYCH_SYSTEM == PSYCH_OSX
 //Placeholder for the OS X version at build time. 
-#define PSYCHTOOLBOX_OS_NAME					"Apple OS X" 
+#define PSYCHTOOLBOX_OS_NAME                    "Apple OS X"
 #endif
 #if PSYCH_SYSTEM == PSYCH_WINDOWS
 //Placeholder for the M$-Windows version at build time.
-#define PSYCHTOOLBOX_OS_NAME					"Microsoft Windows"  
+#define PSYCHTOOLBOX_OS_NAME                    "Microsoft Windows"
 #endif
 #if PSYCH_SYSTEM == PSYCH_LINUX
 //Placeholder for the GNU/Linux version at build time.
