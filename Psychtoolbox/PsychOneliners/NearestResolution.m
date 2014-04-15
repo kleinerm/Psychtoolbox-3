e012374041a05220f3d68f6cff28ded24a0e82ea
Mario Kleiner <mario.kleiner.de@gmail.com>
2014-04-15 22:40:46 +0200

    ResolutionTest, NearestResolution: Handle multiple outputs per screen on Linux.

--- Psychtoolbox/PsychOneliners/NearestResolution.m
+++ Psychtoolbox/PsychOneliners/NearestResolution.m
@@ -1,6 +1,6 @@
-function res=NearestResolution(screenNumber,width,height,hz,pixelSize)
+function res=NearestResolution(screenNumber,width,height,hz,pixelSize,outputId)
 % res=NearestResolution(screenNumber,[width,height,hz,pixelSize])
-% res=NearestResolution(screenNumber [,width][,height][,hz][,pixelSize])
+% res=NearestResolution(screenNumber [,width][,height][,hz][,pixelSize][,outputId])
 % res=NearestResolution(screenNumber,desiredRes)
 %
 % Finds the available screen resolution most similar (in log cartesian space) to that
@@ -8,7 +8,11 @@ function res=NearestResolution(screenNumber,width,height,hz,pixelSize)
 % If you specify pixelSize then the returned res will specify pixelSize. Typically
 % you'll use the returned "res" to set your screen's resolution:
 %
-% SetResolution('Resolution', screenNumber, res)
+% SetResolution(screenNumber, res)
+%
+% Note: On Linux, if more than one video display output is connected to
+% a given screenNumber, you must specify the optional 'outputId' parameter
+% to select the output for which you want a match.
 %
 % Also see SetResolution, ResolutionTest, and Screen Resolution and Resolutions.
 
@@ -21,12 +25,17 @@ function res=NearestResolution(screenNumber,width,height,hz,pixelSize)
 % 6/6/02  dgp Accept res instead of parameter list.
 % 9/23/07 mk  Adapted for Psychtoolbox-3.
 % 2/28/08 mk  Fix parsing, error handling and documentation...
-% 12/28/12 mk Fix bug reported by Stéphane Rainville on GitHub (issue #86).
+% 12/28/12 mk Fix bug reported by Stï¿½phane Rainville on GitHub (issue #86).
+% 04/15/14 mk Allow selection of outputId for which to match.
 
-if nargin<2 || nargin>5
+if nargin<2 || nargin>6
     error(sprintf('%s\n%s','USAGE: res=NearestResolution(screenNumber,[width,height,hz,pixelSize])',...
         '           res=NearestResolution(screenNumber [,width][,height][,hz][,pixelSize])',...
         '           res=NearestResolution(screenNumber,desiredRes)')); %#ok<*SPERR>
+end
+
+if nargin<6
+    outputId = [];
 end
 
 if nargin<5
@@ -104,7 +113,8 @@ if ~isscalar(screenNumber) || ~ismember(screenNumber, Screen('Screens'))
     error(sprintf('Invalid screenNumber %d.',screenNumber));
 end
 
-res=Screen(screenNumber,'Resolutions'); % get a list of available resolutions
+% Get a list of available resolutions on output outputId:
+res=Screen('Resolutions', screenNumber, outputId);
 
 wish.width=width;
 wish.height=height;
@@ -117,11 +127,6 @@ end
 
 [x,i]=min(d); %#ok<ASGLU>
 res=res(i);
-
-%if isfinite(wish.pixelSize)
-%	[dp,ip]=min(log10(wish.pixelSize ./ res.pixelSizes) .^ 2);
-%	res.pixelSize=res.pixelSizes(ip);
-%end
 
 if isempty(wish.pixelSize) || ~isfinite(wish.pixelSize)
     res.pixelSize = [];
@@ -148,11 +153,5 @@ end
 if ~isempty(a.hz) && isfinite(a.hz) && ~isempty(b.hz) && isfinite(b.hz) && (b.hz > 0)
     d=d+log10(a.hz/b.hz)^2;
 end
-
-% Totally ignore pixelSize for matching: This has no meaning for PTB-3's
-% OpenGL implementation and is always returned as [] in the end:
-%if isfinite(a.pixelSize)
-%	d=d+min(log10(a.pixelSize ./ b.pixelSizes) .^ 2);
-%end
 
 return
