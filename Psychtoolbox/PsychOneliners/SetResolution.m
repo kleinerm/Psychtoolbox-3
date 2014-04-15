b7b8be2c7af4932278933474b0434891639a75f6
Mario Kleiner <mario.kleiner.de@gmail.com>
2014-04-16 00:22:08 +0200

    SetResolution: Make useable on Linux + single-output x-screen at least.
    SetResolution can't handle multiple video outputs per screen
    in a reasonable manner, as possible on Linux, where one x-screen
    can have multiple outputs.
    
    Make it distinguish 1 vs. multi-output setups and provide
    proper behavior on single-output screens. On multi-output
    screens on Linux, warn user about limited functionality and
    do the best it can.

--- Psychtoolbox/PsychOneliners/SetResolution.m
+++ Psychtoolbox/PsychOneliners/SetResolution.m
@@ -12,11 +12,11 @@ function oldRes=SetResolution(screenNumber,width,height,hz,pixelSize)
 % "pixelSize" 8, 16, 24, or 32 bits and defaults to current pixelSize.
 % Returns the current resolution as "oldRes".  
 % 
-% 		oldRes=SetResolution(0,1024,768,75);
-% 		w=Screen(0,'OpenWindow');
-% 		Screen(w,'PutImage',image);
-% 		Screen(w,'Close');
-% 		SetResolution(0, oldRes);
+%   oldRes=SetResolution(0,1024,768,75);
+%   w=Screen(0,'OpenWindow');
+%   Screen(w,'PutImage',image);
+%   Screen(w,'Close');
+%   SetResolution(0, oldRes);
 % 
 % To display a list of available resolutions, try ResolutionTest. Also see
 % NearestResolution, ResolutionTest, and Screen('Resolution')
@@ -25,6 +25,13 @@ function oldRes=SetResolution(screenNumber,width,height,hz,pixelSize)
 % NOTE: Apple has all the new LCD screens return a frame rate of 0, so
 % we treat that value as a special case. A request for "hz" of NaN will
 % match only with a frame rate of 0.
+%
+% NOTE: On Linux this only works as you'd expect if there is only one
+% video output connected to a Psychtoolbox screen / X-Screen. On a
+% setup with multiple outputs per screen, this will only change the
+% size of the framebuffer, but not the resolution of the actual displays!
+% Use Screen('ConfigureDisplay', 'Scanout', ...); to change settings on
+% such a multi-display per screen setup on Linux.
 % 
 % Originally written by Sabina Wolfson.
 
@@ -37,72 +44,83 @@ function oldRes=SetResolution(screenNumber,width,height,hz,pixelSize)
 % 6/6/02  dgp Accept res instead of parameter list.
 % 6/7/02  dgp Hz value of NaN matches NaN.
 % 9/23/07 mk  Adapted for Psychtoolbox-3.
+% 4/15/14 mk  Adapted for multi-output per x-screen setups on Linux.
 
 if nargin<2 || nargin>5
-	error(sprintf('%s\n%s','USAGE: oldRes=SetResolution(screenNumber,width,height,[hz],[pixelSize])',...
-	                   '           oldRes=SetResolution(screenNumber,res)'));
+    error(sprintf('%s\n%s','USAGE: oldRes=SetResolution(screenNumber,width,height,[hz],[pixelSize])',...
+                           '       oldRes=SetResolution(screenNumber,res)'));
 end
 
-if screenNumber<0 || screenNumber>max(Screen('Screens'))
-	error(sprintf('Invalid screenNumber %d.',screenNumber));
+if ~ismember(screenNumber, Screen('Screens'))
+    error(sprintf('Invalid screenNumber %d.',screenNumber));
 end
 
-oldRes=Screen(screenNumber,'Resolution');
+oldRes = Screen('Resolution', screenNumber);
 
-if nargin<5
-	pixelSize=oldRes.pixelSize;
+if nargin<5 || isempty(pixelSize)
+    pixelSize=oldRes.pixelSize;
 end
 
-if nargin<4
-	hz=oldRes.hz;
-end
-
-if isempty(hz)
-	hz=oldRes.hz;
+if nargin<4 || isempty(hz)
+    hz=oldRes.hz;
 end
 
 if nargin==2 && isa(width,'struct')
-	res=width;
-	if isfield(res,'width')
-		width=res.width;
-	end
-	if isfield(res,'height')
-		height=res.height;
-	end
-	if isfield(res,'hz')
-		hz=res.hz;
-	end
-	if isfield(res,'pixelSize')
-		pixelSize=res.pixelSize;
-	end
+    res=width;
+    if isfield(res,'width')
+        width=res.width;
+    end
+    
+    if isfield(res,'height')
+        height=res.height;
+    end
+    
+    if isfield(res,'hz')
+        hz=res.hz;
+    end
+    
+    if isfield(res,'pixelSize')
+        pixelSize=res.pixelSize;
+    end
 end
 
 if ~exist('height','var')  || isempty(height) || ~isfinite(height)
-	error('height (in pixels) must be specified.');
+    error('height (in pixels) must be specified.');
 end
 
 if ~exist('width','var') || isempty(width) || ~isfinite(width)
-	error('width (in pixels) must be specified.');
+    error('width (in pixels) must be specified.');
 end
-
-res=Screen(screenNumber,'Resolutions'); % get a list of available resolutions
 
 if isnan(hz)
     hz = 0;
 end
 
-resIndex=find([res.width]==width & [res.height]==height & [res.hz]==hz);
-if isempty(resIndex)
-	error(sprintf('Can''t find a resolution of %g x %g x %g Hz. Resolution remains unchanged.',width,height,hz));
+% One display output per screen? Then we can validate the settings.
+if Screen('ConfigureDisplay', 'NumberOutputs', screenNumber) < 2
+    res = Screen('Resolutions', screenNumber, 0); % get a list of available resolutions
+
+    resIndex=find([res.width]==width & [res.height]==height & [res.hz]==hz);
+    if isempty(resIndex)
+        error(sprintf('Can''t find a resolution of %g x %g x %g Hz. Resolution remains unchanged.',width,height,hz));
+    end
+    
+    nres = res(resIndex(1));
+else
+    % Multiple outputs per screen. Can't validate, but take settings at face value and hope for the best:
+    nres.width = width;
+    nres.height = height;
+    nres.hz = hz;
+    nres.pixelSize = pixelSize;
+    warning('SetResolution: Only changing virtual resolution of multi-display screen %i!\n');
 end
 
-nres = res(resIndex(1));
 if isempty(pixelSize) || ~isfinite(pixelSize)
     nres.pixelSize = [];
 else
     nres.pixelSize = pixelSize;
 end
 
-oldRes=Screen('Resolution', screenNumber, nres.width, nres.height, nres.hz, nres.pixelSize);
+oldRes = Screen('Resolution', screenNumber, nres.width, nres.height, nres.hz, nres.pixelSize);
 
 return;
