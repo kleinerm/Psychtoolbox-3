b8005762c9cde850d8481a8810b0dc472b775157
David Brainard <brainard@psych.upenn.edu>
2014-06-02 11:54:07 -0400

    Change constants to represent 2:1 assumed L:M ratio.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Psychtoolbox/PsychColorimetric/ComputePhotopigmentBleaching.m
+++ Psychtoolbox/PsychColorimetric/ComputePhotopigmentBleaching.m
@@ -22,11 +22,11 @@ function [fractionBleached] = ComputePhotopigmentBleaching(intensity,receptortyp
 % None-the-less, it seems nice to have this routine written so that it will return a number
 % if you give it intensity either in trolands or in isomerizations/cone-sec.  For 560 nm
 % light and the CIE 10 deg fundamentals, I compute that 1 td is 137 isomerizations/cone-sec
-% for L cones and 110 isomerizations/cone-sec for M cones.  Take the average value of 123.5
-% and multiply by (10.^4.3) to get a half-bleach constant in isomerizations/cone-sec of 
-% 2.46e+06 (6.39 log10 isomerizations/cone-sec).  [Computations done 5/23/14 using
-% IsomerizationsInEyeDemo and setting the fundamentals to 'CIE10deg' and wavelength to 560 nm
-% by hand in the code.]
+% for L cones and 110 isomerizations/cone-sec for M cones.  Take the weighted average value
+% of (2*L + 1*M) = 128 and multiply by (10.^4.3) to get a half-bleach constant in
+% isomerizations/cone-sec of  2.55e+06 (6.4 log10 isomerizations/cone-sec).
+% [Computations done 6/2/14 using IsomerizationsInEyeDemo and setting the fundamentals to 'CIE10deg' and wavelength to 560 nm
+% by hand in the code.  These are for the 'Boynton' source.]
 %
 % [ASIDE: I used 10 deg fundamentals because I figure that Rushton's measurements are based
 % on a fairly large field.  Because the macular pigment absorbs a fair amount of light, 
@@ -65,6 +65,7 @@ function [fractionBleached] = ComputePhotopigmentBleaching(intensity,receptortyp
 %
 % 05/23/14 dhb  Wrote it.
 % 05/26/14 dhb  Clean up.
+% 06/02/14 dhb  Take isomerizations number based on 2:1 L:M assumed ratio.
 
 %% Specify receptor type
 if (nargin < 2 || isempty(receptortype))
@@ -90,7 +91,7 @@ switch (receptortype)
                     case 'trolands'
                         Izero = 10^4.3;
                     case 'isomerizations'
-                        Izero = 10^6.39;
+                        Izero = 10^6.4;
                     otherwise
                         error('Unkown input units specified');
                 end
@@ -112,7 +113,7 @@ end
 TEST  = 0;
 if (TEST)
     trolands = logspace(0,7,1000);
-    isomerizations = 123.5*trolands;
+    isomerizations = 128*trolands;
     fractionBleached = ComputePhotopigmentBleaching(trolands,'cones','trolands','Boynton');
     fractionBleached1 = ComputePhotopigmentBleaching(isomerizations,'cones','isomerizations','Boynton');
     figure; clf; hold on;
