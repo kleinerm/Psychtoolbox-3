ea4400ef0ae713f2b618c4084d001fee73fbd6b8
David Brainard <brainard@psych.upenn.edu>
2014-07-25 09:53:38 -0400

    Remove dependency on BrainardLab toolbox.  This means a bit less gets stripped off the path, but I think that is OK for us and certainly better for everyone else.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Psychtoolbox/PsychOneliners/AddToMatlabPathDynamically.m
+++ Psychtoolbox/PsychOneliners/AddToMatlabPathDynamically.m
@@ -2,9 +2,9 @@ function AddToMatlabPathDynamically(directory)
 % AddToMatlabPathDynamically(directory)
 %
 % Add the directory and its subdirectories to Matlab's path, dynamically,
-% and then strip standard Brainard lab temporary paths.
+% with strippint out of .svn and .git directories.
 % 
-% Usefor for putting routines onto path that are specifict to a particular
+% Use for for putting routines onto path that are specifict to a particular
 % project, without them staying around and clogging up the name space.
 %
 % Typical usages:
@@ -17,10 +17,13 @@ function AddToMatlabPathDynamically(directory)
 %   AddToMatlabPathDynamically( fileparts(which(mfilename))); 
 %
 % 7/12/13  dhb  Wrote it.
+% 7/25/14  dhb  Make independent of BrainardLab idiosyncracies.
 
 %% Dynamically add the program code to the path if it isn't already on it.
 if isempty(strfind(path, directory))
 	fprintf('- Adding %s dynamically to the path...', directory);
-	addpath(RemoveTMPPaths(genpath(directory),false), '-end');
+    thePath = RemoveMatchingPaths(genpath(directory),[filesep '.svn']);
+    thePath = RemoveMatchingPaths(thePath,[filesep '.git']);
+	addpath(thePath, '-end');
 	fprintf('Done\n');
 end
