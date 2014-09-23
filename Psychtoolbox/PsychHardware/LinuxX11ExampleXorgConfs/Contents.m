13dfd6a778aa7494a6101250f410b8752637c8bb
Mario Kleiner <mario.kleiner.de@gmail.com>
2014-09-23 22:05:54 +0200

    Add new folder Psychtoolbox/PsychHardware/LinuxX11ExampleXorgConfs
    Contains various xorg.conf files which were successfully tested
    at some point, so can serve as a reference for non-standard
    X11 display setups on Linux.

---
+++ Psychtoolbox/PsychHardware/LinuxX11ExampleXorgConfs/Contents.m
@@ -0,0 +1,25 @@
+% Psychtoolbox/PsychHardware/LinuxX11ExampleXorgConfs
+%
+% This folder contains various example xorg.conf files
+% for use on Linux with the X11 "X-Display" graphics
+% system.
+%
+% These config files configure different multi-gpu or
+% multi-screen configurations, where one or multiple
+% graphics cards can be grouped into one or more x-screens
+% aka Psychtoolbox Screens for use with Psychtoolbox for
+% potential multi-display stimulation, of for having a
+% separate "operator display" with desktop GUI and Octave
+% or Matlab, and a separate "stimulation display(s)" for
+% visual stimulation with Psychtoolbox and one or more
+% onscreen windows.
+%
+% These samples were all successfully tested with various
+% NVidia and AMD single and dual graphics card configurations
+% with single display, dual-display, triple-display and quad-
+% display setups, so they are all known to work.
+%
+% You will need to adapt some samples though for your specific
+% display setup, so these are just meant to serve as a reference.
+%
+
