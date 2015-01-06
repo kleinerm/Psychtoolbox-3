1c7f31529be7d9e5122b1323b322e00f3dac1348
Mario Kleiner <mario.kleiner.de@gmail.com>
2015-01-06 02:43:42 +0100

    Screen/Linux: Add some copyright info to waylands screen glue.
    We use helper functions from Weston's weston-info.c sample
    client, so add some copyright info from it.

--- PsychSourceGL/Source/Linux/Screen/PsychScreenGlueWayland.c
+++ PsychSourceGL/Source/Linux/Screen/PsychScreenGlueWayland.c
@@ -36,6 +36,33 @@
 
 */
 
+/*
+ * Some of the helper code for wl_output and wl_seat enumeration is copied
+ * with modifications from the Weston example client "weston-info.c"
+ *
+ * That code carries the following copyright as of December 2014 (Weston 1.6+):
+ *
+ * Copyright © 2012 Philipp Brüschweiler
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the copyright holders not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.  The copyright holders make
+ * no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifdef PTB_USE_WAYLAND
 
 /* At the moment we still depend on Waffle even for "native" Wayland support.
