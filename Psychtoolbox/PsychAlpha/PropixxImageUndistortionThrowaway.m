4f1d4c7ba4df846f1864ffbb8591d8e61caede15
Mario Kleiner <mario.kleiner.de@gmail.com>
2015-03-16 08:33:03 +0100

    Provide preliminary experimental support for ProPixx fast modes.
    PsychProPixx function and the throwaway demo refined, cleaned up,
    documented to be of some use for others.
    
    This is considered experimental, preliminary, until i learn
    how a good final solution would look like.
    
    Anyway, good enough for people to play with and report back.

--- Psychtoolbox/PsychAlpha/PropixxImageUndistortionThrowaway.m
+++ Psychtoolbox/PsychAlpha/PropixxImageUndistortionThrowaway.m
@@ -1,30 +1,56 @@
 function PropixxImageUndistortionThrowaway(calibfilename, flipmethod, corrmethod, imagefilename)
 % PropixxImageUndistortionThrowaway(calibfilename [, flipmethod=0][, corrmethod=0][, imagefilename])
 %
-% flipmethod = 0 -> Sync flips.
-% flipmethod = 1 -> Async flips for some sort of triplebuffering.
+% A preliminary throwaway demo of how to use PropPixx in fast 4x or 12x
+% display mode. This demo is EXPERIMENTAL and subject to change
+% without warning, potentially even removal, in future Psychtoolbox
+% releases. Make a private copy of it and the PsychProPixx.m file
+% if you want to keep it as is for production use!
+%
+% You will need a powerful, fast graphics card and a well configured
+% operating system and machine for this to work reliably at 12x mode.
+%
+% The demo can be stopped via the ESCAPE key.
+%
+% Parameters and their meaning:
+%
+% 'calibfilename' mandatory name of a display geometry calibration file
+% created via DisplayUndistortionBVL() iff corrmethod is >= 0.
+%
+% flipmethod = 0 -> Sync flips. [ Slow but easy ]
+% flipmethod = 1 -> Async flips for some sort of triplebuffering. [ Potentially faster but more difficult ]
+% flipmethod = 2 -> Fastest method, but only available on Linux with open-source graphics drivers.
 %
 % corrmethod = -1 -> No correction.
-% corrmethod = 0  -> Global correction for whole image.
+% corrmethod = 0  -> Global correction for whole image. [Fastest and default]
 % corrmethod = 1  -> Separate correction for each quadrant.
 %
+% 'imagefilename' optional name of image file to display - see code.
 
 % History:
 % 14-Mar-2015  mk  Initial incomplete prototype for testing.
 
 if nargin < 2 || isempty(flipmethod)
+    % flipmethod 0 is the most safe, but also the least
+    % efficient:
     flipmethod = 0;
 end
 
 if nargin < 3 || isempty(corrmethod)
+    % corrmethod 0 is the most efficient one:
     corrmethod = 0;
 end
 
 % Initialize for unified KbName's and normalized 0 - 1 color range:
 PsychDefaultSetup(2);
 
+% Only check ESCAPE key via KbCheck for ending the demo.
+% KbCheck is expensive especially on OSX, so minimize its
+% impact to the absolute minimum:
+RestrictKeysForKbCheck(KbName('ESCAPE'));
+
 % Choose output screen as usual:
-screenid=max(Screen('Screens'));
+screenid = max(Screen('Screens'));
 
 PsychImaging('PrepareConfiguration');
 
@@ -35,10 +61,16 @@ if flipmethod == 1
 end
 
 if corrmethod == 0
+    % Use global geometry correction, with identical correction
+    % applied to all 4 quadrants of the onscreen window, ie., use
+    % a 2 by 2 tiling [2,2]. This is currently only implemented for
+    % the DisplayUndistortionBVL method. Subdivide warp mesh into
+    % a 73 x 73 grid. This may be much finer than needed and lowering
+    % the number may increase performance at a loss of precision.
     PsychImaging('AddTask', 'AllViews', 'GeometryCorrection', calibfilename, 0, 73, 73, [2, 2]);
 end
 
-[w, srcSize] =PsychImaging('OpenWindow', screenid, 0);
+w = PsychImaging('OpenWindow', screenid, 0);
 
 if nargin < 4
     imagefilename = [];
@@ -52,17 +84,29 @@ end
 img = imread(imagefilename);
 mytex=Screen('MakeTexture', w, img);
 
+% Try to present at 12x rate ie. 1440 Hz stimulus update rate:
 rate = 12
 
 if corrmethod <= 0
     % For global correction (method 0) clear calibfilename, so local
-    % correction gets disabled:
+    % correction gets disabled: Global correction is faster on all
+    % tested systems.
     calibfilename = [];
 end
+
+% Setup for fast display mode, producing the final image in onscreen window
+% 'w', for presentation at rate 'rate' (4 or 12), with 'flipmethod'.
+% Replace 0 with 1 for GPU load benchmarking - has some performance impact
+% itself, but allows assessment of how much we make the graphics card sweat:
 PsychProPixx('SetupFastDisplayMode', w, rate, flipmethod, calibfilename, 0);
+
+% Get a suitable offscreen window 'myimg' for drawing our stimulus:
 myimg = PsychProPixx('GetImageBuffer');
+
+% We want text to be big:
 Screen('TextSize', myimg, 128);
 
+% Setup for procedural gabor - stolen from ProceduralGaborDemo:
 res = [500 500];
 phase = 0;
 sc = 50.0;
@@ -79,40 +123,65 @@ gabortex = CreateProceduralGabor(myimg, tw, th, 0, [0.5 0.5 0.5 0.0]);
 
 % Draw the gabor once, just to make sure the gfx-hardware is ready for the
 % benchmark run below and doesn't do one time setup work inside the
-% benchmark loop: See below for explanation of parameters...
+% benchmark loop:
 Screen('DrawTexture', myimg, gabortex, [], [], 90+tilt, [], [], [], [], kPsychDontDoRotation, [phase+180, freq, sc, contrast, aspectratio, 0, 0, 0]);
 
+% Realtime scheduling:
+Priority(MaxPriority(w));
 
+% Prepare the benchmark / demo run:
 count = 0;
 KbReleaseWait;
-tstart = Screen('Flip', w, [], 2);
+tstart = Screen('Flip', w);
 
 while 1
-  % Draw bunnies into exampleImage, scale 'em up to offscreen window size:
   if 0
+    % Draw bunnies into exampleImage:
     Screen('DrawTexture', myimg, mytex);
   else
+    % Use a gabor shader for fast procedural drawing of Gabor stim:
     Screen('DrawTexture', myimg, gabortex, [], [], 90+count, [], [], [], [], kPsychDontDoRotation, [count+180, freq, sc, contrast, aspectratio, 0, 0, 0]);
   end
 
-  % Some centered text for illustration...
+  % Some centered text for illustration... Text drawing is
+  % relatively expensive, especially so on OSX and Windows!
   Screen('DrawText', myimg, sprintf('%i', count), 200, 80, 1);
   count = count + 1;
 
+  % Queue this stimulus for presentation at soonest video
+  % refresh cycle of the graphics card.
+  % Call KbCheck only on QueueImage calls which actually
+  % trigger a stimulus image update, ie. every 4th or 12th
+  % call. KbCheck calls are relatively expensive and can
+  % really spoil the timing on excessive use:
   if PsychProPixx('QueueImage', myimg) && KbCheck
+    % ESCape key pressed, end this demo:
     break;
   end
 end
 
-tend = Screen('Flip', w);
-KbStrokeWait;
+% Some magic to keep some weird driver happy on some OS:
+WaitSecs(0.1);
+Screen('FillRect', w, 0);
 
+% Final flip for time measurement:
+tend = Screen('Flip', w);
+
+% Done with realtime:
+Priority(0);
+
+% Retrieve timing samples, mostly useful for flipmethod 2:
 PsychProPixx('GetTimingSamples');
+
+% Reenable KbCheck for all keys:
+RestrictKeysForKbCheck([]);
 
 % Close all windows and ressources:
 Screen('CloseAll');
 
-avgtime = 1000 * (tend - tstart) / (count / rate)
-PsychProPixx('DisableFastDisplayMode');
+avgframetimemsecs = 1000 * (tend - tstart) / (count / rate)
+
+% Disable driver, plot some timing plots:
+PsychProPixx('DisableFastDisplayMode', 1);
 
 return;
--- Psychtoolbox/PsychHardware/DatapixxToolbox/DatapixxBasic/PsychProPixx.m
+++ Psychtoolbox/PsychHardware/DatapixxToolbox/DatapixxBasic/PsychProPixx.m
@@ -1,6 +1,84 @@
-function varargout = PsychProPixx (cmd, varargin)
+function varargout = PsychProPixx(cmd, varargin)
+% PsychProPixx() - Drive fast display modes of ProPixx projectors.
+%
+% This function is an EXPERIMENTAL PROTOTYPE. It is subject
+% to backwards incompatible change, or maybe even complete
+% removal from future Psychtoolbox versions without warning!
+% If you intend to use it in your own experiments, better make
+% a private copy of the function file, or be prepared to rewrite
+% your experiment code after Psychtoolbox updates.
+%
+% This function provides a preliminary method to setup Psychtoolbox
+% for the fast display modes of the ProPixx projector and to use those
+% modes in a convenient and relatively efficient fashion.
+%
+% See PropixxImageUndistortionThrowaway.m for a preliminary demo
+% on how to use this function.
+%
+%
+% Subcommands and their meaning:
+%
+% PsychProPixx('SetupFastDisplayMode', window, rate [, flipmethod=0][, localcalibration=none][, dogpumeasure=0]);
+% -- Setup for Propixx fast display mode. 'window' is the handle of
+% the onscreen window which displays on the ProPixx. 'rate' is the
+% desired update rate, 4 for 4-fold rate (= 120 * 4 = 480 Hz), or
+% 12 for 12-fold rate (= 120 * 12 = 1440 Hz). 'flipmethod' method
+% of flipping the stimulus images onto the screen. 0 = Standard
+% Screen('Flip'), 1 = Screen('AsyncFlipBegin') async flips,
+% 2 = Optimized non-blocking flips with later retrieval of timestamps
+% via PsychProPixx('GetTimingSamples'). Method 2 is the most efficient
+% method, but it is only supported on Linux with the open-source graphics
+% drivers. Method 1 is likely the second most efficient method and should
+% work on all operating systems, but returned timestamps need some
+% understanding of the rules for async flips. Method 0 is the most easy
+% to use on any system, but also likely the least efficient one.
+% 'localcalibration' Specify a calibration file for display geometry correction
+% via a local method. This is usually slower than use of a global calibration,
+% so better use that and leave this parameter [] empty.
+% 'dogpumeasure' If set to 1 then the actual processing time per frame
+% on the graphics card (gpu) will be measured with high precision if the
+% gpu + graphics driver supports this. This is useful for benchmarking and
+% tuning of code. Results are returned in the global variable 'gpudur' and
+% optionally plotted via PsychProPixx('DisableFastDisplayMode', 1);
+%
+%
+% image = PsychProPixx('GetImageBuffer');
+% -- Get an offscreen window suitable for drawing a stimulus into it,
+% and then passing it to PsychProPixx('QueueImage', image);
+%
+%
+% presenting = PsychProPixx('QueueImage', image [, tWhen]);
+% - Queue a 'image' for presentation. 'image' must be a texture
+% or OffScreenWindow of proper size, ie., half the width and height
+% of the onscreen window. The most easy way to get a matching
+% offscreen window for drawing into and presentation is to use
+% image = PsychProPixx('GetImageBuffer').
+%
+% Once a sufficient number of images are queued, the final
+% image for driving the ProPixx projector is created and presented,
+% either at the next video refresh, or at the optional target time
+% 'tWhen' - which is passed to the Screen('Flip') command. When
+% this happens, the 'presenting' flag will return as either 1 in offline
+% timestamping mode (flipmethod 2), or as the Flip timestamp of
+% image onset for the whole composite image, otherwise it will return
+% as 0 if an image has just been queued for presentation.
+%
+%
+% samples = PsychProPixx('GetTimingSamples');
+% -- Returns 'Flip' timestamps in the vector 'samples'. Depending
+% on flipmode, either all samples of all presentations are returned
+% (for flipmode 2 on Linux), or up to the most recent 1200 samples
+% are returned (for flipmode 0 or 1 on all operating systems).
+%
+%
+% PsychProPixx('DisableFastDisplayMode' [, plottimestamps]);
+% -- Disable Propixx mode, reset internal variables etc. If the optional
+% 'plottimestamps' flag is set to 1 then plots are generated which show
+% the collected durations of frame presentations, and potentially
+% collected timestamps of gpu rendertime if benchmarking was enabled.
 %
 
+%
 % History:
 % 14-Mar-2015  mk  Initial incomplete prototype for testing.
 
@@ -68,7 +146,7 @@ if strcmpi(cmd, 'QueueImage')
   % Blit outImage to target quadrant and channel, disable filtering as outImage
   % already has matching size for 1-to-1 blit:
   Screen('DrawTexture', win, outImage, [], dstRect, [], 0);
-  
+
   % Update phaseid:
   phaseid = phaseid + 1;
 
@@ -77,15 +155,20 @@ if strcmpi(cmd, 'QueueImage')
     % Yes. Do the output thing:
     phaseid = 0;
 
+    % Restore colormask to normal:
+    Screen('Blendfunction', win, [], [], [1 1 1 1]);
+
     sampleCount = mod(sampleCount + 1, 1200);
 
     % Schedule a flip, do not clear the window after flip:
     if flipmethod == 0
       tvbl(sampleCount + 1) = Screen('Flip', win, tWhen, 2);
+      tstamp = tvbl(sampleCount + 1);
     end
 
     if flipmethod == 1
       tvbl(sampleCount + 1) = Screen('AsyncFlipBegin', win, tWhen, 2);
+      tstamp = tvbl(sampleCount + 1);
     end
 
     if flipmethod == 2
@@ -93,6 +176,7 @@ if strcmpi(cmd, 'QueueImage')
       % Instead we use swap completion logging to collect all
       % timestamps at the end of the session. Linux only feature...
       Screen('Flip', win, tWhen, 2, 1);
+      tstamp = 1;
     end
 
     if dogpumeasure
@@ -120,7 +204,7 @@ if strcmpi(cmd, 'QueueImage')
     end
 
     % Signal flip either needed or already scheduled:
-    varargout{1} = 1;
+    varargout{1} = tstamp;
   else
     varargout{1} = 0;
   end
@@ -129,7 +213,7 @@ if strcmpi(cmd, 'QueueImage')
 end
 
 if strcmpi(cmd, 'GetImageBuffer')
-  varargout{1} = Screen('OpenOffscreenWindow', win, 0, processedRect);
+  varargout{1} = Screen('OpenOffscreenWindow', win, 0, processedRect, [], 32);
   return;
 end
 
@@ -189,7 +273,7 @@ if strcmpi(cmd, 'SetupFastDisplayMode')
   if (length(varargin) >= 4) && ~isempty(varargin{4})
     % Build offscreen window as target for the input image after geometry correction
     % and potential grayscale conversion:
-    processedImage = Screen('OpenOffscreenWindow', win, 0, processedRect);
+    processedImage = Screen('OpenOffscreenWindow', win, 0, processedRect, [], 32);
 
     calib = varargin{4};
     
@@ -231,14 +315,30 @@ if strcmpi(cmd, 'GetTimingSamples')
     end
   end
 
+  varargout{1} = tvbl;
+
   return;
 end
 
 if strcmpi(cmd, 'DisableFastDisplayMode')
-  tvbl = diff(tvbl);
-  tvbl = tvbl(find(tvbl >= 0 & tvbl < 1));
-  plot(1000 * tvbl);
-  clear win processedImage gloperator tvbl
+  if length(varargin) >= 1 && ~isempty(varargin{1}) && varargin{1}
+    tvbl = diff(tvbl);
+    tvbl = tvbl(find(tvbl >= 0 & tvbl < 1));
+    figure;
+    plot(1000 * tvbl);
+    title('Delta between successive presentations [msecs]:')
+
+    if gpumeasure && length(gpudur) > 0
+      figure;
+      plot(1000 * gpudur);
+      title('Processing time needed on GPU per frame [msecs]:')
+    end
+  end
+
+  tvbl = [];
+  win = [];
+  processedImage = [];
+  gloperator = [];
   return;
 end
 
