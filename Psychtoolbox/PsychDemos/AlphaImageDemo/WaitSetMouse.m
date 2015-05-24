6e250430185e3b6e566cd5b55a0c3a49c49ce198
Mario Kleiner <mario.kleiner.de@gmail.com>
2015-05-25 01:05:29 +0200

    WaitSetMouse: Make it cope with Waylands lack of SetMouse support.
    No-Op on Wayland.

--- Psychtoolbox/PsychDemos/AlphaImageDemo/WaitSetMouse.m
+++ Psychtoolbox/PsychDemos/AlphaImageDemo/WaitSetMouse.m
@@ -1,6 +1,10 @@
 function WaitSetMouse(newX, newY, windowPtrOrScreenNumber)
 
 % set and wait for new cursor position to take effect
+if IsWayland
+    warning('WaitSetMouse() not yet supported on Wayland display backend. Doing nothing.');
+    return;
+end
 
 while 1 % wait for new cursor position to be set
     SetMouse(newX,newY,windowPtrOrScreenNumber);
