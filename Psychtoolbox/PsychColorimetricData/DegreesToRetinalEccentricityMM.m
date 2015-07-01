0adf183c7ae061f79d2449d87e0ad6b2f8dd0b08
David Brainard <brainard@psych.upenn.edu>
2015-07-01 10:56:22 -0400

    Few more comment refinements.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Psychtoolbox/PsychColorimetricData/DegreesToRetinalEccentricityMM.m
+++ Psychtoolbox/PsychColorimetricData/DegreesToRetinalEccentricityMM.m
@@ -37,7 +37,23 @@ function eccMm = DegreesToRetinalEccentricityMM(eccDegrees,species,method,eyeLen
 % produce a visual angle of 0.1 degree for an eccentricity of 0.  This
 % seems bad to me. I modified the formulae so that they use the linear
 % approximation for small angles, producing a result that does go through
-% (0,0).
+% (0,0).  This may be related to the fact that there is some ambiguity in
+% the papers between whether the center should be thought of as the fovea
+% or the center of the optical axis.  But I think this difference is small
+% enough that the same formulae would apply across such a shift in origin.
+%
+% I digitized Drasdo and Fowler Figure 2 and compared it to what this
+% routine produces.  I'd call agreement so-so, but considerably better than
+% what the linear approximation produces.  One could probably do better,
+% but my intuition is that the deviations are small compared to eye to eye
+% differences and differences that would be produced by different model
+% eyes, so that juice isn't worth the squeeze.  I pasted my digitization at
+% the end of DegreesToRetinalEccentricity if anyone wants to fuss with
+% this.  But probably if you're going to do that, you should do the whole
+% ray tracing thing with our best current model eye.
+%
+% I have not checked the fit to the Perrry and Cowey curve for Rhesus
+% against a digitization of that figure.
 %
 % See also: EyeLength, RetinalEccentricityMMToDegrees, DegreesToRetinalMM, RetinalMMToDegrees
 %
@@ -113,3 +129,30 @@ switch (method)
 end
 end
 
+%% Drasdo and Fowler Figure 2
+%
+% This is ellipsoid model data, digitized from their figure.  It is not
+% clear to me which curve (ellipsoid or sphere) Dacey and Peterson
+% digitized and fit.  First column is degrees, second is mm.
+% drasdoFowlerData = ...
+%     [1.429393919263E0	4.001594366571E-1
+%     3.492105671073E0	9.230850993158E-1
+%     6.034234593326E0	1.662020859629E0
+%     1.000354303683E1	2.739148342523E0
+%     1.571668991785E1	4.215850660998E0
+%     2.381363626298E1	6.400797183286E0
+%     3.286388095396E1	8.862844615691E0
+%     4.016364401337E1	1.073941407028E1
+%     4.572178303328E1	1.227837640337E1
+%     4.794503864125E1	1.289396133661E1
+%     4.984831373591E1	1.335521158573E1
+%     5.254323612126E1	1.396991961735E1
+%     5.523705130760E1	1.455364379194E1
+%     6.126021391085E1	1.590582608118E1
+%     6.632675657123E1	1.688795588919E1
+%     7.012555636750E1	1.759356938816E1
+%     7.503155517173E1	1.848303992560E1
+%     8.135698310414E1	1.949381518634E1
+%     8.689076374588E1	2.035113266459E1
+%     9.321065568325E1	2.120698864014E1
+%     9.921278150535E1	2.197047764565E1];
--- Psychtoolbox/PsychColorimetricData/RetinalEccentricityMMToDegrees.m
+++ Psychtoolbox/PsychColorimetricData/RetinalEccentricityMMToDegrees.m
@@ -36,7 +36,24 @@ function eccDegrees = RetinalEccentricityMMToDegrees(eccMm,species,method,eyeLen
 % produce a visual angle of 0.1 degree for an eccentricity of 0.  This
 % seems bad to me. I modified the formulae so that they use the linear
 % approximation for small angles, producing a result that does go through
-% (0,0).
+% (0,0).  This may be related to the fact that there is some ambiguity in
+% the papers between whether the center should be thought of as the fovea
+% or the center of the optical axis.  But I think this difference is small
+% enough that the same formulae would apply across such a shift in origin.
+%
+% I digitized Drasdo and Fowler Figure 2 and compared it to what
+% DegreesToRetinalEccentricity produces.  I'd call agreement so-so, but
+% considerably better than what the linear approximation produces.  One
+% could probably do better, but my intuition is that the deviations are
+% small compared to eye to eye differences and differences that would be
+% produced by different model eyes, so that juice isn't worth the squeeze.
+% I pasted my digitization at the end of DegreesToRetinalEccentricity if
+% anyone wants to fuss with this.  But probably if you're going to do that,
+% you should do the whole ray tracing thing with our best current model
+% eye.
+%
+% I have not checked the fit to the Perrry and Cowey curve for Rhesus
+% against a digitization of that figure.
 %
 % See also: EyeLength, DegreesToRetinalEccentricityMM, DegreesToRetinalMM, RetinalMMToDegrees
 %
