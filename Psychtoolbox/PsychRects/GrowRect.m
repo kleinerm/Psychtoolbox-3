a6c8deb9a977f418bea6ede856a1b985f46a8130
Diederick Niehorster <dcnieho@gmail.com>
2015-07-26 00:02:44 +0200

    GrowRect doc typos

--- Psychtoolbox/PsychRects/GrowRect.m
+++ Psychtoolbox/PsychRects/GrowRect.m
@@ -5,9 +5,9 @@ function r = GrowRect(r,horizontalPixels,verticalPixels)
 % and verticalPixels from the top coordinate, and adding horizontalPixels
 % to the right coordinate and verticalPixels to the bottom coordinate. Also
 % see ScaleRect. Input can be multiple rects, concatenated into a Mx4
-% matrix Using a negate number os pixels to grow by would obviously shrink
-% the rect, though be careful as no checks are performed to see if the
-% resulting rect is valid.
+% matrix Using a negative number of pixels to grow by would obviously
+% shrink the rect, though be careful as no checks are performed to see if
+% the resulting rect is valid.
 %
 % Also see PsychRects.
 
