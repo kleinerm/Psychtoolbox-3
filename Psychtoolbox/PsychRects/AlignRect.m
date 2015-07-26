31facaed749b520ad4a5960ae73ab3961a6d6440
Diederick Niehorster <dcnieho@gmail.com>
2015-07-26 21:17:28 +0200

    adding centering while keeping decimal precision action to AlignRect

--- Psychtoolbox/PsychRects/AlignRect.m
+++ Psychtoolbox/PsychRects/AlignRect.m
@@ -3,12 +3,12 @@ function rect=AlignRect(rect,fixedRect,side1,side2)
 %
 % Moves rect to align its center/top/bottom/left/right with the
 % corresponding edge(s)/point of fixedRect. Does "side1" and then "side2".
-% The legal values for side1 and side2 are 'center', 'left', 'right',
+% The legal values for side1 and side2 are 'center', 'centerd', 'left', 'right',
 % 'top', and 'bottom'. 
 %      r=AlignRect(r,screenRect,'center','top');
-% For backward compatibility, also accepts 1,2,3,4,5. You may use the
+% For backward compatibility, also accepts 1,2,3,4,5,6. You may use the
 % pre-defined constants RectLeft,RectRight,RectTop,RectBottom, but there is
-% no named constant for centering.
+% no named constant for centering and centering keeping decimal precision.
 %
 % rect and fixedRect can both be Mx4 rect arrays, but either for one of
 % them M must be 1, or rect and fixedRect must have the same shape. If rect
@@ -22,7 +22,7 @@ function rect=AlignRect(rect,fixedRect,side1,side2)
 % See also PsychRects/Contents.
 
 % Denis Pelli 5/27/96, 7/10/96, 8/5/96, 11/8/06
-% dcn 7/26/2015: vectorized
+% dcn 7/26/2015: vectorized, adding 'centerd' action
 
 if nargin<3
     error('Usage:  rect=AlignRect(rect,fixedRect,side1,[side2])');
@@ -36,11 +36,11 @@ if nargin>3
 end
 for i=1:length(side)
     if isnumeric(side{i})
-        if ~ismember(side{i},1:5)
+        if ~ismember(side{i},1:6)
             error('Illegal side%d value.',i);
         end
     elseif ischar(side{i})
-        [ok,n]=ismember(side{i},{'left','top','right','bottom','center'});
+        [ok,n]=ismember(side{i},{'left','top','right','bottom','center','centerd'});
         if ~ok
             error('Illegal side%d value.',i);
         end
@@ -49,6 +49,8 @@ for i=1:length(side)
         error('Illegal side%d value.',i);
     end
     switch side{i}
+        case 6
+            rect=CenterRectd(rect,fixedRect);
         case 5
             rect=CenterRect(rect,fixedRect);
         case {RectLeft,RectRight},
