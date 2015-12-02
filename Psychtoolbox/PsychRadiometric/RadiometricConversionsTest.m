b19c21b6c219b1d9abb0a51a96da218113052eb0
David Brainard <brainard@psych.upenn.edu>
2015-12-02 08:12:36 -0500

    Added some more tests, which all passed.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Psychtoolbox/PsychRadiometric/RadiometricConversionsTest.m
+++ Psychtoolbox/PsychRadiometric/RadiometricConversionsTest.m
@@ -6,18 +6,25 @@
 % once before but can't currently find it.
 %
 % 6/23/13  dhb  Wrote it.
+% 12/2/15  dhb  More tests.
 
 %% Clear
 clear; close all;
 
 %% Define a monochromatic radiance
 S = [500 1 1];
-radianceWattsPerSrM2 = ones(S(3),1);
+radianceWattsPerSrM2 = 2.7*ones(S(3),1);
 
 %% Define eye parameters
 eyeLengthM = 0.017;
 pupilDiameterM = 0.003;
 pupilAreaM2 = 2*pi*((pupilDiameterM/2)^2);
+
+%% Define stimulus parameters
+stimulusDistanceM = 4.2;
+stimulusAreaM2 = 0.24;
+stimulusSideDeg = (180/pi)*2*atan(sqrt(stimulusAreaM2)/(2*stimulusDistanceM));
+stimulusAreaDeg2 = stimulusSideDeg^2;
 
 %% Convert radiance to retinal irradiance
 retIrradianceWattsPerM2 = RadianceAndPupilAreaEyeLengthToRetIrradiance(radianceWattsPerSrM2,S,pupilAreaM2,eyeLengthM);
@@ -33,6 +40,23 @@ if (abs(retIrradianceWattsPerM2-retIrradianceWattsPerM2Check) > 1e-10)
     error('Retinal irradiance unit calculations do not invert');
 else
     
+%% Convert retinal irradiance to corneal irradiance and back
+fractionalTolerance = 0.005;
+cornIrradianceWattsPerM2 = RadianceAndDistanceAreaToCornIrradiance(radianceWattsPerSrM2,stimulusDistanceM,stimulusAreaM2);
+cornIrradianceWattsPerM2Check = RadianceAndDegrees2ToCornIrradiance(radianceWattsPerSrM2,stimulusAreaDeg2);
+fprintf('Corneal irradiance: %0.3g Watts/M2 (%0.3g check)\n', ...
+    cornIrradianceWattsPerM2,cornIrradianceWattsPerM2Check);
+if (abs(cornIrradianceWattsPerM2-cornIrradianceWattsPerM2Check)/cornIrradianceWattsPerM2 > fractionalTolerance)
+    error('Two ways of computing corneal irradiance from radiance do not agree');
+end
+radianceWattsPerSrM2Check = CornIrradianceAndDegrees2ToRadiance(cornIrradianceWattsPerM2,stimulusAreaDeg2);
+fprintf('Radiance: %0.4f Watts/[sr-M2] (%0.4f check)\n', ...
+    radianceWattsPerSrM2,radianceWattsPerSrM2Check);
+if (abs(radianceWattsPerSrM2-radianceWattsPerSrM2Check)/radianceWattsPerSrM2 > fractionalTolerance)
+    error('Radiance to corneal irradiance and back does not agree');
+end
+
+
 end
 
 
