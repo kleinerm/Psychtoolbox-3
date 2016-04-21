4111b9d188241b99d1ffe99a2a9c44570ec79fb9
Mario Kleiner <mario.kleiner.de@gmail.com>
2016-04-21 05:56:49 +0200

    BitsPlusPlus: Add new subfunction 'UseFE1StereoGoggles'
    This allows to configure the PTB imaging pipeline to
    drive CRS FE1 stereo goggles connected to a Bits
    display device. The pipeline will draw proper
    T-Lock codes to the bottom of each stimulus
    frame to set the goggles for the following
    frame.
    
    The old CRS toolbox helper function bitsGoggles was
    updated to compute proper T-Lock codes and return
    them to BitsPlusPlus instead of directly executing
    them.
    
    BitsStereoDemo.m was updated to use modern PTB
    concepts and demonstrate the goggles with the Bits
    devices.
    
    -> Test pending. Just coded, not tested.

--- Psychtoolbox/PsychHardware/BitsPlusToolbox/bitsGoggles.m
+++ Psychtoolbox/PsychHardware/BitsPlusToolbox/bitsGoggles.m
@@ -1,11 +1,28 @@
-% usage bitsGoggles(left,right,window)
+function [encodedDIOdata, Mask, Data, Command] = bitsGoggles(left,right,window)
+% [encodedDIOdata] = bitsGoggles(left, right [, window])
 %
-% where 0 = goggle open
-% and 1 = goggle closed
+% Drives the FE1 goggles connected to bits 4 and 5 of the digital output
+% of a Bits+, Bits#, etc.
 %
-% drives the FE1 goggles connected to bits 4 and 5 of the digital output.
+% 'left' and 'right' define the required state of the left and
+% right goggle shutter, where a value of:
+% 0 = goggle open
+% 1 = goggle closed
+%
+% If 'window' is given, but the return argument 'encodedDIOdata' is not
+% given, then the control code is drawn into the framebuffer and a flip
+% is performed.
+%
+% If 'encodedDIOdata' is given, then 'window' is not used
+% and the required T-Lock code is returned as a image matrix
+% which could be used for Screen('PutImage') or Screen('DrawTexture').
+%
 
-function bitsGoggles(left,right,window)
+% History:
+% ??/???/????   Written by Cambridge Research Systems.
+% 21-Apr-2016   Extended by mk for use in PTB's imaging pipeline:
+%               Add 'encodedDIOdata' return argument and code path.
+%
 
     %  Bits     Shutter
     % 5    4     L   R
@@ -20,19 +37,26 @@ function bitsGoggles(left,right,window)
 
     bit5 = ~right;
     bit4 = ~xor(left,right);
-    
+
     Mask = 16 + 32;
     %Mask = 65535;
-    
+
     Data=zeros(1,248);
     if (bit4)
         Data=Data + 16;
     end
-    
+
     if (bit5)
         Data=Data + 32;
-    end        
-    
+    end
+
     Command = 0;
 
-    bitsEncodeDIO(Mask,Data,Command, window);
+    if nargout == 0
+        % Call routine to draw the T-Lock code and then Screen('Flip'):
+        bitsEncodeDIO(Mask,Data,Command, window, 0);
+    else
+        % Call common encoder routine to create T-Lock image matrix:
+        encodedDIOdata = BitsPlusDIO2Matrix(Mask, Data, Command);
+    end
+return;
