816c4531418e01384eaeba80ee3c68a05f5508c6
Mario Kleiner <mario.kleiner.de@gmail.com>
2016-05-13 11:16:21 +0200

    sca: Add cursor repositioning on multi-screen setups.
    On multi-screen setups, reposition mouse cursor to center
    of screen 0 at sca time. Works around cursors that can get
    stuck on secondary X-Screens under KDE Plasma 5 as shipping
    in Ubuntu 16.04.0-LTS.

--- Psychtoolbox/PsychOneliners/sca.m
+++ Psychtoolbox/PsychOneliners/sca.m
@@ -10,6 +10,7 @@ function sca
 % 4/6/6   Written (MK).
 % 5/31/8  Add CLUT restore call (MK).
 % 7/03/13 Add PsychJavaSwingCleanup call (MK).
+% 5/13/16 Add mouse pointer repositioning (MK).
 
 % Unhide the cursor if it was hidden:
 % Don't do this on Wayland for now...
@@ -23,6 +24,20 @@ for win = Screen('Windows')
             Screen('AsyncFlipEnd', win);
         end
     end
+end
+
+% Reposition mouse cursor to center of screen 0 on multi-screen
+% setups. This as a workaround for Linux KDE-5 Plasma in Ubuntu 16.04-LTS,
+% which has a weird bug when run on multi X-Screen setups: You can move
+% the mouse cursor off X-Screen 0 onto a secondary X-Screen, but sometimes
+% you can not move it back to X-Screen 0 where the regular GUI resides.
+% Probably some buggy use of pointer barriers which did not take multiple
+% X-Screens into account?
+% Anyway, SetMouse works, so we reposition the cursor to X-Screen 0 at
+% the end of a session:
+if length(Screen('Screens')) > 1
+    [x,y] = RectCenter(Screen('Rect', 0));
+    SetMouse(x,y,0);
 end
 
 % Close all windows, release all Screen() ressources:
