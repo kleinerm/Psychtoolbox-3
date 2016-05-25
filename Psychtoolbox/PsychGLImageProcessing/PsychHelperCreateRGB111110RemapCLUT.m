e703252bc355a58a30f8df6c17af8efeda63156d
Mario Kleiner <mario.kleiner.de@gmail.com>
2016-05-25 18:36:49 +0200

    Fix 11 bpc native framebuffer support for capable AMD gpus.
    Now that i finally could test this on a real
    11 bpc capable AMD Radeon R9-380 Tonga Pro gpu,
    it turns out our pixel formatting for 11 bpc
    BGR101111 mode was wrong! Results looked great
    on older gpus, but apparently more due to some
    artifact of their 10 bpc limited display pipeline.
    
    Updated PsychHelperCreateRGB111110RemapCLUT.m to compute
    a conversion LUT that actually works on the Tonga Pro
    with its DCE-10 display engine and 12 bpc capbable
    display pipeline.
    
    -> This is tested to look correct when outputting to a
       8 bpc panel, as i don't have a suitable CRT monitor
       or digital 12 bpc panel available. Ergo not measured
       with photometer etc., just eye-balled!

--- Psychtoolbox/PsychGLImageProcessing/PsychHelperCreateRGB111110RemapCLUT.m
+++ Psychtoolbox/PsychGLImageProcessing/PsychHelperCreateRGB111110RemapCLUT.m
@@ -6,17 +6,17 @@ function remapCLUTId = PsychHelperCreateRGB111110RemapCLUT
 %
 % Build a 3 rows by 2048 texels RGBA8 lookup texture for mapping HDR RGB
 % pixel values in range 0-2047 (ie. 11 bpc resolution) to RGBA8 framebuffer
-% pixels which will be suitable to drive a RGB11-11-10 framebuffer scanout
+% pixels which will be suitable to drive a BGR10-10-11 framebuffer scanout
 % engine (CRTC). If a graphics cards framebuffer scanout engine CRTC is
-% configured for 11-11-10 ~ 11 bpc framebuffer mode, this will make sure that
+% configured for 10-11-11 ~ 11 bpc framebuffer mode, this will make sure that
 % HDR pixel data is shown properly onscreen.
 %
 % This texture is used as CLUT texture for the
 % RGBMultiLUTLookupCombine_FormattingShader.frag.txt in the final output
 % formatting chain of the imaging pipeline.
 %
-% The expected pixel layout in 11bpc mode is: RGB111110, ie.:
-% R11G11B10 -- That's how a 32 bit pixel is interpreted for video
+% The expected pixel layout in 11bpc mode is: BGR101111, ie.:
+% R11 G11 B10 -- That's how a 32 bit pixel is interpreted for video
 % scanout. However the OpenGL system on all current OS with standard
 % drivers can only output A8R8G8B8 formatted pixels. We solve this via the
 % imaging pipeline: Screen() and OpenGL drawing ops go to the imaging
@@ -26,10 +26,18 @@ function remapCLUTId = PsychHelperCreateRGB111110RemapCLUT
 % lookup corresponding ARGB8 color tuples for the framebuffer, combines
 % them into a single ARGB8 tuple by addition (logical OR), then writes that
 % ARGB tuple to the framebuffer. At display scanout time, the CRTC's will
-% find 32 bit pixels properly formatted for RGB111110 scanout.
+% find 32 bit pixels properly formatted for BGR101111 scanout.
+%
+% This requires a suitable graphics cards with an at least 11 bpc capable
+% display pipeline. This is the case for recent AMD gpu's of the "Sea Islands"
+% and "Volcanic Islands" family with a DCE-8 or later display engine. Those are
+% said to have full 12 bpc pipelines.
 %
 % History:
-% 8-June-2014  Written - Derived from PsychHelperCreateARGB2101010RemapCLUT.m (MK).
+%  8-Jun-2014  Written - Derived from PsychHelperCreateARGB2101010RemapCLUT.m (MK).
+% 25-May-2016  Fixed up for DCE-10 "Volcanic Islands" Radeon R9 380 Tonga Pro (MK).
+%              Turns out they need very different formatting and our original
+%              implementation on non-12 bpc capable gpu's caused wrong test results.
 
 % This routine assumes that a mogl GL context is properly set up:
 global GL;
@@ -48,69 +56,75 @@ else
     % Startindex for 1st LUT entry:
     j=1;
 
-    % The order of channels vs. texture rows is backwards for some
-    % reason, but this is the correct way for BGR10-11-11 scanout
-    % with format code 7. Don't question it, bitshifting and swizzling
-    % hurts the brain, just accept it.
+    % This is the correct input to output color channels/bits mapping for the
+    % BGR10-11-11 scanout with 32bpp format code 7 as used by the GPU.
+    %
+    %  Input:  -> Output
+    %    B2:9  -> A0:7
+    %    B0:1  -> R6:7
+    %    G5:10 -> R0:5
+    %    G0:4  -> G3:7
+    %    R8:10 -> G0:2
+    %    R0:7  -> B0:7
 
-    % First the 10 bpc, only 1024 levels BLUE LUT: Texrow 1
+    % First the 11 bpc, 2048 levels RED LUT: Texrow 1
+    for i=0:2047
+        % Memory order for texture creation is RGBA:
+
+        % Nothing in R:
+        clut(j)= 0;
+        j=j+1;
+        % R8:10 -> G0:2:
+        clut(j)= bitshift(bitand(i, bin2dec('11100000000')), -8);
+        j=j+1;
+        % R0:7  -> B0:7
+        clut(j)= bitshift(bitand(i, bin2dec('00011111111')), +0);
+        j=j+1;
+        % Nothing in A:
+        clut(j)= 0;
+        j=j+1;
+    end
+
+    % Then the 11 bpc, 2048 levels GREEN LUT: Texrow 2
+    for i=0:2047
+        % Memory order for texture creation is RGBA:
+
+        % G5:10 -> R0:5:
+        clut(j)= bitshift(bitand(i, bin2dec('11111100000')), -5);
+        j=j+1;
+        % G0:4  -> G3:7:
+        clut(j)= bitshift(bitand(i, bin2dec('00000011111')), +3);
+        j=j+1;
+        % Nothing in B:
+        clut(j)= 0;
+        j=j+1;
+        % Nothing in A:
+        clut(j)= 0;
+        j=j+1;
+    end
+
+    % Last the only 10 bpc, only 1024 levels BLUE LUT: Texrow 3
     for i=0:1023
-        % Memory order is probably BGRA:
+        % Memory order for texture creation is RGBA:
 
         % We only have 1024 intensity steps 'i', but because this
         % sub-LUT also has 2048 slots, we fill every two consecutive
         % slots with the same values, ie., do the fill process twice for
         % each value or step of i:
         for repeatsteap = 1:2
-            % LSB 8 bits of blue into blue:
-            clut(j)= bitand(i, bin2dec('11111111'));
+            % B0:1 - > R6:7
+            clut(j)= bitshift(bitand(i, bin2dec('0000000011')), +6);
             j=j+1;
-            % MSB 2 bits of blue into 2 LSB of green:
-            clut(j)= bitshift(bitand(i, bin2dec('1100000000')), -8);
-            j=j+1;
-            % Nothing in red:
+            % Nothing in G:
             clut(j)= 0;
             j=j+1;
-            % Nothing in alpha:
+            % Nothing in B:
             clut(j)= 0;
+            j=j+1;
+            % B2:9  -> A0:7:
+            clut(j)= bitshift(bitand(i, bin2dec('1111111100')), -2);
             j=j+1;
         end
-    end
-
-    % Then the 11 bpc, 2048 levels GREEN LUT: Texrow 2
-    for i=0:2047
-        % Memory order is probably BGRA:
-
-        % Nothing in blue
-        clut(j)= 0;
-        j=j+1;
-        % LSB 6 bits of green into 6 MSB of green:
-        clut(j)= bitshift(bitand(i, bin2dec('111111')), 2);
-        j=j+1;
-        % MSB 5 bits of green into 5 LSB of red:
-        clut(j)= bitshift(bitand(i, bin2dec('11111000000')), -6);
-        j=j+1;
-        % Nothing in alpha:
-        clut(j)= 0;
-        j=j+1;
-    end
-
-    % First the 11 bpc, 2048 levels RED LUT: Texrow 3
-    for i=0:2047
-        % Memory order is probably BGRA:
-
-        % Nothing in blue:
-        clut(j)= 0;
-        j=j+1;
-        % Nothing in green:
-        clut(j)= 0;
-        j=j+1;
-        % 3 LSB of red into 3 MSB of red:
-        clut(j)= bitshift(bitand(i, bin2dec('111')), 5);
-        j=j+1;
-        % 8 MSB of red into 8 LSB of alpha:
-        clut(j)= bitshift(bitand(i, bin2dec('11111111000')), -3);
-        j=j+1;
     end
 
     % Cache computed LUT in users config dir:
@@ -118,16 +132,13 @@ else
 end
 
 % Hostformat defines byte-order in host memory:
-% This should work on any little-endian system (Windows/Linux On IntelPC,
-% Linux on IntelMac, OS/X on IntelMac) - pretty much everything.
-% On BigEndian systems (OS/X PowerPC) we may need to replace this by a
-% GL.UNSIGNED_INT_8_8_8_8 instead to reverse byte order at conversion time.
-hostformat = GL.UNSIGNED_INT_8_8_8_8_REV;
+% At increasing memory locations for each texel first R, then G, then B, then A.
+hostformat = GL.UNSIGNED_BYTE;
 
 % Create and setup texture from 'clut':
 remapCLUTId = glGenTextures(1);
 glBindTexture(GL.TEXTURE_RECTANGLE_EXT, remapCLUTId);
-glTexImage2D(GL.TEXTURE_RECTANGLE_EXT, 0, GL.RGBA, 2048, 3, 0, GL.BGRA, hostformat, clut);
+glTexImage2D(GL.TEXTURE_RECTANGLE_EXT, 0, GL.RGBA8, 2048, 3, 0, GL.RGBA, hostformat, clut);
 glBindTexture(GL.TEXTURE_RECTANGLE_EXT, 0);
 
 % Ready.
