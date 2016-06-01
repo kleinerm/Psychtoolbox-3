2dd811d5f61a297b9bb9511dad3defb00bbe5ae7
David Brainard <brainard@psych.upenn.edu>
2016-06-01 09:36:37 -0400

    Added test program and comments about behavior of our CIEConeFundamentals code for field sizes larger than 10-degrees.
    The standard only covers field sizes between 1 and 10 degrees.  Our code will return estimates for larger field sizes.  These estimates seem like a reasonable extension, but were not previously thought through and documented as carefully as they should have been.  The new test program and references to it in comments of other routines remedy this.
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Psychtoolbox/PsychTests/CIEConeFundamentalsTest.m
+++ Psychtoolbox/PsychTests/CIEConeFundamentalsTest.m
@@ -11,13 +11,15 @@
 % the Stockman/Sharpe 2-degree and 10 degree fundamentals if one starts
 % with the tabulated LMS absorbances.  The agreement is less perfect
 % if one uses the nomogram and recommended lambda-max values to
-% generate the absorbances.  See comment on this point in StockmanSharpeNomogram.m
+% generate the absorbances.  See comment on this point in
+% StockmanSharpeNomogram.m.
 %
 % The code here is closely related (and uses) a more general set of code
 % for setting parameters for photoreceptors and computing quantal 
 % sensitivities. See:
 %    DefaultPhotoreceptors, FillInPhotoreceptors, PrintPhotoreceptors,IsomerizationsInDishDemo
-%    IsomerizationsInEyeDemo, ComputeCIEConeFundamentals, ComputeRawConeFundamentals.
+%    IsomerizationsInEyeDemo, ComputeCIEConeFundamentals,
+%    ComputeRawConeFundamentals, CIEConeFundamentalsFieldSizeTEst.
 %
 % 8/11/11  dhb  Wrote it
 % 8/14/11  dhb  Clean up a little.
