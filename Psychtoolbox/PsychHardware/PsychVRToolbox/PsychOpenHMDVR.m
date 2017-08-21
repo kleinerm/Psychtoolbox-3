98ad2bdb31c55de5044ece0332c5dd1f8b751383
Mario Kleiner <mario.kleiner.de@gmail.com>
2017-08-21 07:37:55 +0200

    PsychOpenHMDVR: Take full advantage of Rift DK2 120Hz mono mode.
    The 1080x948 pixels at 120 Hz mode of the Rift DK2 is actually a
    monoscopic viewer mode. It still needs our lens distortion and
    color aberration correction, but automatically mirrors the same
    image (full framebuffer content) into both eyes, at 120 Hz refresh
    rate.
    
    Make sure we make use of this efficient mono monitor mode.
    
    Btw. Using the PsychImaging PanelFitter task for a -90 degree
    rotation will also produce a valid monoscopic image, possibly
    at lower overhead. Downside is no lens distortion/aberration
    correction though, so only useful for certain stimuli.

--- Psychtoolbox/PsychHardware/PsychVRToolbox/PsychOpenHMDVR.m
+++ Psychtoolbox/PsychHardware/PsychVRToolbox/PsychOpenHMDVR.m
@@ -61,7 +61,12 @@ function varargout = PsychOpenHMDVR(cmd, varargin)
 % in some kind of 3D virtual world. This is the default if omitted. The task
 % 'Stereoscopic' sets up for display of stereoscopic stimuli, but without
 % head tracking. 'Monoscopic' sets up for display of monocular stimuli, ie.
-% the HMD is just used as a special kind of standard display monitor.
+% the HMD is just used as a special kind of standard display monitor. Please
+% note that the Oculus Rift DK2 has a special video mode for such monoscopic
+% presentation: If you set the video mode to 1080x948@120 Hz, then the DK2
+% will only display a monoscopic image identical to both eyes, but at a refresh
+% rate of 120 Hz. This allows you to (ab)use the DK2 as a high-speed 120 Hz
+% monitor!
 %
 % 'basicRequirements' defines basic requirements for the task. Currently
 % defined are the following strings which can be combined into a single
@@ -501,6 +506,8 @@ if cmd == 0
   end
 
   % Need to create a display list with VR warp blit first:
+  imax = 2;
+
   dl = glGenLists(1);
   glNewList(dl, GL.COMPILE);
 
@@ -517,11 +524,29 @@ if cmd == 0
     glViewport(0, 0, hmd{handle}.panelWidth, hmd{handle}.panelHeight);
   end
 
+  % Is this a Rift DK2 in special 1080x948 @ 120Hz video mode?
+  if strcmp(hmd{handle}.modelName, 'Rift (DK2)') && hmd{handle}.panelRotated && (hmd{handle}.panelWidth == 948)
+    % Yes. In this mode, the content of the framebuffer is considered a monoscopic image,
+    % and it is replicated into the top and bottom half of the display panel, thereby
+    % cloning the framebuffer as identical image into left and right eye of the user.
+    % Iow. the Rift acts as a monoscopic image viewer, with 120 Hz refresh rate. It still
+    % needs lens distortion correction and color abberation correction though. Ergo still
+    % run our warp shader, but only the left-eye one, sourcing from one input texture,
+    % doing the warp:
+    imax = 1;
+
+    % Extend the blitted quad from that one shader pass to the full framebuffer/viewport
+    % size, by some translation and scaling of the quad. The Rift will undo this during
+    % "cloning" of the "fullscreen" quad into both eyes:
+    glTranslatef(1, 0, 0);
+    glScalef(2, 1, 1);
+  end
+
   glMatrixMode(GL.PROJECTION);
   glPushMatrix;
   glLoadIdentity;
 
-  for i=1:2
+  for i=1:imax
     glUseProgram(hmd{handle}.glsl(i));
     glBindTexture(GL.TEXTURE_2D, hmd{handle}.inTex(i));
     glBegin(GL.QUADS);
@@ -1641,6 +1666,16 @@ if strcmpi(cmd, 'PerformPostWindowOpenSetup')
     % Yes. Disable ipd correction as it seems to do more harm than good
     % on the only HMD on which it would be used - the Rift CV:
     hmd{handle}.ipd = 0;
+  end
+
+  % Is this a Rift DK2 in special 1080x948 @ 120Hz video mode? And no Monoscopic mode selected?
+  if strcmp(hmd{handle}.modelName, 'Rift (DK2)') && hmd{handle}.panelRotated && (hmd{handle}.panelWidth == 948) && ...
+     isempty(strfind(hmd{myhmd.handle}.basicTask, 'Monoscopic'))
+     % Yes. Inform user that this is probably suboptimal, as stereoscopic display
+     % won't work in this op-mode of the DK2:
+     fprintf('PsychOpenHMDVR: WARNING: Rift DK2 in 1080x948@120 Hz MONO mode detected, but you are not using the Monoscopic\n');
+     fprintf('PsychOpenHMDVR: WARNING: basicTask. Note that stereoscopic rendering and display will not work. You only get the\n');
+     fprintf('PsychOpenHMDVR: WARNING: left eye view displayed to both eyes, while still wasting resources on stereo rendering.\n');
   end
 
   % Return success result code 1:
