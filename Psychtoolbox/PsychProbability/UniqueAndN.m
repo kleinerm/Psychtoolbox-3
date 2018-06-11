04d3f42d91560bc35f997d1c03b38a8042e14630
Diederick C. Niehorster <dcnieho@gmail.com>
2018-06-11 18:40:24 +0200

    UniqueAndN gained byRow option (#486)
    UniqueAndN: Add optional operateByRow option.
    
    Allows to find unique rows instead of unique elements.
    Also some minor cleanups to code and help.

--- Psychtoolbox/PsychProbability/UniqueAndN.m
+++ Psychtoolbox/PsychProbability/UniqueAndN.m
@@ -1,14 +1,25 @@
-function [b,n] = UniqueAndN(in)
-% [uniques,n] = uniqueen(input)
+function [b,n] = UniqueAndN(input,operateByRow)
+% [uniques,n] = UniqueAndN(input,[operateByRow=false])
 %
 % returns the unique elements in input (sorted) and
 % the number of times the item ocurred
+% optionally does so for unique rows (operateByRow=true)
+% instead of unique elements
 % 
 % DN    2008
+% DN    2017 Added byRow option
 
-in      = sort(in);  % Necessary for the trick below to work
-
-[b,i,j] = unique(in);
+if nargin<2 || isempty(operateByRow) || ~operateByRow
+    
+    input   = sort(input);  % Necessary for the trick below to work
+    
+    [b,~,j] = unique(input);
+    
+else
+    input   = sortrows(input);
+    
+    [b,~,j] = unique(input,'rows');
+end
 
 d       = diff([0; j(:); max(j)+1]);
 
