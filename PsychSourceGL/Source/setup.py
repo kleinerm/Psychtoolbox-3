1a715972652a1361e95e572ee36e6dc5dc9780b1
Mario Kleiner <mario.kleiner.de@gmail.com>
2018-06-19 17:37:21 +0200

    New file setup.py: A rudimentary build file for initial bringup.
    Only builds GetSecs, and is not yet complete. But it shows the basics,
    and it allows a compiler + linker run, although loading the extension
    fails due to missing symbols, etc.

---
+++ PsychSourceGL/Source/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+
+GetSecs = Extension('GetSecs',
+                    extra_compile_args = ['-Wno-date-time'],
+                    define_macros = [('PTBMODULE_GetSecs', None), ('PTBOCTAVE3MEX', None), ('PSYCH_LANGUAGE', 'PSYCH_PYTHON')],
+                    include_dirs = ['Common/Base', 'Linux/Base', 'Common/GetSecs', 'Common/Screen'],
+                    sources = ['Common/Base/MiniBox.c', 'Common/Base/PsychInstrument.c', 'Common/Base/ProjectTable.c', 'Common/Base/PsychError.c',
+                               'Common/Base/PsychHelp.c', 'Common/Base/PsychRegisterProject.c',  'Common/Base/PsychVersioning.c', 'Common/Base/PsychAuthors.c',
+                               'Common/Base/PsychInit.c', 'Common/Base/MODULEVersion.c', 'Common/Base/PsychScriptingGlue.c', 'Linux/Base/PsychTimeGlue.c',
+                               'Common/GetSecs/GetSecs.c', 'Common/GetSecs/RegisterProject.c'],
+                    libraries = ['c', 'rt']
+                   )
+
+setup (name = 'PackageName',
+        version = '1.0',
+        description = 'This is a demo package',
+        ext_modules = [GetSecs])
