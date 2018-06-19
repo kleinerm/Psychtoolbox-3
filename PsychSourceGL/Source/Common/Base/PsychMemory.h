5882376ffe5cd82a4c61fdbe9ae8fd4cce11b91b
Mario Kleiner <mario.kleiner.de@gmail.com>
2018-06-19 17:30:42 +0200

    PsychMemory: Formatting cleanup. Compiler warning fixes for non-Matlab/Octave.

--- PsychSourceGL/Source/Common/Base/PsychMemory.h
+++ PsychSourceGL/Source/Common/Base/PsychMemory.h
@@ -1,43 +1,41 @@
 /*
-  Psychtoolbox2/Source/Common/PsychMemory.h		
-  
-  AUTHORS:
-  Allen.Ingling@nyu.edu				awi
-  mario.kleiner@tuebingen.mpg.de	mk
+  Psychtoolbox/Source/Common/PsychMemory.h
 
-  PLATFORMS: All 
-  
+  AUTHORS:
+
+  Allen.Ingling@nyu.edu             awi
+  mario.kleiner.de@gmail.com        mk
+
+  PLATFORMS: All
+
   PROJECTS: All
 
   HISTORY:
-  09/04/02  awi		Wrote it.
+
+  09/04/02  awi     Wrote it.
   05/10/06  mk      Added our own allocator for Octave-Port.
   03/19/11  mk      Make 64-bit clean.  
 
-  DESCRIPTION:
-
-  TO DO: 
-
 */
 
-//begin include once 
+//begin include once
 #ifndef PSYCH_IS_INCLUDED_PsychMemory
 #define PSYCH_IS_INCLUDED_PsychMemory
 
 // Convert a double value (which encodes a memory address) into a ptr:
-void*  PsychDoubleToPtr(volatile double dptr);
+void* PsychDoubleToPtr(volatile double dptr);
 
 // Convert a memory address pointer into a double value:
 double PsychPtrToDouble(void* ptr);
 
 //allocate memory which is valid between until control returns to
-//the calling module
+//the calling module:
 void *PsychCallocTemp(size_t n, size_t size);
 void *PsychMallocTemp(size_t n);
 
 //free memory
 #if PSYCH_LANGUAGE == PSYCH_MATLAB
-	#define PsychFreeTemp 			mxFree
+    #define PsychFreeTemp  mxFree
 #else
 
 // Our own allocator / memory manager is used.
@@ -50,28 +48,22 @@ void PsychFreeTemp(void* ptr);
 // Master cleanup routine: Frees all allocated memory.
 void PsychFreeAllTempMemory(void);
 
-// Pointer to the start of our list of allocated temporary memory buffers:
-static size_t* PsychTempMemHead = NULL;
-// Total count of allocated memory in Bytes:
-static size_t totalTempMemAllocated = 0;
-
 #endif
 
 //allocate memory which is valid while the module is loaded
 
 /*
-	For Matlab this should 
-	1- allocate memory with mxCalloc or mxMalloc
-	2- call mexMakeMemoryPersistent on the pointer
-	3- store the pointer in a linked list of pointers 
-	   to memory which should be freed with MexAtExit
-	   when the module is purged
-	   
-	do something similar on cleanup for environments where we use
-	genuine malloc and calloc and free instead of the matlab 
-	versions.
-*/
+    For Matlab this should 
+    1- allocate memory with mxCalloc or mxMalloc
+    2- call mexMakeMemoryPersistent on the pointer
+    3- store the pointer in a linked list of pointers 
+        to memory which should be freed with MexAtExit
+        when the module is purged
 
+    do something similar on cleanup for environments where we use
+    genuine malloc and calloc and free instead of the matlab 
+    versions.
+*/
 
 //end include once
 #endif
