eac8411a26d2369ee8ca25cf60c869df7e0cba1d
Mario Kleiner <mario.kleiner.de@gmail.com>
2018-07-14 23:17:02 +0200

    PsychHID: White-space/indentation/formatting cleanup in *Report* functions.
    -> Prep for some serious work.
    -> No functional change intended.

--- PsychSourceGL/Source/Common/PsychHID/PsychHIDSetReport.c
+++ PsychSourceGL/Source/Common/PsychHID/PsychHIDSetReport.c
@@ -1,39 +1,41 @@
 /*
- Source/Common/PsychHID/PsychHIDSetReport.c
-
- PROJECTS: PsychHID
-
- PLATFORMS:  All.
-
- AUTHORS:
-    denis.pelli@nyu.edu                 dgp
-    mario.kleiner@tuebingen.mpg.de      mk
-
- HISTORY:
- 3/15/05  dgp		Wrote it.
- 4/23/05  dgp Now use low-level setReport instead of high-level HIDSetReport so that we can specify a time out (50 ms) instead of hanging forever when there is trouble.
+ * Source/Common/PsychHID/PsychHIDSetReport.c
+ *
+ * PROJECTS: PsychHID
+ *
+ * PLATFORMS:  All.
+ *
+ * AUTHORS:
+ *  denis.pelli@nyu.edu             dgp
+ *  mario.kleiner.de@gmail.com      mk
+ *
+ * HISTORY:
+ *  3/15/05  dgp        Wrote it.
+ *  4/23/05  dgp        Now use low-level setReport instead of high-level HIDSetReport so that we can specify a time out (50 ms) instead of hanging forever when there is trouble.
+ *
  */
 
 #include "PsychHID.h"
 
 static char useString[] = "err=PsychHID('SetReport',deviceNumber,reportType,reportID,report)";
-static char synopsisString[] = "Send a report to the connected USB HID device.\n"
-"\"deviceNumber\" specifies which device.\n"
-"\"reportType\" is 2=output, 3=feature (0 to just echo arguments).\n"
-"\"reportID\" is either zero or an integer (1 to 255) specifying the topic, e.g. read analog, read digital, write analog, etc. "
-"If you provide a non-zero reportID, the first byte of your \"report\" will be overwritten with this reportID. You have to "
-"take this into account, ie., leave a leading byte of space for the reportID to avoid corrupting your actual report data. "
-"If reportID is zero, then your \"report\" will be sent as-is, without any special treatment of the first byte.\n"
-"\"report\" must be an array of char or integer (8-, 16-, or 32-bit, signed or unsigned) holding "
-"the correct total number of bytes.\n"
-"The returned value \"err.n\" is zero upon success and a nonzero error code upon failure, "
-"as spelled out by \"err.name\" and \"err.description\".\n";
+static char synopsisString[] =
+    "Send a report to the connected USB HID device.\n"
+    "\"deviceNumber\" specifies which device.\n"
+    "\"reportType\" is 2=output, 3=feature (0 to just echo arguments).\n"
+    "\"reportID\" is either zero or an integer (1 to 255) specifying the topic, e.g. read analog, read digital, write analog, etc. "
+    "If you provide a non-zero reportID, the first byte of your \"report\" will be overwritten with this reportID. You have to "
+    "take this into account, ie., leave a leading byte of space for the reportID to avoid corrupting your actual report data. "
+    "If reportID is zero, then your \"report\" will be sent as-is, without any special treatment of the first byte.\n"
+    "\"report\" must be an array of char or integer (8-, 16-, or 32-bit, signed or unsigned) holding "
+    "the correct total number of bytes.\n"
+    "The returned value \"err.n\" is zero upon success and a nonzero error code upon failure, "
+    "as spelled out by \"err.name\" and \"err.description\".\n";
 
 static char seeAlsoString[] = "GetReport";
 
 /*
- The HID firmware notes I have to program the Measurement Computing PMD1208FS indicate that the reportID is one byte. It specifies
- what the report is for (0x03 read digital, 0x04 write digital, 0x10 read analog, 0x14 write analog, etc.).
+ * The HID firmware notes I have to program the Measurement Computing PMD1208FS indicate that the reportID is one byte. It specifies
+ * what the report is for (0x03 read digital, 0x04 write digital, 0x10 read analog, 0x14 write analog, etc.).
  */
 
 extern double AInScanStart;
@@ -58,36 +60,45 @@ PsychError PSYCHHIDSetReport(void)
     int i;
 
     PsychPushHelp(useString, synopsisString, seeAlsoString);
-    if (PsychIsGiveHelp()) {PsychGiveHelp(); return(PsychError_none);};
+    if (PsychIsGiveHelp()) { PsychGiveHelp(); return(PsychError_none); };
+
     PsychErrorExit(PsychCapNumOutputArgs(1));
     PsychErrorExit(PsychCapNumInputArgs(4));
+
     outErr = PsychGetOutArgMxPtr(1);
+
     PsychCopyInIntegerArg(1, TRUE, &deviceIndex);
     PsychCopyInIntegerArg(2, TRUE, &reportType);
     PsychCopyInIntegerArg(3, TRUE, &reportID);
+
     report = PsychGetInArgMxPtr(4);
     reportBuffer = (void *)mxGetData(report);
 
     switch (mxGetClassID(report)) {
-    case mxCHAR_CLASS:
-    case mxINT8_CLASS:
-    case mxUINT8_CLASS:
-    case mxINT16_CLASS:
-    case mxUINT16_CLASS:
-    case mxINT32_CLASS:
-    case mxUINT32_CLASS:
-        break;
-    default:
-        PrintfExit("\"report\" array must be char or integer (8-, 16-, or 32-bit).");
-        break;
+        case mxCHAR_CLASS:
+        case mxINT8_CLASS:
+        case mxUINT8_CLASS:
+        case mxINT16_CLASS:
+        case mxUINT16_CLASS:
+        case mxINT32_CLASS:
+        case mxUINT32_CLASS:
+            break;
+
+        default:
+            PrintfExit("\"report\" array must be char or integer (8-, 16-, or 32-bit).");
+            break;
     }
 
     reportSize = mxGetElementSize(report)*mxGetNumberOfElements(report);
-    if (reportSize > MAXREPORTSIZE) PsychErrorExitMsg(PsychError_user, "Tried to send a HID report which exceeds the maximum allowable size! Aborted.");
-    if (reportSize < 1) PsychErrorExitMsg(PsychError_user, "Tried to send an empty HID report! Aborted.");
+    if (reportSize > MAXREPORTSIZE)
+        PsychErrorExitMsg(PsychError_user, "Tried to send a HID report which exceeds the maximum allowable size! Aborted.");
+
+    if (reportSize < 1)
+        PsychErrorExitMsg(PsychError_user, "Tried to send an empty HID report! Aborted.");
 
     // No invalid numbers, no input reports for 'SetReport':
-    if (reportType < 0 || reportType > 3 || reportType == 1) PsychErrorExitMsg(PsychError_user, "Invalid 'reportType' for this function provided!");
+    if (reportType < 0 || reportType > 3 || reportType == 1)
+        PsychErrorExitMsg(PsychError_user, "Invalid 'reportType' for this function provided!");
 
     PsychHIDVerifyInit();
 
@@ -107,13 +118,14 @@ PsychError PSYCHHIDSetReport(void)
     #if PSYCH_SYSTEM == PSYCH_OSX
         IOHIDDeviceInterface122 **interface = NULL;
         interface = PsychHIDGetDeviceInterfacePtrFromIndex(deviceIndex);
-        if (interface) error = (*interface)->setReport(interface, reportType - 1, reportID, reportBuffer, (psych_uint32)reportSize, 50, NULL, NULL, NULL);
-        else PrintfExit("PsychHID SetReport: Bad interface.\n");
+        if (interface)
+            error = (*interface)->setReport(interface, reportType - 1, reportID, reportBuffer, (psych_uint32)reportSize, 50, NULL, NULL, NULL);
+        else
+            PrintfExit("PsychHID SetReport: Bad interface.\n");
 
         // Timestamp:
-        if (reportID == 0x11) {
+        if (reportID == 0x11)
             PsychGetPrecisionTimerSeconds(&AInScanStart);
-        }
 
         // Error condition?
         PsychHIDErrors(NULL, error, &name, &description);
@@ -143,12 +155,12 @@ PsychError PSYCHHIDSetReport(void)
         }
 
         // Timestamp:
-        if (reportID == 0x11) {
+        if (reportID == 0x11)
             PsychGetPrecisionTimerSeconds(&AInScanStart);
-        }
 
         // Positive value means no error:
-        if (error >= 0) error = 0;
+        if (error >= 0)
+            error = 0;
 
         // Error condition?
         PsychHIDErrors((hid_device*)device->interface, error, &name, &description);
