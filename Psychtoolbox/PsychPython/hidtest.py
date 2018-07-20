c6051e36a7b54252c1441fbc0ced16e20b12a476
Mario Kleiner <mario.kleiner.de@gmail.com>
2018-07-21 01:59:51 +0200

    PsychPython: Add two very rudimentary wip test scripts.
    - hidtest.py to test most basic PsychHID KbQueue functionality.
    - ppatest.py to test basic PsychPortAudio functionality, also
                 implicitely GetSecs, WaitSecs, and PsychHID('KbCheck').
    
    This is mosly a backup commit to simplify moving this stuff around.
    It will probably get replaced by better stuff soon.

---
+++ Psychtoolbox/PsychPython/hidtest.py
@@ -0,0 +1,57 @@
+# hidtest.py - Basic test of PsychHID for Python
+# Also tests WaitSecs, as it is utilized here.
+#
+# Collects key responses for 5 seconds via KbQueues while being blocked on
+# the main thread. The disabled latter part will perform some low-level access
+# to a USB-HID device, to test HID report send/receive. However, this is so
+# device specific as being useless without customization to your specific machine
+# OS and HID device. Rest assured it has some use for Mario's laptop.
+#
+# The test will be subjec to change without notice, it is throwaway wip code.
+#
+# (c) 2018 Mario Kleiner - Licensed under MIT license.
+
+from psychtoolboxclassic import *
+
+def run():
+
+    # KbQueueTest
+    WaitSecs('YieldSecs', 1);
+    keys = [1] * 256;
+    PsychHID('KbQueueCreate', [], keys);
+    PsychHID('KbQueueStart');
+    PsychHID('Keyboardhelper', -12);
+    WaitSecs('YieldSecs', 5);
+    PsychHID('KbQueueStop');
+    PsychHID('Keyboardhelper', -10);
+    while PsychHID('KbQueueFlush'):
+        evt = PsychHID('KbQueueGetEvent');
+        print(evt);
+
+    PsychHID('KbQueueRelease');
+    return;
+
+    # USB HID reports test:
+    #hiddevs = PsychHID('Devices')
+    mouseIndex = 5;
+    wheelDelta = 0;
+    WaitSecs('YieldSecs', 1);
+
+    options = dict();
+    options['print'] = 0;
+    err = PsychHID('ReceiveReports', mouseIndex, options);
+    print(err);
+
+    while not PsychHID('KbCheck')[0]:
+        rep = PsychHID('GetReport', mouseIndex, 1, 0, 5);
+        print(rep);
+        WaitSecs('YieldSecs', 1);
+
+    err = PsychHID('ReceiveReportsStop', mouseIndex)
+    print(err);
+
+    print('\n\nNow all the other remaining ones:\n\n');
+    [rep, errs] = PsychHID('GiveMeReports', mouseIndex);
+    print(errs);
+    print('\n\nReports:\n\n');
+    print(rep);
---
+++ Psychtoolbox/PsychPython/ppatest.py
@@ -0,0 +1,122 @@
+# ppatest.py - Basic test of PsychPortAudio for Python
+# Also tests WaitSecs, GetSecs, and PsychHID basics, as
+# they are utilized here.
+#
+# Plays a beep tone, then another beep tone, then records sound for 10 seconds,
+# then plays it back until the end or a key is pressed.
+# The test will be subjec to change without notice, it is throwaway wip code.
+#
+# (c) 2018 Mario Kleiner - Licensed under MIT license.
+
+from psychtoolboxclassic import *
+from math import *
+
+def printstatus(pahandle):
+    info = PsychPortAudio('GetStatus', pahandle);
+    print(info);
+    return(info['Active']);
+
+def run():
+
+    # Samplerate:
+    Fs = 48000;
+
+    # Tone frequency:
+    f = 500;
+
+    # Length of audio vector - 1 secs of playback:
+    sample = 1 * Fs;
+
+    # Build sinewave:
+    a = [0] * sample;
+
+    for n in range(sample):
+        a[n] = sin(2 * pi * f * n / Fs);
+
+    # Replicated into two rows - One row for each stereo-channel, ie.
+    # m'th row == m'th channel, n'th column = n'th sample frame:
+    stereowav = [a, a];
+
+    hiddevs = PsychHID('Devices');
+    print(hiddevs)
+
+    PsychPortAudio('Verbosity', 5);
+
+    # Open audio device: Default audio device, default opmode (playback), default
+    # latency/timing-precision (low-latency, high precision), sample rate Fs,
+    # stereo (2) channel output:
+    pahandle = PsychPortAudio('Open', [], [], [], Fs, 2);
+
+    PsychPortAudio('Volume', pahandle, 0.5);
+
+    # Fill in audio matrix for playback:
+    PsychPortAudio('FillBuffer', pahandle, stereowav);
+
+    # Start playback for one repetition (1), 5 seconds from now, wait for sound onset:
+    PsychPortAudio('Start', pahandle, 1, GetSecs() + 1, 1)
+
+    # Go into a loop that prints playback status once a second, as long as playback
+    # is active:
+    info = PsychPortAudio('GetStatus', pahandle);
+    print(info, ' Spec ', info['Active']);
+    while printstatus(pahandle):
+        WaitSecs('YieldSecs', 1);
+
+    # Wait for sound to stop by itself, block until then:
+    [startTime, endPositionSecs, xruns, estStopTime] = PsychPortAudio('Stop', pahandle, 1)
+    print('StartTime', startTime, 'secs. Stop time', estStopTime, 'secs.\n');
+
+    for n in range(sample):
+        a[n] = sin(2 * pi * f * 1.5 * n / Fs);
+
+    # Replicated into two rows - One row for each stereo-channel, ie.
+    # m'th row == m'th channel, n'th column = n'th sample frame:
+    stereowav2 = [a, a];
+
+    b1 = PsychPortAudio('CreateBuffer', pahandle, stereowav2);
+    PsychPortAudio('FillBuffer', pahandle, b1);
+    PsychPortAudio('Start', pahandle, 1, GetSecs() + 1, 1)
+    [startTime, endPositionSecs, xruns, estStopTime] = PsychPortAudio('Stop', pahandle, 1)
+    print('StartTime', startTime, 'secs. Stop time', estStopTime, 'secs.\n');
+
+    # Close sound device:
+    PsychPortAudio('Close', pahandle);
+
+    pahandle = PsychPortAudio('Open', [], 2, 0, Fs, 2);
+
+    # Preallocate an internal audio recording  buffer with a capacity of 10 seconds:
+    PsychPortAudio('GetAudioData', pahandle, 10);
+
+    # Start audio capture immediately and wait for the capture to start.
+    # We set the number of 'repetitions' to zero,
+    # i.e. record until recording is manually stopped.
+    PsychPortAudio('Start', pahandle, 0, 0, 1);
+
+    [audiodata, a, b, c] = PsychPortAudio('GetAudioData', pahandle, None, 9.9, None, 0);
+    print('Type', type(audiodata), 'Shape', audiodata.shape, 'Datatype', audiodata.dtype);
+
+    PsychPortAudio('Close', pahandle);
+
+    pahandle = PsychPortAudio('Open', [], [], [], Fs, 2);
+
+    PsychPortAudio('Volume', pahandle, 1);
+
+    # Fill in audio matrix for playback:
+    PsychPortAudio('FillBuffer', pahandle, audiodata);
+
+    # Start playback for one repetition (1), 5 seconds from now, wait for sound onset:
+    PsychPortAudio('Start', pahandle, 1, GetSecs() + 1, 1)
+
+    # Go into a loop that prints playback status once a second, as long as playback
+    # is active:
+    info = PsychPortAudio('GetStatus', pahandle);
+    print(info, ' Spec ', info['Active']);
+    while printstatus(pahandle) and not PsychHID('KbCheck')[0]:
+        WaitSecs('YieldSecs', 1);
+
+    # Stop playback:
+    [startTime, endPositionSecs, xruns, estStopTime] = PsychPortAudio('Stop', pahandle)
+    print('StartTime', startTime, 'secs. Stop time', estStopTime, 'secs.\n');
+
+    # Close sound device:
+    PsychPortAudio('Close', pahandle);
