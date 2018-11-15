bd23d7fe32ce49f64fca9352d0a4a3981e1bf7e1
Mario Kleiner <mario.kleiner.de@gmail.com>
2018-11-15 05:58:29 +0100

    Add CV1Test.m as a timing test for Oculur Rift CV1 via Photometer.
    Uses a ColorCal-II connected to a RTBox to measure onset time
    of white-flashes on the HMD, to try to get measurement of real
    onset time on the HMD.
    
    So far shows substantial discrepancy between our timestamping
    and reality.

--- Psychtoolbox/PsychHardware/PsychVRToolbox/Contents.m
+++ Psychtoolbox/PsychHardware/PsychVRToolbox/Contents.m
@@ -2,7 +2,8 @@
 %
 % Functions for use of and access to Virtual Reality devices.
 %
+% CV1Test               - A timing test script for HMDs by use of a photometer.
 % OculusVR              - How to setup Oculus VR head mounted displays and general intro.
 % PsychVRHMD            - General driver for VR head mounted displays (HMDs).
-% PsychOculusVR         - Driver for devices from Oculus VR, e.g., the Oculus Rift.
+% PsychOculusVR         - Driver for devices from Oculus VR, e.g., the Oculus Rift, using the v0.5 SDK.
 % PsychOpenHMDVR        - Driver for devices supported by OpenHMD (http://www.openhmd.net).
