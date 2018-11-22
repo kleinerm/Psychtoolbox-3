dd8a2c95295dd5fe3733f78b7a90ce6b4c6501dc
kleinerm <mario.kleiner.de@gmail.com>
2018-11-22 04:43:34 +0100

    PsychOculusVRCore1: On Octave, link against CAPI shim.
    Turns out, our old DLL hack no longer works on Octave 4.4.1+ with the current Oculus runtime. The libOVRRT61_1.dll is not found and we get an invalid mex file error at mex file load time.
    
    Fix this by compiling the OVR_CAPIshim.c C-API loader shim and
    other utility functions directly from the SDK into the mex file.
    
    This prevents the mex file load error, as tested on Octave-4.4.1 against the current Oculus runtime on Windows 10.
    
    The driver/shim will locate and load the proper libOVRRT64_1.dll from the installed runtime at ovr_Initialize() time, doing the right thing in a non-hacky way.
    
    Also clarify we need exactly SDK version 1.16. Earlier versions lack
    needed functionality, and the later 1.17+ SDK introduces backwards incompatible API breakage!

--- PsychSourceGL/Source/windowsmakeit64_twisty.m
+++ PsychSourceGL/Source/windowsmakeit64_twisty.m
@@ -121,9 +121,11 @@ if onoctave == 0
 
     if what == 13
         % Build PsychOculusVRCore1 for 64-Bit Matlab:
-        % Needs the Oculus VR SDK v1.16 or later installed side-by-side to the Psychtoolbox-3
+        % Needs the Oculus VR SDK v1.16 installed side-by-side to the Psychtoolbox-3
         % folder, so that it shares the same parent folder as Psychtoolbox-3,
         % and the SDK must be renamed from OculusSDK to OculusSDK1Win.
+        % CAUTION: Need exactly v1.16 SDK, no earlier or later versions, due to
+        % backwards incompatible API changes in v1.17+
         mex -v -outdir ..\Projects\Windows\build -output PsychOculusVRCore1 -DPTBMODULE_PsychOculusVRCore1 -largeArrayDims -DWIN32 -I"C:\Program Files\Microsoft SDKs\Windows\v7.1\Include" -I..\..\..\OculusSDK1Win\LibOVR\Include -ICommon\Base -IWindows\Base -ICommon\PsychOculusVRCore1 Common\PsychOculusVRCore1\*.c Windows\Base\*.c Common\Base\*.c kernel32.lib user32.lib winmm.lib ..\..\..\OculusSDK1Win\LibOVR\Lib\Windows\x64\Release\VS2010\LibOVR.lib
         movefile(['..\Projects\Windows\build\PsychOculusVRCore1.' mexext], [PsychtoolboxRoot 'PsychBasic\MatlabWindowsFilesR2007a\']);
     end
@@ -324,16 +326,19 @@ else
 
     if what == 13
         % Build PsychOculusVRCore1 for 64-Bit Octave-4.4.1 or later:
-        % Needs the Oculus VR SDK v1.16 or later installed side-by-side to the Psychtoolbox-3
+        % Needs the Oculus VR SDK v1.16 installed side-by-side to the Psychtoolbox-3
         % folder, so that it shares the same parent folder as Psychtoolbox-3,
         % and the SDK must be renamed from OculusSDK to OculusSDK1Win.
+        % CAUTION: Need exactly v1.16 SDK, no earlier or later versions, due to
+        % backwards incompatible API changes in v1.17+
         try
-            % We copy the installed Oculus v1.16+ runtime DLL from
-            % C:\Program Files\Oculus\Support\oculus-runtime\LibOVRRT64_1.dll into the
-            % SDK folder and abuse that as import library for our mex file. Weird, but
-            % true - this works on Octave-4.4.1 64-Bit under Windows:
-            copyfile('C:\Program Files\Oculus\Support\oculus-runtime\LibOVRRT64_1.dll', '..\..\..\OculusSDK1Win\LibOVR\Lib\Windows\Win32\Release\VS2010\');
-            mexoctave -g -v --output ..\Projects\Windows\build\PsychOculusVRCore1.mex -DPTBMODULE_PsychOculusVRCore1 -DPTBOCTAVE3MEX -I..\..\..\OculusSDK1Win\LibOVR\Include -ICommon\Base -IWindows\Base -ICommon\PsychOculusVRCore1 Common\PsychOculusVRCore1\*.c Windows\Base\*.c Common\Base\*.c kernel32.lib user32.lib winmm.lib -L..\..\..\OculusSDK1Win\LibOVR\Lib\Windows\Win32\Release\VS2010 -lOVRRT64_1
+            % For the Octave build, we compile the OVR_CAPIshim.c shim file and
+            % other helper C files from the SDK directly into our mex file, instead
+            % of statically linking against LibOVR.lib, as that .lib import file only
+            % works with MSVC, but not with Octave's gcc based build system. The shim
+            % will locate and runtime-link against the libOVRRT_64_1.dll of the installed
+            % Oculus VR runtime during initialization:
+            mexoctave -g -v --output ..\Projects\Windows\build\PsychOculusVRCore1.mex -DPTBMODULE_PsychOculusVRCore1 -DPTBOCTAVE3MEX -I..\..\..\OculusSDK1Win\LibOVR\Include -ICommon\Base -IWindows\Base -ICommon\PsychOculusVRCore1 Common\PsychOculusVRCore1\*.c Windows\Base\*.c Common\Base\*.c ..\..\..\OculusSDK1Win\LibOVR\Src\*.c* kernel32.lib user32.lib winmm.lib
             movefile(['..\Projects\Windows\build\PsychOculusVRCore1.' mexext], target);
         catch %#ok<*CTCH>
             % Empty. We just want to make sure the delete() call below is executed
