d5de9bddcce9765bf5577e9a02e6f3713a66847b
Jon Peirce <jon.peirce@gmail.com>
2018-12-14 13:53:51 +0000

    Python: moved "tests" into demos and added pythonic versions

---
+++ PsychPython/demos/hidtest_pythonic.py
@@ -0,0 +1,48 @@
+# hidtest.py - Basic test of PsychHID for Python
+# Also tests WaitSecs, as it is utilized here.
+#
+# Collects key responses for 5 seconds via KbQueues while being blocked on
+# the main thread. The disabled latter part will perform some low-level access
+# to a USB-HID device, to test HID report send/receive. However, this is so
+# device specific as being useless without customization to your specific machine
+# OS and HID device. Rest assured it has some use for Mario's laptop.
+#
+# The test will be subjec to change without notice, it is throwaway wip code.
+#
+# (c) 2018 Mario Kleiner - Licensed under MIT license.
+
+from psychtoolbox import hid
+import psychtoolbox as ptb
+
+print("Devices:", hid.num_devices())
+for dev in hid.devices():
+    print(dev['usagePageValue'], dev['usageValue'], dev['product'])
+kbInds, kbNames, kbDevices = hid.get_keyboard_indices('Millikey')
+
+print(kbNames)
+
+def run():
+
+    # KbQueueTest
+    ptb.WaitSecs('YieldSecs', 1);
+    keys = [1] * 256;
+    keyboard = hid.Keyboard(kbInds[0])
+
+    # keyboard.queue_create(num_slots=10000)  # not really needed
+
+    ptb.PsychHID('Keyboardhelper', -12);  # stops keys going to stdout
+
+    keyboard.queue_start()
+    ptb.WaitSecs('YieldSecs', 5);
+    keyboard.queue_stop()
+
+    ptb.PsychHID('Keyboardhelper', -10);  # restarts keys going to stdout
+
+    while keyboard.flush():
+        evt = keyboard.queue_get_event()
+        print(evt)
+
+
+# if this was run as a script (not an import) then call run()
+if __name__ == "__main__":
+    run()
