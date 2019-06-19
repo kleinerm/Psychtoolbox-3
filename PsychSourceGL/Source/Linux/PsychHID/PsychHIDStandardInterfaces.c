b83effd377e9961f6e6332d833440021959ed811
Mario Kleiner <mario.kleiner.de@gmail.com>
2019-06-19 23:15:08 +0200

    PsychHID/Linux: Allow keyboard queue cookedKeys to handle non-US keyboards.
    Try to use X input methods and contexts + XwcLookupString() to
    map keypress events to wchar_t UCS-4 Unicode characters, instead
    of only mapping to Latin-1 / ASCII. Fallback to old method if new
    one fails.
    
    This takes the currently selected keyboard layout into account,
    instead of only mapping according to standard US keyboard layout.
    This allows, e.g., to return the unicode values for german umlauts
    if a german keyboard layout is set.
    
    One caveat is that at least on Ubuntu 18.04.2-LTS, keyboard layouts
    selected by KDE's keyboard layout GUI / switcher are not respected.
    US layout is always used. What does work is to use the commnand line
    command "setxkbmap -layout targetlayout", e.g., for selecting a
    german "de" layout:
    
    setxkbmap -layout de
    
    I think the OSX and Windows versions of PsychHID already take the
    keyboard layout into account.
    
    One limitations remains: GNU/Octave is still not able to represent
    char() strings in UTF-32 unicode like Matlab. It represents unicode
    in theory not at all, in practice it boils down to storing UTF-8
    unicode. This makes actual processing of non-LATIN 1 / non-ASCII
    characters like german umlauts in Octave somewhat painful:
    
    https://wiki.octave.org/International_Characters_Support

--- PsychSourceGL/Source/Linux/PsychHID/PsychHIDStandardInterfaces.c
+++ PsychSourceGL/Source/Linux/PsychHID/PsychHIDStandardInterfaces.c
@@ -40,6 +40,8 @@ static psych_condition KbQueueCondition;
 static psych_bool  KbQueueThreadTerminate;
 static psych_thread KbQueueThread;
 static XEvent KbQueue_xevent;
+static XIM x_inputMethod = NULL;
+static XIC x_inputContext = NULL;
 
 static XDevice* GetXDevice(int deviceIndex)
 {
@@ -170,6 +172,17 @@ void PsychHIDShutdownHIDStandardInterfaces(void)
         if (psychHIDKbQueueFirstPress[i]) {
             PsychHIDOSKbQueueRelease(i);
         }
+    }
+
+    // Delete input method and context if any was in use:
+    if (x_inputContext) {
+        XDestroyIC(x_inputContext);
+        x_inputContext = NULL;
+    }
+
+    if (x_inputMethod) {
+        XCloseIM(x_inputMethod);
+        x_inputMethod = NULL;
     }
 
     // Release keyboard queue mutex:
@@ -527,6 +540,8 @@ void KbQueueProcessEvents(psych_bool blockingSinglepass)
     int i, j, index, deviceid, numValuators;
     unsigned int screen_width, screen_height;
     char asciiChar;
+    wchar_t wideChar;
+    Status status_return;
 
     while (1) {
         XGenericEventCookie *cookie = &KbQueue_xevent.xcookie;
@@ -656,8 +671,14 @@ void KbQueueProcessEvents(psych_bool blockingSinglepass)
                         key.keycode      = index;
                         key.state        = event->mods.effective;
 
-                        if (1 == XLookupString(((XKeyEvent*) (&key)), &asciiChar, 1, NULL, NULL)) {
-                            // Mapped: Assign it.
+                        if (x_inputContext &&
+                            (1 == XwcLookupString(x_inputContext, &key, &wideChar, 1, NULL, &status_return)) &&
+                            (status_return == XLookupChars || status_return == XLookupBoth)) {
+                            // Mapped to wide character according to current keyboard layout:
+                            evt.cookedEventCode = (int) wideChar;
+                        }
+                        else if (1 == XLookupString((XKeyEvent*) &key, &asciiChar, 1, NULL, NULL)) {
+                            // Mapped to ISO Latin-1 or ASCII CTRL char: Assign it.
                             evt.cookedEventCode = (int) asciiChar;
                         }
                         else {
@@ -1150,6 +1171,37 @@ PsychError PsychHIDOSKbQueueCreate(int deviceIndex, int numScankeys, int* scanKe
 
     // Store associated X-Window handle, or zero for unspecified:
     psychHIDKbQueueXWindow[deviceIndex] = windowHandle;
+
+    if (x_inputMethod == NULL) {
+        // Create an input method and context in the currently set locale
+        // for use in translation to the currently set keyboard layout. This
+        // is used for the event.CookedKey of returned keyboard queue events.
+
+        // Try to use env setting for locale modifiers:
+        XSetLocaleModifiers("");
+        x_inputMethod = XOpenIM(thread_dpy, NULL, NULL, NULL);
+        if (!x_inputMethod) {
+            // Fallback to internal settings:
+            XSetLocaleModifiers("@im=none");
+            x_inputMethod = XOpenIM(thread_dpy, NULL, NULL, NULL);
+        }
+
+        if (!x_inputMethod) {
+            printf("PsychHID-WARNING: Failed to setup international keyboard handling due to failed input method creation.\n");
+        }
+        else {
+            x_inputContext = XCreateIC(x_inputMethod, XNInputStyle, XIMPreeditNone | XIMStatusNone, NULL);
+            if (!x_inputContext) {
+                printf("PsychHID-WARNING: Failed to setup international keyboard handling due to failed input context creation.\n");
+            }
+            else {
+                XSetICFocus(x_inputContext);
+            }
+        }
+
+        if (!x_inputContext)
+            printf("PsychHID-WARNING: Only US keyboard layouts will be mapped properly due to this failure for GetChar() et al.\n");
+    }
 
     // Create event buffer:
     if (!PsychHIDCreateEventBuffer(deviceIndex, numValuators, numSlots)) {
