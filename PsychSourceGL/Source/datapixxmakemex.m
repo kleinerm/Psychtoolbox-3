8e24ac532113c883962164d42a6207e216b579de
Mario Kleiner <mario.kleiner.de@gmail.com>
2019-07-28 23:02:41 +0200

    datapixxmakemex: Update for 64-Bit Matlab R2019a build on OSX.
    Needs a different way of passing linker arguments. The old way used to
    work on R2012a, but no longer on R2019a.

--- PsychSourceGL/Source/datapixxmakemex.m
+++ PsychSourceGL/Source/datapixxmakemex.m
@@ -90,8 +90,10 @@ function datapixxmakemex()
         S = [S ' ' PTBDIR 'PsychSourceGL/Source/OSX/Base/PsychTimeGlue.c'];
         if IsOctave
             S = [S ' ''-mmacosx-version-min=10.11'' '];
+            S = [S ' ''-Wl,-headerpad_max_install_names -F/System/Library/Frameworks/ -F/Library/Frameworks/ -framework ApplicationServices -framework CoreServices -framework CoreFoundation -framework Carbon -framework CoreAudio -framework IOKit,-syslibroot,/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk -mmacosx-version-min=10.11'' '];
+        else
+            S = [S ' LDFLAGS="\$LDFLAGS -framework ApplicationServices -framework CoreServices -framework CoreFoundation -framework Carbon -framework CoreAudio -framework IOKit" '];
         end
-        S = [S ' ''-Wl,-headerpad_max_install_names -F/System/Library/Frameworks/ -F/Library/Frameworks/ -framework ApplicationServices -framework CoreServices -framework CoreFoundation -framework Carbon -framework CoreAudio -framework IOKit,-syslibroot,/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk -mmacosx-version-min=10.11'' '];
         if (IsOctave)
             S = [S ' --output ' VPIXXDIR 'VPixx_Software_Tools/DatapixxToolbox_trunk/mexdev/build/octave/macosx/Datapixx.mex'];
         else
