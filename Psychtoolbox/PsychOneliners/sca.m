27848896462e16084252914ec98fb5784dcf6cf7
Mario Kleiner <mario.kleiner.de@gmail.com>
2020-01-06 17:57:10 +0100

    sca(): Also restore X-Screen size on Linux on close if 16 bpc mode was used.
    Execute the screenRestoreCmd if it was defined by PsychImaging.

--- Psychtoolbox/PsychOneliners/sca.m
+++ Psychtoolbox/PsychOneliners/sca.m
@@ -1,9 +1,13 @@
 function sca
 % sca -- Execute Screen('CloseAll');
-% This is just a convenience wrapper that allows you
-% to save typing that long, and frequently needed,  command.
-% It also unhides the cursor if hidden, and restores graphics card gamma
-% tables if they've been altered.
+% This is just a convenience wrapper that allows you to save typing the long,
+% and frequently needed, command.
+%
+% It also unhides the cursor if hidden, restores graphics card gamma tables if
+% they've been altered, and restores other low-level display system state, like
+% centering the mouse cursor on screen 0 of a multi-X-Screen Linux setup. For
+% Matlab it implements a workaround for Matlab Java GUI bugs related to resolution
+% switching and entering/leaving fullscreen display mode.
 %
 
 % History:
@@ -11,6 +15,10 @@ function sca
 % 5/31/8  Add CLUT restore call (MK).
 % 7/03/13 Add PsychJavaSwingCleanup call (MK).
 % 5/13/16 Add mouse pointer repositioning (MK).
+% 1/6/20  Add X-Screen size restore (MK).
+
+% screenRestoreCmd is defined by PsychImaging() in certain situations:
+global screenRestoreCmd;
 
 % Unhide the cursor if it was hidden:
 % Don't do this on Wayland for now...
@@ -22,6 +30,16 @@ for win = Screen('Windows')
     if Screen('WindowKind', win) == 1
         if Screen('GetWindowInfo', win, 4) > 0
             Screen('AsyncFlipEnd', win);
+        end
+    end
+end
+
+% Restore X-Screen sizes of not-yet-restored X-Screens:
+if IsLinux && ~isempty(screenRestoreCmd)
+    for i=1:length(screenRestoreCmd)
+        if ~isempty(screenRestoreCmd{i})
+            eval(screenRestoreCmd{i}, 'fprintf(''Oopsie!\n'')');
+            screenRestoreCmd{i} = [];
         end
     end
 end
