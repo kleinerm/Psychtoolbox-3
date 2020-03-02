24cb3a68960a51b3dd1ee39eece6c86ab687b240
iandol <iandol@aristotle>
2020-03-02 17:53:36 +0800

    fix whitespace to use spaces (PTB default)...

--- Psychtoolbox/PsychDemos/ProceduralColorGratingDemo.m
+++ Psychtoolbox/PsychDemos/ProceduralColorGratingDemo.m
@@ -8,15 +8,15 @@ function ProceduralColorGratingDemo(color1,color2,baseColor)
 % 01/03/2020 initial version (Ian Andolina)
 
 if ~exist('color1','var') || isempty(color1)
-	color1 = [1 0 0 1];
+    color1 = [1 0 0 1];
 end
 
 if ~exist('color2','var') || isempty(color2)
-	color2 = [0 1 0 1];
+    color2 = [0 1 0 1];
 end
 
 if ~exist('baseColor','var') || isempty(baseColor)
-	baseColor = [0.5 0.5 0.5 1];
+    baseColor = [0.5 0.5 0.5 1];
 end
 
 % Setup defaults and unit color range:
@@ -53,8 +53,8 @@ normalise = false;
 % Build a procedural texture, we also keep the shader as we will show how to
 % modify it (though not as efficient as using parameters in drawtexture)
 [texture, ~, shader] = CreateProceduralColorGrating(win, virtualSize,...
-	virtualSize, color1, color2, radius, normalise);
-			
+    virtualSize, color1, color2, radius, normalise);
+
 % These settings are the parameters passed in directly to DrawTexture
 % angle
 angle = 45;
@@ -66,24 +66,24 @@ frequency = 0.03;
 contrast = 0.5;
 % sigma < 0 is a sinusoid.
 sigma = -1.0;
-			
+
 % Preperatory flip
 showTime = 3;
 vbl = Screen('Flip', win);
 tstart = vbl + ifi; %start is on the next frame
 
 while vbl < tstart + showTime
-	% Draw a message
-	Screen('DrawText', win, 'Standard Color Sinusoidal Grating', 10, 10, [1 1 1]);
+    % Draw a message
+    Screen('DrawText', win, 'Standard Color Sinusoidal Grating', 10, 10, [1 1 1]);
     % Draw the shader texture with parameters
-	Screen('DrawTexture', win, texture, [], [],...
-		angle, [], [], baseColor, [], [],...
-		[phase, frequency, contrast, sigma]);
+    Screen('DrawTexture', win, texture, [], [],...
+        angle, [], [], baseColor, [], [],...
+        [phase, frequency, contrast, sigma]);
 
     Screen('DrawingFinished', win);
-    
+
     vbl = Screen('Flip', win, vbl + 0.5 * ifi);
-	phase = phase - 15;
+    phase = phase - 15;
 end
 
 %--- now we switch to a square wave grating using sigma >= 0
@@ -96,17 +96,17 @@ vbl = Screen('Flip', win);
 tstart = vbl + ifi; %start is on the next frame
 
 while vbl < tstart + showTime
-	% Draw a message
-	Screen('DrawText', win, 'Standard Color Squarewave Grating', 10, 10, [1 1 1]);
+    % Draw a message
+    Screen('DrawText', win, 'Standard Color Squarewave Grating', 10, 10, [1 1 1]);
     % Draw the shader texture with parameters
-	Screen('DrawTexture', win, texture, [], [],...
-		angle, [], [], baseColor, [], [],...
-		[phase, frequency, contrast, sigma]);
+    Screen('DrawTexture', win, texture, [], [],...
+        angle, [], [], baseColor, [], [],...
+        [phase, frequency, contrast, sigma]);
 
     Screen('DrawingFinished', win);
-    
+
     vbl = Screen('Flip', win, vbl + 0.5 * ifi);
-	phase = phase - 15;
+    phase = phase - 15;
 end
 
 %--- now we can change the parameters in the shader to be B-Y grating
@@ -121,17 +121,17 @@ vbl = Screen('Flip', win);
 tstart = vbl + ifi; %start is on the next frame
 
 while vbl < tstart + showTime
-	% Draw a message
-	Screen('DrawText', win, 'Blue-Yellow Color Squarewave Grating', 10, 10, [1 1 1]);
+    % Draw a message
+    Screen('DrawText', win, 'Blue-Yellow Color Squarewave Grating', 10, 10, [1 1 1]);
     % Draw the shader texture with parameters
-	Screen('DrawTexture', win, texture, [], [],...
-		angle, [], [], baseColor, [], [],...
-		[phase, frequency, contrast, sigma]);
+    Screen('DrawTexture', win, texture, [], [],...
+        angle, [], [], baseColor, [], [],...
+        [phase, frequency, contrast, sigma]);
 
     Screen('DrawingFinished', win);
-    
+
     vbl = Screen('Flip', win, vbl + 0.5 * ifi);
-	phase = phase - 15;
+    phase = phase - 15;
 end
 
 %--- now we change the baseColor to show its effect on the grating
@@ -142,19 +142,19 @@ vbl = Screen('Flip', win);
 tstart = vbl + ifi; %start is on the next frame
 
 while vbl < tstart + showTime
-	%draw the background with the same baseColor
-	Screen('FillRect',win, baseColor);
-	% Draw a message
-	Screen('DrawText', win, 'Blue-Yellow Grating modifying the base color', 10, 10, [1 1 1]);
+    %draw the background with the same baseColor
+    Screen('FillRect',win, baseColor);
+    % Draw a message
+    Screen('DrawText', win, 'Blue-Yellow Grating modifying the base color', 10, 10, [1 1 1]);
     % Draw the shader texture with parameters
-	Screen('DrawTexture', win, texture, [], [],...
-		angle, [], [], baseColor, [], [],...
-		[phase, frequency, contrast, sigma]);
+    Screen('DrawTexture', win, texture, [], [],...
+        angle, [], [], baseColor, [], [],...
+        [phase, frequency, contrast, sigma]);
 
     Screen('DrawingFinished', win);
-    
+
     vbl = Screen('Flip', win, vbl + 0.5 * ifi);
-	phase = phase - 15;
+    phase = phase - 15;
 end
 
 % Close onscreen window, release all resources:
--- Psychtoolbox/PsychGLImageProcessing/CreateProceduralColorGrating.m
+++ Psychtoolbox/PsychGLImageProcessing/CreateProceduralColorGrating.m
@@ -1,5 +1,5 @@
 function [id, rect, shader] = CreateProceduralColorGrating(windowPtr, width, height, ...
-	color1, color2, radius, normalise)
+    color1, color2, radius, normalise)
 % [id, rect, shader] = CreateProceduralColorGrating(windowPtr, width, height [, color1=[1 0 0]]  [, color2=[0 1 0]] [, radius=0])
 %
 % A procedural color grating shader that can generate either sinusoidal or square gratings varying between two colors. Initial parameters are color1, color2 and radius. When drawing the shader using Screen('DrawTexture|s'), you can pass additional values:
@@ -25,45 +25,41 @@ global GL;
 AssertGLSL;
 
 if nargin < 1 || isempty(windowPtr) 
-	error('You must provide a PTB window pointer!');
+    error('You must provide a PTB window pointer!');
 end
 
 if nargin < 2 || isempty(width) 
-	width = 500;
+    width = 500;
 end
 
 if nargin < 3 || isempty(height) 
-	height = width;
+    height = width;
 end
 
 if nargin < 4 || isempty(color1)
-	color1 = [1 0 0 1];
+    color1 = [1 0 0 1];
 elseif length(color1) == 3
-	color1 = [color1 1]; %add alpha
-elseif length(color1) == 4
-	% we are ok
-else
-	warning('color1 must be a 4 component RGBA vector [red green blue alpha], resetting color1 to red!');
-	color1 = [1 0 0 1];
+    color1 = [color1 1]; %add alpha
+elseif length(color1) ~= 4
+    warning('color1 must be a 4 component RGBA vector [red green blue alpha], resetting color1 to red!');
+    color1 = [1 0 0 1];
 end
 
 if nargin < 5 || isempty(color2)
-	color2 = [0 1 0 1];
+    color2 = [0 1 0 1];
 elseif length(color2) == 3
-	color2 = [color2 1]; %add alpha
-elseif length(color2) == 4
-	% we are ok
-else
-	warning('color2 must be a 4 component RGBA vector [red green blue alpha], resetting color2 to green!');
-	color2 = [0 1 0 1];
+    color2 = [color2 1]; %add alpha
+elseif length(color2) ~= 4
+    warning('color2 must be a 4 component RGBA vector [red green blue alpha], resetting color2 to green!');
+    color2 = [0 1 0 1];
 end
 
 if nargin < 6 || isempty(radius)
-	radius = 0;
+    radius = 0;
 end
 
 if nargin < 7 || isempty(normalise)
-	normalise = 0;
+    normalise = 0;
 end
 
 % Switch to windowPtr OpenGL context:
@@ -78,7 +74,7 @@ glUniform2f(glGetUniformLocation(shader, 'center'), width/2, height/2);
 glUniform4f(glGetUniformLocation(shader, 'color1'), color1(1),color1(2),color1(3),color1(4));
 glUniform4f(glGetUniformLocation(shader, 'color2'), color2(1),color2(2),color2(3),color2(4));
 if radius>0
-	glUniform1f(glGetUniformLocation(shader, 'radius'), radius); 
+    glUniform1f(glGetUniformLocation(shader, 'radius'), radius); 
 end
 glUniform1f(glGetUniformLocation(shader, 'normalise'), normalise);
 glUseProgram(0);
