6df8825214e6f3c0622974ccfb23d0739244f010
Mario Kleiner <mario.kleiner.de@gmail.com>
2020-10-29 05:32:48 +0100

    PsychtoolboxVersion: Update help text, clarify versioning scheme.

--- Psychtoolbox/PsychBasic/PsychtoolboxVersion.m
+++ Psychtoolbox/PsychBasic/PsychtoolboxVersion.m
@@ -1,54 +1,51 @@
-function [versionString, versionStructure]=PsychtoolboxVersion
-% OS X, Windows, Linux: ___________________________________________________
+function [versionString, versionStructure] = PsychtoolboxVersion
 %
-% [versionString, versionStructure]=PsychtoolboxVersion
+% [versionString, versionStructure] = PsychtoolboxVersion
 %
 % Return a string identifying this release of the Psychtoolbox.
 % The first three numbers identify the base version of Psychtoolbox:
 %
-% - Leftmost: Increments indicate a disruptive change in the feature
-% set and design of the software, by abrupt introduction of design changes.
-% This should never happen, as this would mean a completely new product,
-% entirely incompatible with the old software.
+% - Leftmost (major version): Increments indicate a disruptive change in
+% the feature set, application programming interfaces or core design of the
+% software, by abrupt introduction of drastic design changes. This should
+% never happen, as this would mean a completely new product, entirely
+% incompatible with the old software.
 %
-% - Middle: Increments indicate significant enhancements or changes in
-% functionality. This will usually only happen every couple of years at
-% most.
+% - Middle (minor version): Increments indicate very significant
+% enhancements or changes in functionality. This would for example happen
+% if there would be backwards incompatible changes to the programming
+% interface or functionality which may require code adjustments in user
+% code or even choosing different hardware or operating systems to run it.
+% This will usually only happen every couple of years at most.
 %
-% - Rightmost: A counter to distinguish multiple releases having the same
-% leftmost and middle version numbers. This happens if there are backwards
-% incompatible changes to the programming interface or functionality which
-% may require code adjustments in user code. It also happens if we cancel
-% support for platforms (Matlab/Octave versions, operating system versions,
-% processor architectures etc.). This happens occassionally.
+% - Rightmost (patch level): A counter to distinguish multiple releases
+% having the same leftmost and middle version numbers. It increments if we
+% cancel support for platforms (Matlab/Octave versions, operating system
+% versions, processor architectures etc.). This happens occassionally.
 %
 % Numeric values of the three integer fields contained in versionString are
 % available in fields of the second return argument, "versionStructure".
 %
 % The field 'Flavor' defines the subtype of Psychtoolbox being used:
 %
-% * beta: An experimental release that is already tested by the developers,
-% but not yet sufficiently tested or proven in the field. Beta releases
-% contain lots of new and experimental features that may be useful to you
-% but that may change slightly in behaviour or syntax in the final release,
-% making it necessary for you to adapt your code after a software update.
-% Beta releases are known to be imperfect and fixing bugs in them is not a
-% high priority.  The term 'current' is a synonym for 'beta'. Beta releases
-% are the only releases we provide at this point.
+% * beta: The term 'current' is a synonym for 'beta'. Beta releases are the
+% only releases we provide at this point. 'Beta' is a historical name, not
+% anything is really much of beta quality about them, they are considered
+% the most stable and trustworthy releases.
 %
 % * trunk: Development prototypes, for testing and debugging by developers
-% and really adventuruous users, not for production use!
+% and really adventureous users, not for production use!
 %
 % * Psychtoolbox-x.y.z: Old, no longer supported Psychtoolbox versions.
 %
-% * Debian package: A Psychtoolbox provided by GNU/Debian or NeuroDebian.
+% * Debian package: A Psychtoolbox provided by GNU/Debian, Ubuntu or NeuroDebian.
 %
 % The revision number and the provided URL allows you to visit the developer
 % website in the Internet and get direct access to all development logs
 % regarding your working copy of Psychtoolbox.
 %
-% Be aware that execution of the PsychtoolboxVersion command can take a
-% lot of time (in the order of multiple seconds to 1 minute).
+% Be aware that execution of the PsychtoolboxVersion command can take a lot
+% of time (in the order of multiple seconds to 1 minute).
 %
 % Most Psychtoolbox mex files now provide a built-in 'Version' command
 % which returns version for themselves.  The version string for the
@@ -77,7 +74,7 @@ function [versionString, versionStructure]=PsychtoolboxVersion
 %   04/30/12   mk       Kill MacOS-9 support.
 %   05/27/12   mk       Switch over to GitHub hosting.
 %   08/06/14   mk       Integrate (Neuro)Debian versioning support. Cleanups.
-%   10/28/20   mk       Add SVN support via Matlabs SVNKit.
+%   10/28/20   mk       Add SVN support via Matlabs SVNKit. Clarify versioning scheme.
 
 global Psychtoolbox
 
