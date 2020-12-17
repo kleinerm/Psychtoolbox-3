33bee7813eb1ba13161d6b40cfa1bfb7e707452c
Mario Kleiner <mario.kleiner.de@gmail.com>
2020-12-17 21:43:46 +0100

    Remove old precompiled intel-ddx for Ubuntu 14.04-LTS.
    This is no longer supported by PTB 3.0.17, so save another
    dozen MB's or so.

--- Psychtoolbox/PsychHardware/LinuxDrivers/Contents.m
+++ Psychtoolbox/PsychHardware/LinuxDrivers/Contents.m
@@ -8,13 +8,6 @@
 % Laptops with the proprietary NVidia graphics driver
 % instead of the open-source nouveau driver.
 %
-% This folder contains some customized drivers for old
-% Ubuntu 14.04.2 LTS. Nowadays you should rather upgrade
-% your Linux distribution to Ubuntu 14.04.5 LTS er even
-% better to Ubuntu 16.04 LTS, instead of installing these
-% custom drivers for old and outdated Ubuntu versions.
-%
-%
 % CAUTION: These drivers are only for specific processor
 % architectures and versions of the X-Server, and thereby
 % only for specific versions of specific Linux distributions.
@@ -28,41 +21,9 @@
 %
 % xdpyinfo | grep 'X.Org version'
 %
-% ... into a terminal window. It should report version 1.16 for a
-% XOrg 1.16 server.
+% ... into a terminal window. It should report version 1.19 for a
+% XOrg 1.19 server.
 %
 % Currently the following drivers are provided:
 %
-%
-% Drivers for 64-Bit Ubuntu/KUbuntu/*buntu versions 14.04.2 LTS
-% and 14.10, as well as other compatible 64-Bit distributions with a
-% XOrg 1.16 X-Server:
-%
-% FOR INTEL graphics cards, install intel_drv.so_64BitForXOrg1-16
-%
-% This binary driver is built from intel-ddx driver version 2.99.917.
-% It contains some useful bug fixes and multi-display improvements
-% over the standard 2.99.914 driver that is included in Ubuntu
-% 14.04.2-LTS and 14.10 by default. It also contains a modification to
-% disable graphics triple-buffering by default.
-%
-% 0. If the file isn't available in your LinuxDrivers directory, you
-% can simply download it from this URL:
-%
-% https://github.com/Psychtoolbox-3/Psychtoolbox-3/blob/master/Psychtoolbox/PsychHardware/LinuxDrivers/intel_drv.so_64BitForXOrg1-16?raw=true
-%
-% 1. Install it from a terminal window via:
-%
-% sudo cp intel_drv.so_64BitForXOrg1-16 /usr/lib/x86_64-linux-gnu/xorg/extra-modules/intel_drv.so
-%
-% 2. Logout and login again, so the new driver gets loaded.
-%
-% The source code (and license) from which this binary driver
-% was built can be found in the following tar.gz file:
-%
-% xf86-video-intel-2-99-917+MKTripleBufferOff.tar.gz
-%
-% The patch which was applied to the original 2.99.917 source to
-% disable triple buffering by default can be found in the file:
-% 0001-Disable-triple-buffering-by-default.patch
-%
+
