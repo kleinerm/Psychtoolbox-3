6cf3bbb62392780b36f4c7c57c71c098ed1273de
Mario Kleiner <mario.kleiner.de@gmail.com>
2021-02-09 23:30:34 +0100

    BatchAnalyzeTiming: Fixup for use without 3rd party subdir() function.

--- Psychtoolbox/PsychTests/BatchAnalyzeTiming.m
+++ Psychtoolbox/PsychTests/BatchAnalyzeTiming.m
@@ -9,24 +9,23 @@ function BatchAnalyzeTiming(sdir)
         sdir = pwd;
     end
     
-    % Recursively get filenames of all result files in 'sdir' directory tree:
-    [fdir, fnames] = subdir(sdir);
+    % Get filenames of all result files in 'sdir' directory tree:
+    fnames = FileFromFolder(sdir);
 
-    [gnrTotal, gnrSkipped, gnrCorrupt, gnrDisagree, gnrDelayed, meanDiff, stdDiff, rawmeanDiff, rawstdDiff, rawrangeDiff] = deal(0);
+    [gnrTotal, gnrSkipped, gnrCorrupt, gnrDisagree, gnrDelayed, meanDiff, stdDiff, rawmeanDiff, rawstdDiff] = deal(0);
     sc = 0;
     rangeDiff = zeros(3,0);
     rawrangeDiff = zeros(3,0);
     
     % Check each for valid and matching datafile:
     for fi = 1:length(fnames)
-        fname = char(fnames{fi});
+        fname = char(fnames(fi).fname);
 
         % Only interested in Priority 1 runs with sync-flip and medium load:
-%        if ~isempty(strfind(fname, 'Res_flipconfig_prio1_syncflip')) && ~isempty(strfind(fname,'_medium_')) %&& ~isempty(strfind(fname,'noload'))
-        if ~isempty(strfind(fname, 'Res_flipconfig_'))
+        if ~isempty(strfind(fname, 'Res_flip')) %#ok<STREMP>
 
             % Load result file: Will create struct 'res' with data:
-            load(fname);
+            load(fname); %#ok<LOAD>
             
             if (res.measurementType == 'p') && res.VBLTimestampingMode == 1 && isfield(res, 'onsetFlipTime') && res.onsetFlipTime(end) > 0
             %if (res.measurementType == 'd') && res.VBLTimestampingMode == 1 && isfield(res, 'onsetFlipTime') && res.onsetFlipTime(end) > 0
