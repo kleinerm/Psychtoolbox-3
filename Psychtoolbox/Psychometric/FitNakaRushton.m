acf4a509b6ddfff34fa735f32f1d7bd2a796fd1d
David Brainard <brainard@psych.upenn.edu>
2021-07-01 08:07:05 -0400

    Add end statement
    There was an implicit end for the main function.  This worked fine in Matlab interpreter, but not compiler.  Added explicit end.

--- Psychtoolbox/Psychometric/FitNakaRushton.m
+++ Psychtoolbox/Psychometric/FitNakaRushton.m
@@ -2,10 +2,11 @@ function [params,f] = FitNakaRushton(contrast,response,params0)
 % [params,f] = FitNakaRushton(contrast,response [, params0])
 %
 % Find the parameters that best fit the data.  If the initial guess
-% isn't good, this can get stuck in local minima.  
+% isn't good, this can get stuck in local minima.
 %
 % 8/1/05    dhb, pr     Wrote it.
 % 8/2/07    dhb         Change argument order.
+% 7/1/21    dhb         Add end statement for compiler happiness.
 
 % If params0 isn't passed, try to pick a good default.
 if (nargin < 3 || isempty(params0))
@@ -23,26 +24,29 @@ end
 
 vlb = [0      0.001     0.001]';
 vub = [1000   100000    100]';
-params = fmincon(@FitNakaRushtonFun,params0,[],[],[],[],vlb,vub,[],options); 
+params = fmincon(@FitNakaRushtonFun,params0,[],[],[],[],vlb,vub,[],options);
 f = FitNakaRushtonFun(params);
 
-    % Nested target function to optimize:
-    function [f] = FitNakaRushtonFun(params)
-    % [f] = FitNakaRushtonFun(params)
-    % 
-    % Evaluate model fit and return measure of goodness of fit (f).
-    %
-    % 8/1/05    dhb, pr     Wrote it.
-    % 8/2/07    dhb         Get rid of silly call to ComputeNakaRushtonError.
+end
 
-    % Unpack paramters, make predictions
-    prediction = ComputeNakaRushton(params,contrast);
-    nPoints = length(prediction);
-    error = prediction-response;
-    f = 10000*sqrt(sum(error.^2)/nPoints);
+% Target function to optimize:
+function [f] = FitNakaRushtonFun(params)
+% [f] = FitNakaRushtonFun(params)
+%
+% Evaluate model fit and return measure of goodness of fit (f).
+%
+% 8/1/05    dhb, pr     Wrote it.
+% 8/2/07    dhb         Get rid of silly call to ComputeNakaRushtonError.
 
-    % Handle bizarre parameter values.
-    if (isnan(f))
-        f = 2000;
-    end
+% Unpack paramters, make predictions
+prediction = ComputeNakaRushton(params,contrast);
+nPoints = length(prediction);
+error = prediction-response;
+f = 10000*sqrt(sum(error.^2)/nPoints);
+
+% Handle bizarre parameter values.
+if (isnan(f))
+    f = 2000;
+end
+
 end
