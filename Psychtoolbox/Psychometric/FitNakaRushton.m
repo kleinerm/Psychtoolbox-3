462a7f908252fec63a56d14af4bd50493d005dd8
David Brainard <brainard@psych.upenn.edu>
2021-07-01 08:19:09 -0400

    Added example, debugged.  Seems to work.

--- Psychtoolbox/Psychometric/FitNakaRushton.m
+++ Psychtoolbox/Psychometric/FitNakaRushton.m
@@ -8,6 +8,22 @@ function [params,f] = FitNakaRushton(contrast,response,params0)
 % 8/2/07    dhb         Change argument order.
 % 7/1/21    dhb         Add end statement for compiler happiness.
 
+% Examples:
+%{
+Rmax = 2; sigma = 0.5; n = 2.4;
+params = [Rmax sigma n];
+contrast = linspace(0,1,10);
+response = ComputeNakaRushton(params,contrast);
+fineContrast = linspace(0,1,100);
+fitParams = FitNakaRushton(contrast,response);
+predict = ComputeNakaRushton(fitParams,fineContrast);
+figure; clf; hold on
+plot(contrast,response,'ko','MarkerSize',12,'MarkerFaceColor','k');
+plot(fineContrast,predict,'k','LineWidth',2);
+xlabel('Contrast'); ylabel('Response');
+xlim([0 1]); ylim([0 2]);
+%}
+
 % If params0 isn't passed, try to pick a good default.
 if (nargin < 3 || isempty(params0))
     params0(1) = max(response);
@@ -24,13 +40,13 @@ end
 
 vlb = [0      0.001     0.001]';
 vub = [1000   100000    100]';
-params = fmincon(@FitNakaRushtonFun,params0,[],[],[],[],vlb,vub,[],options);
-f = FitNakaRushtonFun(params);
+params = fmincon(@(params)FitNakaRushtonFun(params,contrast,response),params0,[],[],[],[],vlb,vub,[],options);
+f = FitNakaRushtonFun(params,contrast,response);
 
 end
 
 % Target function to optimize:
-function [f] = FitNakaRushtonFun(params)
+function [f] = FitNakaRushtonFun(params,contrast,response)
 % [f] = FitNakaRushtonFun(params)
 %
 % Evaluate model fit and return measure of goodness of fit (f).
