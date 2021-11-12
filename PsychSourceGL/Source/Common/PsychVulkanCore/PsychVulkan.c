ca73da02fcaf152c6510e21b3e6a74992259c028
kleinerm <mario.kleiner.de@gmail.com>
2021-11-12 07:24:40 +0100

    PsychVulkanCore: Fix validation warning for non-AMD on Windows.
    Adding the VK_STRUCTURE_TYPE_HDR_METADATA_EXT to surface capability
    queries is something that displeases the validation layer on Windows with
    non-AMD gpu's, as this is AMD specific, not part of the standard spec.
    Skip it on Windows for non-AMD to avoid one warning.

--- PsychSourceGL/Source/Common/PsychVulkanCore/PsychVulkan.c
+++ PsychSourceGL/Source/Common/PsychVulkanCore/PsychVulkan.c
@@ -1224,7 +1224,8 @@ psych_bool PsychProbeSurfaceProperties(PsychVulkanWindow* window, PsychVulkanDev
         // amdvlk as of 2020-Q2-4 on Linux crashes in windowed mode if we set this non-NULL, because of a driver bug, so be careful to not provoke this:
         // Note: This has been fixed as of amdvlk 2020-Q2-6, so no crash anymore in windowed mode, but returns some hard-coded values instead of
         // real monitor data, so enabling it again is currently of little additional value.
-        .pNext = ((PSYCH_SYSTEM == PSYCH_WINDOWS) || (window->display != VK_NULL_HANDLE)) ? &window->nativeDisplayHDRMetadata : NULL,
+        // Also skip for non-AMD on MS-Windows, otherwise we get validation warnings.
+        .pNext = (((PSYCH_SYSTEM == PSYCH_WINDOWS) && (vulkan->driverProps.driverID == VK_DRIVER_ID_AMD_PROPRIETARY_KHR)) || (window->display != VK_NULL_HANDLE)) ? &window->nativeDisplayHDRMetadata : NULL,
     };
 
     VkSurfaceCapabilities2KHR surfaceCapabilities2 = {
