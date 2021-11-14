44964d7626dea729a8e802759e70635342fd3c6f
kleinerm <mario.kleiner.de@gmail.com>
2021-11-14 23:04:36 +0100

    TextToStuffColorMismatchTest: Update it to be relevant in the year 2021.
    We only use the FTGL text renderer plugin by default, so remove redundant
    testing code for Apple crap. Also clean up linter warnings.
    
    Tested on Win-10, R2021b.

--- Psychtoolbox/PsychTests/TextToStuffColorMismatchTest.m
+++ Psychtoolbox/PsychTests/TextToStuffColorMismatchTest.m
@@ -2,24 +2,20 @@ function TextToStuffColorMismatchTest
 % TextToStuffColorMismatchTest - Test if text is drawn
 % in the color it was requested to be drawn.
 %
-% This demonstrates a bug in the OSX CoreText renderer,
-% as used in released PTB versions since at least
-% 30th December 2013.
+% In the past, this demonstrated a bug in the OSX CoreText renderer,
+% as used in released PTB versions far in the past. Nowadays nobody gives
+% a crap about Apples crap anymore and we use our own OS independent plugin
+% text renderer by default, so this is what we test.
 %
 % History:
 %
 % 16-Jun-2015  mk  Derived from code provided by user shandelman116.
 % 14-Jul-2015  mk  Adapt to new situation that renderer 0 == CoreText on OSX.
+% 14-Nov-2021  mk  Throw out a lot of pointless code for the year 2021.
 
-%Setup
-Screen('Preference', 'DefaultTextYPositionIsBaseline', 0);
+% Setup
 Screen('Preference', 'TextAlphaBlending', 0);
 Screen('Preference', 'TextAntiAliasing', 0);
-if IsOSX
-    % Renderer 0 = CoreText on OSX.
-    Screen('Preference','TextRenderer', 0);
-end
-
 [window, wsize] = Screen('OpenWindow', 0, 0);
 
 centerX = wsize(3)/2;
@@ -42,75 +38,39 @@ crosslines = [
 
 crossLineCols = [];
 crossTextCols = [];
-crossLineCols2 = [];
-crossTextCols2 = [];
 
-%Drawing fixation cross as lines then as text
+% Drawing fixation cross as lines then as text
 for grey = greys
     Screen('DrawLines', window, crosslines, 5, grey, [centerX, centerY]);
     Screen('Flip', window);
     crossLine = Screen('GetImage', window, [], [], [], 1);
 
-    [dummy dummy textbounds] = DrawFormattedText(window, fixcross, 'center', 'center', grey);
+    [~, ~, textbounds] = DrawFormattedText(window, fixcross, 'center', 'center', grey);
     Screen('FrameRect', window, 255, textbounds);
     Screen('Flip', window);
     crossText = Screen('GetImage', window, [], [], [], 1);
 
-    %Compile list of the color used for each type from a center pixel
-    crossLineCols = [crossLineCols, crossLine(centerY, centerX)];
+    % Compile list of the color used for each type from a center pixel
+    crossLineCols = [crossLineCols, crossLine(centerY, centerX)]; %#ok<*AGROW> 
     crossTextCols = [crossTextCols, crossText(centerY, centerX)];
-end
-
-if 0
-    % Now testing TextRenderer = 2 - FTGL plugin
-    Screen('Preference','TextRenderer', 2);
-
-    % Need to remove black for this part to work
-    greys(1) = 1;
-
-    for grey = greys
-        Screen('DrawLines', window, crosslines, 5, grey, [centerX, centerY]);
-        Screen('Flip', window);
-        crossLine2 = Screen('GetImage', window, [], [], [], 1);
-        DrawFormattedText(window, fixcross, 'center', 'center', grey);
-        Screen('Flip', window);
-        crossText2 = Screen('GetImage', window, [], [], [], 1);
-        [rows, cols] = find(crossText2); %Need to use this since the centering is too off to pull the center pixel values
-        %Compile list of the color used for each type from a center pixel
-        crossLineCols2 = [crossLineCols2, crossLine2(centerY, centerX)];
-        crossTextCols2 = [crossTextCols2, crossText2(rows(1), cols(1))];
-    end
 end
 
 ShowCursor('Arrow');
 sca;
 
-% Restore to standard HQ OS specific text renderer 1:
+% Restore to standard HQ plugin text renderer 1:
 Screen('Preference','TextRenderer', 1);
 
 % Checking for TextRenderer 1
-figure
+figure;
 plot(crossLineCols, crossTextCols, '*-');
 
 if ~isequal(crossLineCols, crossTextCols)
     fprintf('\n\n\nDANGER WILL ROBINSON! Mismatch between requested and rendered text colors on text renderer!!!\n');
     hold on;
-    plot([1/255:0.01:1] * 255, 255 * ([1/255:0.01:1].^(1/1.2)))
+    plot((1/255:0.01:1) * 255, 255 * ((1/255:0.01:1).^(1/1.2)))
     hold off;
 
-    crossTextCols
+    crossTextCols %#ok<*NOPRT> 
     crossLineCols
-end
-
-if ~isempty(crossTextCols2)
-    %Checking with TextRenderer 2
-
-    figure
-    plot(crossLineCols2, crossTextCols2, '*-');
-
-    if ~isequal(crossLineCols2, crossTextCols2)
-        fprintf('\n\n\nDANGER WILL ROBINSON! Mismatch between requested and rendered text colors on text renderer 2!!!\n');
-        crossTextCols2
-        crossLineCols2
-    end
 end
