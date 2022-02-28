2e03a3a70d4b16ed973ecd8b595580828cb7a752
Mario Kleiner <mario.kleiner.de@gmail.com>
2022-02-28 17:31:10 +0100

    GetClicks(): Add timeout for 1st click, and click time reporting.
    'untilTime' if set gives an absolute time of when waiting for 1st
    click should end with all-zero return values.
    
    'clickSecs' is a vector reporting detection times of 1st, 2nd, 3rd,...
    click.
    
    Also reduce the yield interval from 2 to 1 msecs, to account for
    modern cpu's and operating systems.
    
    Some help text cleanups, and minor other tweaks.

--- Psychtoolbox/PsychBasic/GetClicks.m
+++ Psychtoolbox/PsychBasic/GetClicks.m
@@ -1,15 +1,16 @@
-function [clicks,x,y,whichButton] = GetClicks(w, interClickSecs, mouseDev)
-% [clicks,x,y,whichButton] = GetClicks([windowPtrOrScreenNumber][, interclickSecs][, mouseDev])
+function [clicks,x,y,whichButton,clickSecs] = GetClicks(w, interClickSecs, mouseDev, untilTime)
+% [clicks,x,y,whichButton,clickSecs] = GetClicks([windowPtrOrScreenNumber][, interclickSecs][, mouseDev][, untilTime=inf])
 %
-% Wait for the user to click the mouse, count the number of clicks
-% that occur within a inter-click interval of each other, and
-% then return the number of clicks and the mouse location, as well as the
-% numbers of the pressed buttons "whichButton".
-% 
+% Wait for the user to click the mouse, and then count the number of clicks
+% that occur within a inter-click interval of each other. Return the number
+% of 'clicks' and the mouse location 'x', 'y', as well as the indices of the
+% pressed buttons in vector 'whichButton', and a vector 'clickSecs' with
+% the timestamps of when each click was detected.
+%
 % The x,y location is the location at the downstroke of the first mouse
 % click. The mouse position (x,y) is "local", i.e. relative to the origin of
 % the window or screen, if supplied; otherwise it's "global", i.e. relative
-% to the origin of the main screen (the one with the menu bar).
+% to the origin of the desktop.
 %
 % The allowed inter-click interval can be adjusted by setting the Matlab
 % global variable "ptb_mouseclick_timeout" to a value in seconds. E.g.,
@@ -20,6 +21,13 @@ function [clicks,x,y,whichButton] = GetClicks(w, interClickSecs, mouseDev)
 % 'interClickSecs' for a given call to GetClicks. A setting of zero would
 % disable multi-click detection, ie., only wait for first mouse-click or
 % press, then return immediatly.
+%
+% If the optional parameter 'untilTime' is provided, GetClicks will only wait
+% for the first click until that time and then return, regardless if the 1st click
+% happened or not. On such a timeout, all return arguments will be zero. E.g.,
+% [clicks,x,y,whichButton,clickSecs] = GetClicks(window, 0.2, [], GetSecs + 5);
+% would wait up to 5 seconds in total for the first click to happen, and then
+% another 0.2 seconds for a 2nd click, then 0.2 seconds for a 3rd click...
 %
 % The optional 'mouseDev' parameter allows to select a specific mouse or
 % pointer device to query if your system has multiple pointer devices.
@@ -32,8 +40,8 @@ function [clicks,x,y,whichButton] = GetClicks(w, interClickSecs, mouseDev)
 % 6/7/96   dhb  Modified as per Pelli suggestion to respond to cmd-.
 %          dhb  Modified as per Pelli suggestion to stop any playing sound.
 % 8/23/96  dhb  Added support for windowPtr argument.
-% 3/10/97  dgp	 windowPtrOrScreenNumber
-% 6/10/02  awi	 Added See Also.
+% 3/10/97  dgp  windowPtrOrScreenNumber
+% 6/10/02  awi  Added See Also.
 % 2/28/06  mk   Completely rewritten as a wrapper around GetMouse, based on
 %          mk   the semantic and description of OS-9 GetClicks().
 % 6/17/06  mk   We also pass the windowPtr - argument on Windows now, because
@@ -43,6 +51,7 @@ function [clicks,x,y,whichButton] = GetClicks(w, interClickSecs, mouseDev)
 %               msecs but use WaitSecs('YieldSecs') waits to prevent
 %               overload.
 % 07/29/11 mk   Allow specification of 'mouseDev' mouse device index.
+% 02/28/22 mk   Add 1st click timeout 'untilTime', and 'clickSecs' timestamps.
 
 % Inter-click interval (in secs.) for multiple mouse-clicks.
 global ptb_mouseclick_timeout;
@@ -50,7 +59,7 @@ global ptb_mouseclick_timeout;
 % Setup default click timeout if noone set:
 if isempty(ptb_mouseclick_timeout)
     ptb_mouseclick_timeout = 0.5; % Default timeout for multi-clicks is 500 msecs.
-end;
+end
 
 if nargin < 2
     interClickSecs = [];
@@ -64,6 +73,10 @@ if nargin < 3
     mouseDev = [];
 end
 
+if nargin < 4 || isempty(untilTime)
+    untilTime = inf;
+end
+
 % Are we in nice mode?
 nice = 1;
 
@@ -72,26 +85,41 @@ nice = 1;
 % wait where it yields the cpu for at least the given amount of time, we
 % can use rather strict/short wait intervals without the danger of
 % overloading the system, so no need to differentiate between OS:
-rtwait = 0.002; % 2 msecs yielding, ie., could take a bit longer.
+rtwait = 0.001; % 1 msecs yielding, ie., could take a bit longer.
 
 % Wait for release of buttons if some already pressed:
-buttons=1;
-while any(buttons)
+buttons = 1;
+while any(buttons) && (GetSecs < untilTime)
     [x,y,buttons] = GetMouse([], mouseDev);
-    if (nice>0), WaitSecs('YieldSecs', rtwait); end;
-end;
+    if ((nice > 0) && any(buttons)), WaitSecs('YieldSecs', rtwait); end
+end
+
+if any(buttons)
+    % Timed out:
+    [clicks, x, y, whichButton, clickSecs] = deal(0);
+    return;
+end
 
 % Wait for first mouse button press:
-while ~any(buttons)
+while ~any(buttons) && (GetSecs < untilTime)
     if nargin < 1
         % Don't pass windowPtr argument if none supplied.
         [x,y,buttons] = GetMouse;
     else
         % Pass windowPtr argument to GetMouse for proper remapping.
         [x,y,buttons] = GetMouse(w, mouseDev);
-    end;
-    if (nice>0), WaitSecs('YieldSecs', rtwait); end;
-end;
+    end
+    if ((nice > 0) && ~any(buttons)), WaitSecs('YieldSecs', rtwait); end
+end
+
+if ~any(buttons)
+    % Timed out:
+    [clicks, x, y, whichButton, clickSecs] = deal(0);
+    return;
+end
+
+% Timestamp 1st click:
+clickSecs = GetSecs;
 
 % First mouse click done. (x,y) is our returned mouse position. Assign
 % button number(s) of clicked button(s) as well:
@@ -99,29 +127,31 @@ whichButton = find(buttons);
 
 % Wait for further click in the timeout interval.
 clicks = 1;
-tend=GetSecs + interClickSecs;
+tend = clickSecs + interClickSecs;
 
 while GetSecs < tend
     % If already down, wait for release...
     while any(buttons) && GetSecs < tend
         [xd,yd,buttons] = GetMouse([], mouseDev);
-        if (nice>0), WaitSecs('YieldSecs', rtwait); end;
-    end;
+        if ((nice > 0) && any(buttons)), WaitSecs('YieldSecs', rtwait); end
+    end
 
     % Wait for a press or timeout:
     while ~any(buttons) && GetSecs < tend
         [xd,yd,buttons] = GetMouse([], mouseDev);
-        if (nice>0), WaitSecs('YieldSecs', rtwait); end;    
-    end;
+        if ((nice > 0) && ~any(buttons)), WaitSecs('YieldSecs', rtwait); end
+    end
 
     % Mouse click or timeout?
     if any(buttons) && GetSecs < tend
         % Mouse click. Count it.
-        clicks=clicks+1;
-        % Extend timeout for the next mouse click:
-        tend=GetSecs + interClickSecs;
-    end;
-end;
+        clicks = clicks + 1;
+
+        % Timestamp and extend timeout for the next mouse click:
+        clickSecs(end+1) = GetSecs;
+        tend = clickSecs(end) + interClickSecs;
+    end
+end
 
 % At this point, (x,y) contains the mouse-position of the first click
 % and clicks should contain the total number of distinctive mouse clicks.
