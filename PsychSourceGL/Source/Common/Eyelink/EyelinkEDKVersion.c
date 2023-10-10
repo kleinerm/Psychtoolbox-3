d6223fcf34b9c8288835480c32025ba6055b7bfc
Brian Richardson <brian@sr-research.com>
2023-10-10 15:59:18 -0400

    Eyelink: Add a new subfunction 'EyelinkEDKVersion' Resolved PR 816 issues: - indentation fix, tab > spaces - remove deprecated header section from HISTORY - clean up variable def & comments

--- PsychSourceGL/Source/Common/Eyelink/EyelinkEDKVersion.c
+++ PsychSourceGL/Source/Common/Eyelink/EyelinkEDKVersion.c
@@ -1,59 +1,44 @@
 /*
- 
-	Psychtoolbox-3/PsychSourceGL/Source/Common/Eyelink/EyelinkEDKVersion.c
- 
-	PROJECTS: Eyelink 
- 
-	AUTHORS:
-    brian@sr-research.com   br
- 
-	HISTORY:
- 
-    03/06/21	fbr		created
- 
-	TARGET LOCATION:
+    Psychtoolbox-3/PsychSourceGL/Source/Common/Eyelink/EyelinkEDKVersion.c
 
-    Eyelink.mex* resides in:
-    PsychHardware/EyelinkToolbox/EyelinkBasic
- 
- */
+    PROJECTS: Eyelink 
+
+    AUTHORS:
+    brian@sr-research.com   br
+
+*/
 
 #include "PsychEyelink.h"
 
 static char useString[] = "[edkVersionString] = Eyelink('EDKVersion')";
 
-static char synopsisString[] =
-"Returns a string with the eyelink_core library version number.\n";
+static char synopsisString[] = "Returns a string with the eyelink_core library version number.\n";
 
 static char seeAlsoString[] = "";
 
 /*
- ROUTINE: EyelinkDllVersion
- PURPOSE:
-	Returns the eyelink_core library version number.
-	returns: 0 if no reply, else time
- */
+ROUTINE: EyelinkDllVersion
+PURPOSE:
+    Returns the eyelink_core library version number.
+    returns: 0 if no reply, else time
+*/
 
 PsychError EyelinkEDKVersion(void)
 {
-	char strEdkVersion[40]="0.0.0.0";
-	
-	//all sub functions should have these two lines
-	PsychPushHelp(useString, synopsisString, seeAlsoString);
-	if(PsychIsGiveHelp()){PsychGiveHelp();return(PsychError_none);};
-	
-	//check to see if the user supplied superfluous arguments
-	PsychErrorExit(PsychCapNumInputArgs(0));
-	PsychErrorExit(PsychRequireNumInputArgs(0));
-	PsychErrorExit(PsychCapNumOutputArgs(1));
-	
-//	// Verify eyelink is up and running
-//	EyelinkSystemIsConnected();
-//	EyelinkSystemIsInitialized();
-	
-	eyelink_dll_version(strEdkVersion);
-	
-	PsychCopyOutCharArg(1, FALSE, strEdkVersion);
-	
-	return(PsychError_none);	
+    char strEdkVersion[40];
+
+    //all sub functions should have these two lines
+    PsychPushHelp(useString, synopsisString, seeAlsoString);
+    if(PsychIsGiveHelp()){PsychGiveHelp();return(PsychError_none);};
+
+    //check to see if the user supplied superfluous arguments
+    PsychErrorExit(PsychCapNumInputArgs(0));
+    PsychErrorExit(PsychRequireNumInputArgs(0));
+    PsychErrorExit(PsychCapNumOutputArgs(1));
+
+    eyelink_dll_version(strEdkVersion);
+
+    PsychCopyOutCharArg(1, FALSE, strEdkVersion);
+
+    return(PsychError_none);	
 }
