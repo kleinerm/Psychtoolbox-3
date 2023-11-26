f4bfa25c884a8fae5db85602f3c75bd8d2e57fdf
Mario Kleiner <mario.kleiner.de@gmail.com>
2023-11-26 20:21:24 +0100

    help PsychDemos, MovieDemos: Some fixes so Matlabs help system can cope.

--- Psychtoolbox/PsychDemos/Contents.m
+++ Psychtoolbox/PsychDemos/Contents.m
@@ -111,7 +111,7 @@
 %    RenderDemo              - Render CIE xyY as a color patch.
 %    SadowskiDemo            - Shows the "Sadowski" color afterimage effect.
 %    SimpleHDRDemo           - Minimal demo for using a HDR display, showing some basic stimuli.
-%    SimpleHDRLinuxStereoDemo -Like SimpleHDRDemo, but with Linux/X11 stereo hack for stereoscopic HDR stimulation.
+%    SimpleHDRLinuxStereoDemo - Like SimpleHDRDemo, but with Linux/X11 stereo hack for stereoscopic HDR stimulation.
 %    SimpleImageMixingDemo   - Shows how to mix two images via alpha-blending, using an alpha-mask which itself is morphed between two masks.
 %                              See also ImageMixingTutorial for a more complex demonstration of the technique.
 %    SimpleSoundScheduleDemo - Simple demo for basic use of sound schedules with PsychPortAudio.
