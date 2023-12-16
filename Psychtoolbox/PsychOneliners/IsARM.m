40a4296be377f2350f9a08153ea54ef79244c898
Mario Kleiner <mario.kleiner.de@gmail.com>
2023-12-16 07:54:04 +0100

    IsARM(): Try to detect running on 64-Bit ARM or Apple Silicon Matlab/Octave.

--- Psychtoolbox/PsychOneliners/IsARM.m
+++ Psychtoolbox/PsychOneliners/IsARM.m
@@ -4,11 +4,12 @@ function resultFlag = IsARM
 % Returns true if the processor architecture is ARM.
 
 % HISTORY
-% 4.4.2013 mk   Wrote it.
+% 04.04.2013 mk   Wrote it.
+% 16.12.2023 mk   Simplify and make more robust and recognize Apple Silicon. 
 
 persistent rc;
 if isempty(rc)
-     rc= ~isempty(strfind(computer, 'arm-'));
+     rc = ~isempty(strfind(computer, 'arm-')) || ~isempty(strfind(computer, 'aarch')) || streq(computer, 'MACA64'); %#ok<STREMP>
 end
 
 resultFlag = rc;
