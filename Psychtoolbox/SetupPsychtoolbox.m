af1984862b5e9b73f52bf3ba15adc99b964af126
Mario Kleiner <mario.kleiner.de@gmail.com>
2024-01-25 18:04:59 +0000

    SetupPsychtoolbox: Code cleanup, dead code removal, help text updates.

--- Psychtoolbox/SetupPsychtoolbox.m
+++ Psychtoolbox/SetupPsychtoolbox.m
@@ -15,27 +15,19 @@ function SetupPsychtoolbox(tryNonInteractiveSetup)
 % This non-interactice setup mode is highly experimental, not well tested, not
 % supported in case of any trouble!
 %
-% This setup routine is meant for people who want to install Psychtoolbox
-% but don't have direct access to the internet. Installation in that case
-% is a three step procedure:
+% Installation is a three step procedure:
 %
 % 1. Download and unpack a full working copy of PTB into your target
 % folder. Obviously you need to somehow get a copy, either via conventional
-% download from a computer with network connection (See 'help
-% DownloadPsychtoolbox' or 'help UpdatePsychtoolbox') or from a helpful
-% colleague.
+% download from a computer with network connection, visit this URL for that
+% download: http://psychtoolbox.org/download.html
+% Or from a helpful colleague or copied from some other machine.
 %
 % 2. Change your Matlab/Octave working directory to the Psychtoolbox installation
 % folder, e.g., 'cd /Applications/Psychtoolbox'.
 %
 % 3. Type 'SetupPsychtoolbox' to run this script.
 %
-% Please be aware that the recommended method of installation is via the
-% online Subversion system, i.e., DownloadPsychtoolbox and
-% UpdatePsychtoolbox. Some functionality may not work with a copy that is
-% set up via this script, e.g., PsychtoolboxVersion may provide incomplete
-% version information. Convenient upgrades via UpdatePsychtoolbox may be
-% impossible. Download size with this method is much higher as well.
 %
 % If you get stuck, post your question to the forum:
 %
@@ -62,10 +54,10 @@ function SetupPsychtoolbox(tryNonInteractiveSetup)
 % 10/28/15 mk  32-Bit Octave-4 support for MS-Windows reestablished.
 % 04/01/16 mk  64-Bit Octave-4 support for MS-Windows established.
 % 06/01/16 mk  32-Bit Octave-4 support for MS-Windows removed.
+% 01/25/24 mk  Cleanup, dead code removal.
 
-% Flush all MEX files: This is needed at least on M$-Windows to
-% work if Screen et al. are still loaded.
-clear mex
+% Flush all MEX files: This is needed at least on M$-Windows if Screen et al. are still loaded.
+clear mex; %#ok<CLMEX>
 
 if nargin < 1 || isempty(tryNonInteractiveSetup)
     tryNonInteractiveSetup = 0;
@@ -76,67 +68,14 @@ if tryNonInteractiveSetup
     pause('off');
 end
 
-% Check if this is 32-Bit Octave-4 on Windows, which we don't support at all:
-if isempty(strfind(computer, 'x86_64')) && ~isempty(strfind(computer, 'mingw32'))
-    fprintf('Psychtoolbox 3.0.13 and later do no longer work with 32-Bit GNU/Octave-4 on MS-Windows.\n');
-    fprintf('You need to use 64-Bit Octave-4 if you want to use Psychtoolbox with Octave on Windows.\n');
-    fprintf('DownloadPsychtoolbox() with flavor ''Psychtoolbox-3.0.12'', does support 32-Bit Octave-4 on Windows.\n');
-    error('Tried to setup on 32-Bit Octave, which is no longer supported on Windows.');
-end
-
-% Check if this is 32-Bit Matlab on Windows or Linux, which we don't support anymore:
-if (strcmp(computer, 'PCWIN') || strcmp(computer, 'GLNX86'))
-    fprintf('Psychtoolbox 3.0.12 and later do no longer work with 32-Bit versions of Matlab.\n');
-    fprintf('You need to upgrade to a supported 64-Bit version of Octave or Matlab. 32-Bit Octave is still\n');
-    fprintf('supported on GNU/Linux.\n');
-    fprintf('If you must use a legacy 32-Bit Matlab environment, you can call this function\n');
-    fprintf('DownloadPsychtoolbox() with flavor ''Psychtoolbox-3.0.11'', which does support 32-Bit Matlab on Linux and Windows.\n');
-    error('Tried to setup on 32-Bit Matlab, which is no longer supported.');
-end
-
-% Check if this is 32-Bit Octave or 32-Bit Matlab on OSX, which we don't support anymore:
-if (~isempty(strfind(computer, 'apple-darwin')) || strcmp(computer,'MACI')) && isempty(strfind(computer, '64'))
-    fprintf('Psychtoolbox 3.0.11 and later do no longer work with 32-Bit versions of Octave or Matlab on OSX.\n');
-    fprintf('You need to upgrade to a 64-Bit version of Octave or Matlab on OSX, which is fully supported.\n');
-    fprintf('You can also use the alternate download function DownloadLegacyPsychtoolbox() to download\n');
-    fprintf('an old legacy copy of Psychtoolbox-3.0.9, which did support 32-Bit Octave 3.2 on OSX, or use\n');
-    fprintf('DownloadPsychtoolbox() with flavor ''Psychtoolbox-3.0.10'', which does support 32-Bit Matlab on OSX.\n');
-    error('Tried to setup on 32-Bit Octave or 32-Bit Matlab, which are no longer supported on OSX.');
-end
-
-% Check if this is Octave-3 on Windows, which we don't support at all:
-if strcmp(computer, 'i686-pc-mingw32')
-    fprintf('Psychtoolbox 3.0.10 and later does no longer work with GNU/Octave-3 on MS-Windows.\n');
-    fprintf('You need to use 32-Bit Octave-4 if you want to use Psychtoolbox with Octave on Windows.\n');
-    fprintf('You can also use the alternate download function DownloadLegacyPsychtoolbox() to download\n');
-    fprintf('an old legacy copy of Psychtoolbox-3.0.9 which did support 32-Bit Octave 3.2 on Windows.\n');
-    error('Tried to setup on Octave, which is no longer supported on MS-Windows.');
-end
-
-if strcmp(computer,'MAC')
-    fprintf('This version of Psychtoolbox is no longer supported under MacOSX on the Apple PowerPC hardware platform.\n');
-    fprintf('You can get modern versions of Psychtoolbox-3 for Linux if you choose to install GNU/Linux on your PowerPC\n');
-    fprintf('machine. These are available from the GNU/Debian project and a future Ubuntu 12.10 release\n.');
-    fprintf('Alternatively you can download old - totally unsupported - releases of Psychtoolbox version 3.0.9\n');
-    fprintf('from GoogleCode by use of the alternate download function DownloadLegacyPsychtoolbox().\n\n');
-    error('Apple MacOSX on Apple PowerPC computers is no longer supported by this Psychtoolbox version.');
-end
-
-% Check OS
-IsWin = ~isempty(strfind(computer, 'PCWIN')) || ~isempty(strfind(computer, '-w64-mingw32'));
-IsOSX = ~isempty(strfind(computer, 'MAC')) || ~isempty(strfind(computer, 'apple-darwin'));
-IsLinux = strcmp(computer,'GLNX86') || strcmp(computer,'GLNXA64') || ~isempty(strfind(computer, 'linux-gnu'));
+% Validate supported OS:
+IsWin = ispc;
+IsOSX = ismac;
+IsLinux = isunix && ~ismac;
 
 if ~IsWin && ~IsOSX && ~IsLinux
-    os = computer;
-    if strcmp(os,'MAC2')
-        os = 'Mac OS9';
-    end
-    fprintf('Sorry, this updater doesn''t support your operating system: %s.\n', os);
-    fprintf([mfilename ' can only install the new (OSX, Linux and Windows) \n'...
-        'OpenGL-based versions of the Psychtoolbox-3. To install the older (OS9 and Windows) \n'...
-        'versions (not based on OpenGL, aka PTB-2) please go to the legacy Psychtoolbox website: \n'...
-        'web http://psychtoolbox.org/PTB-2/index.html\n']);
+    fprintf('Sorry, this updater doesn''t support your operating system: %s.\n', computer);
+    fprintf([mfilename ' can only install the Linux, Windows and macOS versions of the Psychtoolbox-3.\n']);
     error(['Your operating system is not supported by ' mfilename '.']);
 end
 
@@ -150,12 +89,7 @@ fprintf('Will setup working copy of the Psychtoolbox folder inside: %s\n',target
 fprintf('\n');
 
 % Does SAVEPATH work?
-if exist('savepath') %#ok<EXIST>
-   err=savepath;
-else
-   err=path2rc;
-end
-
+err=savepath;
 if err
     try
         % If this works then we're likely on Matlab:
@@ -185,8 +119,7 @@ if err
     end
 end
 
-% Handle Windows ambiguity of \ symbol being the fileseparator and a
-% parameter marker:
+% Handle Windows ambiguity of \ symbol being the fileseparator and a parameter marker:
 if IsWin
     searchpattern = [filesep filesep 'Psychtoolbox[' filesep pathsep ']'];
     searchpattern2 = [filesep filesep 'Psychtoolbox'];
@@ -241,12 +174,8 @@ while any(regexp(path, searchpattern))
             rmpath(s);
         end
     end
-    if exist('savepath') %#ok<EXIST>
-       savepath;
-    else
-       path2rc;
-    end
 
+    savepath;
     fprintf('Success.\n\n');
 end
 
@@ -256,12 +185,7 @@ p=targetdirectory;
 pp=genpath(p);
 addpath(pp);
 
-if exist('savepath') %#ok<EXIST>
-   err=savepath;
-else
-   err=path2rc;
-end
-
+err=savepath;
 if err
     fprintf('SAVEPATH failed. Psychtoolbox is now already installed and configured for use on your Computer,\n');
     fprintf('but i could not save the updated Matlab/Octave path, probably due to insufficient permissions.\n');
@@ -271,29 +195,6 @@ if err
 else 
     fprintf('Success.\n\n');
 end
-
-% fprintf(['Now setting permissions to allow everyone to write to the Psychtoolbox folder. This will \n'...
-%     'allow future updates by every user on this machine without requiring administrator privileges.\n']);
-% 
-% try
-%     if IsOSX || IsLinux
-%         [s,m]=fileattrib(p,'+w','a','s'); % recursively add write privileges for all users.
-%     else
-%         [s,m]=fileattrib(p,'+w','','s'); % recursively add write privileges for all users.
-%     end
-% catch %#ok<CTCH>
-%     s = 0;
-%     m = 'Setting file attributes is not supported under Octave.';
-% end
-% 
-% if s
-%     fprintf('Success.\n\n');
-% else
-%     fprintf('\nFILEATTRIB failed. Psychtoolbox will still work properly for you and other users, but only you\n');
-%     fprintf('or the system administrator will be able to run the UpdatePsychtoolbox script to update Psychtoolbox,\n');
-%     fprintf('unless you or the system administrator manually set proper write permissions on the Psychtoolbox folder.\n');
-%     fprintf('The error message of FILEATTRIB was: %s\n\n', m);
-% end
 
 if IsOSX
     % Apples trainwreck needs special treatment. If Psychtoolbox has been
