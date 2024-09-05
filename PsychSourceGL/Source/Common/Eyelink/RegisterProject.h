8cc5ae1600c853b6b19b572d27bfe4f0041d7a23
Mario Kleiner <mario.kleiner.de@gmail.com>
2024-09-05 02:17:13 +0200

    Eyelink: Formatting/Cleanup and future compat fixes.

--- PsychSourceGL/Source/Common/Eyelink/RegisterProject.h
+++ PsychSourceGL/Source/Common/Eyelink/RegisterProject.h
@@ -1,38 +1,30 @@
 /*
+    Psychtoolbox-3/PsychSourceGL/Source/Common/Eyelink/RegisterProject.h
 
-	/osxptb/trunk/PsychSourceGL/Source/OSX/Eyelink/RegisterProject.h
-  
-	PROJECTS: Eyelink 
-  
-	AUTHORS:
-		cburns@berkeley.edu				cdb
-		E.Peters@ai.rug.nl				emp
-		f.w.cornelissen@med.rug.nl		fwc
-  
-	PLATFORMS:	Currently only OS X  
-    
-	HISTORY:
+    PROJECTS: Eyelink
 
-		11/21/05  cdb		Created.
+    AUTHORS:
 
-	TARGET LOCATION:
+    cburns@berkeley.edu             cdb
+    E.Peters@ai.rug.nl              emp
+    f.w.cornelissen@med.rug.nl      fwc
 
-		Eyelink.mexmac resides in:
-			EyelinkToolbox
+    PLATFORMS: All
+
+    HISTORY:
+
+    11/21/05  cdb       Created.
 */
 
-//begin include once 
+// begin include once
 #ifndef PSYCH_IS_INCLUDED_RegisterProject
 #define PSYCH_IS_INCLUDED_RegisterProject
 
-#include "PsychEyelink.h"
+#define PSYCH_NOLM
 
+#include "PsychEyelink.h"
 
 PsychError PsychModuleInit(void);
 
-//end include once
+// end include once
 #endif
-
-
-
-
